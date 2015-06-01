e93ab8a731859914ea8c85b1447d9a2624febc6b
shzheng <shzheng@Ctrip.com>
2015-06-01 09:34:11 +0800

    1

---
+++ arduino/Motor/rpmCalc/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.trackBar3 = new System.Windows.Forms.TrackBar();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.trackBar2 = new System.Windows.Forms.TrackBar();
+			this.trackBar4 = new System.Windows.Forms.TrackBar();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// trackBar3
+			// 
+			this.trackBar3.Location = new System.Drawing.Point(32, 108);
+			this.trackBar3.Maximum = 255;
+			this.trackBar3.Name = "trackBar3";
+			this.trackBar3.Size = new System.Drawing.Size(792, 42);
+			this.trackBar3.TabIndex = 7;
+			this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Location = new System.Drawing.Point(32, 12);
+			this.trackBar1.Maximum = 255;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(792, 42);
+			this.trackBar1.TabIndex = 7;
+			this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+			// 
+			// trackBar2
+			// 
+			this.trackBar2.Location = new System.Drawing.Point(32, 60);
+			this.trackBar2.Maximum = 255;
+			this.trackBar2.Name = "trackBar2";
+			this.trackBar2.Size = new System.Drawing.Size(792, 42);
+			this.trackBar2.TabIndex = 7;
+			this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+			// 
+			// trackBar4
+			// 
+			this.trackBar4.Location = new System.Drawing.Point(32, 156);
+			this.trackBar4.Maximum = 255;
+			this.trackBar4.Name = "trackBar4";
+			this.trackBar4.Size = new System.Drawing.Size(792, 42);
+			this.trackBar4.TabIndex = 7;
+			this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(46, 220);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(78, 16);
+			this.checkBox1.TabIndex = 8;
+			this.checkBox1.Text = "checkBox1";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(409, 220);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 9;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(240, 248);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(522, 21);
+			this.textBox1.TabIndex = 10;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(877, 285);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.trackBar4);
+			this.Controls.Add(this.trackBar2);
+			this.Controls.Add(this.trackBar1);
+			this.Controls.Add(this.trackBar3);
+			this.Name = "Form1";
+			this.Text = "3";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar4;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ arduino/Motor/rpmCalc/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+
+	public partial class Form1 : Form
+	{
+		SerialPort port;
+		FileStream fs;
+		StreamWriter sw;
+		private delegate void FlushClient(string s);
+		public Form1()
+		{
+			InitializeComponent();
+
+			try
+			{
+				this.FormClosed += new FormClosedEventHandler(Form1_FormClosed);
+
+				fs = new FileStream("d:\\data22.txt", FileMode.Create, FileAccess.Write);
+				sw = new StreamWriter(fs);
+				if (port == null)
+				{
+					//COM4为Arduino使用的串口号，需根据实际情况调整
+					port = new SerialPort("COM12", 9600, Parity.None, 8, StopBits.One);
+					port.Open();
+				}
+			}
+			catch
+			{
+				MessageBox.Show("error");
+			}
+		}
+
+		void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			if (port != null && port.IsOpen)
+			{
+				port.Close();
+			}
+		}
+
+		private void PortWrite(int chn, int data)
+		{
+			byte[] d = new byte[2];
+			d[0] = (byte)(0x050 | chn);
+			d[1] = (byte)(data & 0x0FF);
+			if (port != null && port.IsOpen)
+			{
+				port.Write(d, 0, 1);
+				Thread.Sleep(10);
+				port.Write(d, 1, 1);
+			}
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+		}
+
+		private void trackBar1_Scroll(object sender, EventArgs e)
+		{
+			int v = trackBar1.Value;
+			if (checkBox1.Checked)
+			{
+				this.Text = v.ToString();
+				PortWrite(1, v);
+			}
+			else
+			{
+				this.Text = (2000 + (v >> 3)).ToString();
+				PortWrite(5, v);
+			}
+		}
+
+		private void trackBar2_Scroll(object sender, EventArgs e)
+		{
+			int v = trackBar2.Value;
+			if (checkBox1.Checked)
+			{
+				this.Text = v.ToString();
+				PortWrite(2, v);
+			}
+			else
+			{
+				this.Text = (2000 + (v >> 3)).ToString();
+				PortWrite(6, v);
+			}
+		}
+
+		private void trackBar3_Scroll(object sender, EventArgs e)
+		{
+			int v = trackBar3.Value;
+			if (checkBox1.Checked)
+			{
+				this.Text = v.ToString();
+				PortWrite(3, v);
+			}
+			else
+			{
+				this.Text = (2000 + (v >> 3)).ToString();
+				PortWrite(7, v);
+			}
+		}
+
+		private void trackBar4_Scroll(object sender, EventArgs e)
+		{
+			int v = trackBar4.Value;
+			if (checkBox1.Checked)
+			{
+				this.Text = v.ToString();
+				PortWrite(4, v);
+			}
+			else
+			{
+				this.Text = (2000 + (v >> 3)).ToString();
+				PortWrite(8, v);
+			}
+		}
+
+
+		public void aa()
+		{
+			//string s = "";
+			string s2 = "";
+			//double start = 300;//rpm
+			//double end = 20000;//rpm
+
+			double start = 39;//rpm
+			double end = 600;//rpm
+
+			//8000tick/p ->
+			//1000p/8s ->
+			//
+
+			double count = 128;
+			double targetrpm = start;
+			double inc = ((end - start)) / count;
+			for (double i = 0; i < count; i++)
+			{
+				double rps = targetrpm / 60;
+				double rpms = rps / 1000;
+				double rpus = rpms / 1000;
+				double uspr = 1 / rpus;
+				double uspp3 = uspr / 18;
+				double uspp4 = uspr / 24;
+				//s += targetrpm + ",";
+				s2 += (int)(uspp4) + ",";
+				targetrpm += inc;
+				inc *= 0.983;
+			}
+
+			textBox1.Text = s2;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			aa();
+		}
+
+	}
+}
+
+
