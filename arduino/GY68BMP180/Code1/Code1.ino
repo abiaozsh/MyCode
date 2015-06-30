656de8cdbca2487bd5fabe50bb4369de405e6e09
shzheng <shzheng@Ctrip.com>
2015-06-30 17:16:07 +0800

    1

--- arduino/GY68BMP180/Code1/Code1.ino
+++ arduino/GY68BMP180/Code1/Code1.ino
@@ -1,35 +1,6 @@
-
-Temperature: 30.60deg C
-
-Pressure: 100265 Pa
-
-Altitude: 88.61 M
-
-
-
-Temperature: 30.60deg C
-
-Pressure: 100267 Pa
-
-Altitude: 88.45 M
-
-
-
-Temperature: 30.50deg C
-
-Pressure: 100274 Pa
-
-Altitude: 87.86 M
-
-
-
-
 #include <Wire.h>
-
 #define BMP085_ADDRESS 0x77  // I2C address of BMP085
-
 const unsigned char OSS = 3;  // Oversampling Setting ultra high resolution
-
 // Calibration values
 int ac1;
 int ac2;
@@ -42,16 +13,137 @@ int b2;
 int mb;
 int mc;
 int md;
-
-// b5 is calculated in bmp085GetTemperature(...), this variable is also used in bmp085GetPressure(...)
-// so ...Temperature(...) must be called before ...Pressure(...).
 long b5; 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define cbi(reg, bitmask) *reg &= ~bitmask
+#define sbi(reg, bitmask) *reg |= bitmask
+#define pulseClock cbi(P_SCK, B_SCK); asm ("nop"); sbi(P_SCK, B_SCK)
+#define resetLCD sbi(P_DC, B_DC); sbi(P_MOSI, B_MOSI); sbi(P_SCK, B_SCK); sbi(P_CS, B_CS); cbi(P_RST, B_RST); delay(10); sbi(P_RST, B_RST)
+
+#define fontbyte(x) pgm_read_byte(&cfont.font[x])  
+#define bitmapbyte(x) pgm_read_byte(&bitmap[x])  
+
+#define regtype volatile uint8_t
+#define regsize volatile uint8_t
+#define bitmapdatatype uint8_t*
+
+#define LEFT 0
+#define RIGHT 9999
+#define CENTER 9998
+
+#define LCD_COMMAND 0
+#define LCD_DATA 1
+
+// PCD8544 Commandset
+// ------------------
+// General commands
+#define PCD8544_POWERDOWN			0x04
+#define PCD8544_ENTRYMODE			0x02
+#define PCD8544_EXTENDEDINSTRUCTION	0x01
+#define PCD8544_DISPLAYBLANK		0x00
+#define PCD8544_DISPLAYNORMAL		0x04
+#define PCD8544_DISPLAYALLON		0x01
+#define PCD8544_DISPLAYINVERTED		0x05
+// Normal instruction set
+#define PCD8544_FUNCTIONSET			0x20
+#define PCD8544_DISPLAYCONTROL		0x08
+#define PCD8544_SETYADDR			0x40
+#define PCD8544_SETXADDR			0x80
+// Extended instruction set
+#define PCD8544_SETTEMP				0x04
+#define PCD8544_SETBIAS				0x10
+#define PCD8544_SETVOP				0x80
+// Display presets
+#define LCD_BIAS					0x03	// Range: 0-7 (0x00-0x07)
+#define LCD_TEMP					0x02	// Range: 0-3 (0x00-0x03)
+#define LCD_CONTRAST				0x46	// Range: 0-127 (0x00-0x7F)
+
+
+class LCD5110
+{
+public:
+  LCD5110(int SCK, int MOSI, int DC, int RST, int CS);
+  void	InitLCD(int contrast=LCD_CONTRAST);
+  void	setContrast(int contrast);
+  //void	enableSleep();
+  //void	disableSleep();
+  void	clrScr();
+  void	invert(bool mode);
+  void	drawBitmap(int x, int y, bitmapdatatype bitmap, int sx, int sy);
+  void	drawBitmapBuff(int x, int y, byte *bitmap, int sx, int sy);
+
+  regtype			*P_SCK, *P_MOSI, *P_DC, *P_RST, *P_CS;
+  regsize			B_SCK, B_MOSI, B_DC, B_RST, B_CS;
+  uint8_t			SCK_Pin, RST_Pin;			// Needed for for faster MCUs
+  boolean			_sleep;
+  int				_contrast;
+
+  void	_LCD_Write(unsigned char data, unsigned char mode);
+  void	_print_char(unsigned char c, int x, int row);
+  void	_convert_float(char *buf, double num, int width, byte prec);
+};
+
+
+
+/*
+Temperature: 30.50deg C
+ 
+ Pressure: 100274 Pa
+ 
+ Altitude: 87.86 M
+ */
+
+
+#define _SCK  10
+#define _MOSI 11
+#define _DC 12
+#define _CS 13
+#define _RST A0
+
+LCD5110 myGLCD(_SCK,_MOSI,_DC,_RST,_CS);
+uint8_t buff[504];
+
+
+uint8_t line0[10];
+uint8_t line1[10];
+uint8_t line2[10];
+uint8_t line3[10];
+
+PROGMEM prog_uint8_t Font[] = {
+B00000000,
+B01111110,
+B10000001,
+B01111110,//0
+B00000000,
+B10000001,
+B11111111,
+B00000001,//1
+B00000000,
+B10000111,
+B10001001,
+B01110001,//2
+B00000000,
+B10001001,
+B10001001,
+B01110111,//3
+B00000000,
+B10001001,
+B10001001,
+B01110111,//3
+};
+
+
+float basepressure;
 
 void setup(){
   Serial.begin(9600);
   Wire.begin();
-
+  myGLCD.InitLCD();
   bmp085Calibration();
+  delay(1000);
+  float temperature = bmp085GetTemperature(bmp085ReadUT()); //MUST be called first
+  basepressure = bmp085GetPressure(bmp085ReadUP());
 }
 
 void loop()
@@ -59,25 +151,38 @@ void loop()
   float temperature = bmp085GetTemperature(bmp085ReadUT()); //MUST be called first
   float pressure = bmp085GetPressure(bmp085ReadUP());
   float altitude = calcAltitude(pressure); //Uncompensated caculation - in Meters 
-  
   Serial.print("Temperature: ");
   Serial.print(temperature, 2); //display 2 decimal places
   Serial.println("deg C");
-
   Serial.print("Pressure: ");
   Serial.print(pressure, 0); //whole number only.
   Serial.println(" Pa");
-
   Serial.print("Altitude: ");
   Serial.print(altitude, 2); //display 2 decimal places
   Serial.println(" M");
-
   Serial.println();//line break
-
-  //��¼ʵ�ʵ���
-
   delay(500); //wait a second and get values again.
+  
+  printline();
+  myGLCD.drawBitmapBuff(0, 0, buff, 84, 48);
+
 }
+
+void printline()
+{
+//x:84
+//y:48
+//  setPixel(0,0,1);
+//  setPixel(0,1,1);
+//  setPixel(1,1,0);
+//  setPixel(1,0,0);
+
+//x:84
+//y:0~6
+//void setLine(byte x, byte y, byte val)
+
+}
+
 
 // Stores all of the bmp085's calibration values into global variables
 // Calibration values are required to calculate temp and pressure
@@ -262,3 +367,188 @@ float calcAltitude(float pressure){
 
   return C;
 }
+
+
+
+
+
+
+LCD5110::LCD5110(int SCK, int MOSI, int DC, int RST, int CS)
+{ 
+  P_SCK	= portOutputRegister(digitalPinToPort(SCK));
+  B_SCK	= digitalPinToBitMask(SCK);
+  P_MOSI	= portOutputRegister(digitalPinToPort(MOSI));
+  B_MOSI	= digitalPinToBitMask(MOSI);
+  P_DC	= portOutputRegister(digitalPinToPort(DC));
+  B_DC	= digitalPinToBitMask(DC);
+  P_RST	= portOutputRegister(digitalPinToPort(RST));
+  B_RST	= digitalPinToBitMask(RST);
+  P_CS	= portOutputRegister(digitalPinToPort(CS));
+  B_CS	= digitalPinToBitMask(CS);
+  pinMode(SCK,OUTPUT);
+  pinMode(MOSI,OUTPUT);
+  pinMode(DC,OUTPUT);
+  pinMode(RST,OUTPUT);
+  pinMode(CS,OUTPUT);
+  SCK_Pin=SCK;
+  RST_Pin=RST;
+}
+
+void LCD5110::_LCD_Write(unsigned char data, unsigned char mode)
+{   
+  cbi(P_CS, B_CS);
+
+  if (mode==LCD_COMMAND)
+    cbi(P_DC, B_DC);
+  else
+    sbi(P_DC, B_DC);
+
+  for (unsigned char c=0; c<8; c++)
+  {
+    if (data & 0x80)
+      sbi(P_MOSI, B_MOSI);
+    else
+      cbi(P_MOSI, B_MOSI);
+    data = data<<1;
+    pulseClock;
+  }
+
+  sbi(P_CS, B_CS);
+}
+
+void LCD5110::InitLCD(int contrast)
+{
+  if (contrast>0x7F)
+    contrast=0x7F;
+  if (contrast<0)
+    contrast=0;
+
+  resetLCD;
+
+  _LCD_Write(PCD8544_FUNCTIONSET | PCD8544_EXTENDEDINSTRUCTION, LCD_COMMAND);
+  _LCD_Write(PCD8544_SETVOP | contrast, LCD_COMMAND);
+  _LCD_Write(PCD8544_SETTEMP | LCD_TEMP, LCD_COMMAND);
+  _LCD_Write(PCD8544_SETBIAS | LCD_BIAS, LCD_COMMAND);
+  _LCD_Write(PCD8544_FUNCTIONSET, LCD_COMMAND);
+  _LCD_Write(PCD8544_SETYADDR, LCD_COMMAND);
+  _LCD_Write(PCD8544_SETXADDR, LCD_COMMAND);
+  for (int c=0; c<504; c++)
+    _LCD_Write(0x00, LCD_DATA);
+  _LCD_Write(PCD8544_DISPLAYCONTROL | PCD8544_DISPLAYNORMAL, LCD_COMMAND);
+
+  _sleep=false;
+  _contrast=contrast;
+}
+
+void LCD5110::setContrast(int contrast)
+{
+  if (contrast>0x7F)
+    contrast=0x7F;
+  if (contrast<0)
+    contrast=0;
+  _LCD_Write(PCD8544_FUNCTIONSET | PCD8544_EXTENDEDINSTRUCTION, LCD_COMMAND);
+  _LCD_Write(PCD8544_SETVOP | contrast, LCD_COMMAND);
+  _LCD_Write(PCD8544_FUNCTIONSET, LCD_COMMAND);
+  _contrast=contrast;
+}
+
+void LCD5110::clrScr()
+{
+  if (!_sleep)
+  {
+    _LCD_Write(PCD8544_SETYADDR, LCD_COMMAND);
+    _LCD_Write(PCD8544_SETXADDR, LCD_COMMAND);
+    for (int c=0; c<504; c++)
+      _LCD_Write(0x00, LCD_DATA);
+  }
+}
+
+void LCD5110::invert(bool mode)
+{
+  if (!_sleep)
+  {
+    if (mode==true)
+      _LCD_Write(PCD8544_DISPLAYCONTROL | PCD8544_DISPLAYINVERTED, LCD_COMMAND);
+    else
+      _LCD_Write(PCD8544_DISPLAYCONTROL | PCD8544_DISPLAYNORMAL, LCD_COMMAND);
+  }
+}
+
+void LCD5110::drawBitmap(int x, int y, bitmapdatatype bitmap, int sx, int sy)
+{
+  int starty, rows;
+
+  if (!_sleep)
+  {
+    starty = y / 8;
+
+    if (sy%8==0)
+      rows=sy/8;  
+    else
+      rows=(sy/8)+1;
+
+    for (int cy=0; cy<rows; cy++)
+    {
+      _LCD_Write(PCD8544_SETYADDR | (starty+cy), LCD_COMMAND);
+      _LCD_Write(PCD8544_SETXADDR | x, LCD_COMMAND);
+      for(int cx=0; cx<sx; cx++)
+        _LCD_Write(bitmapbyte(cx+(cy*sx)), LCD_DATA);
+    }      
+    _LCD_Write(PCD8544_SETYADDR, LCD_COMMAND);
+    _LCD_Write(PCD8544_SETXADDR, LCD_COMMAND);
+  }
+}
+
+void LCD5110::drawBitmapBuff(int x, int y, byte *bitmap, int sx, int sy)
+{
+  int starty, rows;
+
+  if (!_sleep)
+  {
+    starty = y / 8;
+
+    if (sy%8==0)
+      rows=sy/8;  
+    else
+      rows=(sy/8)+1;
+
+    for (int cy=0; cy<rows; cy++)
+    {
+      _LCD_Write(PCD8544_SETYADDR | (starty+cy), LCD_COMMAND);
+      _LCD_Write(PCD8544_SETXADDR | x, LCD_COMMAND);
+      for(int cx=0; cx<sx; cx++)
+        _LCD_Write(bitmap[cx+(cy*sx)], LCD_DATA);
+    }      
+    _LCD_Write(PCD8544_SETYADDR, LCD_COMMAND);
+    _LCD_Write(PCD8544_SETXADDR, LCD_COMMAND);
+  }
+}
+
+
+
+//x:84
+//y:48
+void setPixel(byte x, byte y, byte val)
+{
+  int idx = x + (y >> 3) * 84;
+  int shift = y & 7;
+  if (val == 0)
+  {
+    buff[idx] &= ~(1 << shift);
+  }
+  else
+  {
+    buff[idx] |= (1 << shift);
+  }
+}
+
+
+//x:84
+//y:0~6
+void setLine(byte x, byte y, byte val)
+{
+  int idx = x + y * 84;
+  buff[idx] = val;
+}
+
+
--- arduino/ds1302/code6/code6.ino
+++ arduino/ds1302/code6/code6.ino
@@ -1,43 +1,12 @@
-DS1302:00 2050-120-28 08:18:30
-
-DS1302:00 2050-120-28 08:18:08
-
-DS1302:00 2050-120-28 08:18:88
-
-DS1302:00 2050-120-28 08:18:88
-
-DS1302:00 2050-120-28 08:18:48
-
-DS1302:00 2050-120-28 08:18:48
-
-DS1302:00 2050-120-28 08:18:128
-
-DS1302:00 2050-120-28 08:18:128
-
-DS1302:00 2050-120-28 08:18:28
-
-DS1302:00 2050-120-28 08:18:28
-
-DS1302:00 2050-120-28 08:18:18
-
-DS1302:00 2050-120-28 08:18:18
-
-DS1302:00 2050-120-28 08:18:98
-
-DS1302:00 2050-120-28 08:18:98
-
-DS1302:00 2050-120-28 08:18:58
-
-
 
 struct Time {
-	uint8_t sec;
-	uint8_t min;
-	uint8_t hr;
-	uint8_t date;
-	uint8_t mon;
-	uint8_t day;
-	uint8_t yr;
+  uint8_t sec;
+  uint8_t min;
+  uint8_t hr;
+  uint8_t date;
+  uint8_t mon;
+  uint8_t day;
+  uint8_t yr;
 };
 
 void GetTime(Time* t);
@@ -63,152 +32,166 @@ void SetTime(Time* t);
 #define kClockBurstWrite 0xBE
 
 uint8_t bcdToDec(const uint8_t bcd) {
-	return (10 * ((bcd & 0xF0) >> 4) + (bcd & 0x0F));
+  return (10 * ((bcd & 0xF0) >> 4) + (bcd & 0x0F));
 }
 
 uint8_t decToBcd(const uint8_t dec) {
-	const uint8_t tens = dec / 10;
-	const uint8_t ones = dec % 10;
-	return (tens << 4) | ones;
+  const uint8_t tens = dec / 10;
+  const uint8_t ones = dec % 10;
+  return (tens << 4) | ones;
 }
 
 volatile int temp;
 void wait()
 {
-	for(int i=0;i<100;i++)
-	{
-		temp++;
-	}
+  for(int i=0;i<10;i++)
+  {
+    temp++;
+  }
 }
 
 void writeOut(const uint8_t value) {
-	DDR_IO |= BIT_IO;//pinMode(io_pin, OUTPUT);
-	wait();
-	uint8_t val = value;
-	for (uint8_t i = 0; i < 8; i++)  {
-		if(val & 1)//digitalWrite(io_pin, !!(value & (1 << i)));
-		{
-			PORT_IO |= BIT_IO;
-		}
-		else
-		{
-			PORT_IO &= ~BIT_IO;
-		}
-		val = val>>1;
-		wait();
-		PORT_CLK |= BIT_CLK;//digitalWrite(sclk_pin, HIGH);
-		wait();
-		PORT_CLK &= ~BIT_CLK;//digitalWrite(sclk_pin, LOW);
-		wait();
-	}
-	DDR_IO &= ~BIT_IO;
-	wait();
+  DDR_IO |= BIT_IO;//pinMode(io_pin, OUTPUT);
+  wait();
+  uint8_t val = value;
+  for (uint8_t i = 0; i < 8; i++)  {
+    if(val & 1)//digitalWrite(io_pin, !!(value & (1 << i)));
+    {
+      PORT_IO |= BIT_IO;
+    }
+    else
+    {
+      PORT_IO &= ~BIT_IO;
+    }
+    val = val>>1;
+    wait();
+    PORT_CLK |= BIT_CLK;//digitalWrite(sclk_pin, HIGH);
+    wait();
+    PORT_CLK &= ~BIT_CLK;//digitalWrite(sclk_pin, LOW);
+    wait();
+  }
+  DDR_IO &= ~BIT_IO;
+  wait();
 }
 
 uint8_t readIn() {
-	uint8_t input_value = 0;
-	wait();
-	for (uint8_t i = 0; i < 8; i++) {
-		if(PIN_IO & BIT_IO)//digitalWrite(io_pin, !!(value & (1 << i)));
-		{
-			input_value |= 1;  // Bits are read LSB first.
-		}
-		else
-		{
-			input_value |= 0;  // Bits are read LSB first.
-		}
-		input_value = input_value << 1;
-		wait();
-		PORT_CLK |= BIT_CLK;//digitalWrite(sclk_pin, HIGH);
-		wait();
-		PORT_CLK &= ~BIT_CLK;//digitalWrite(sclk_pin, LOW);
-		wait();
-	}
-	return input_value;
+  uint8_t input_value = 0;
+  wait();
+  if(PIN_IO & BIT_IO)//digitalWrite(io_pin, !!(value & (1 << i)));
+  {
+    input_value |= 1;  // Bits are read LSB first.
+  }
+  else
+  {
+    input_value |= 0;  // Bits are read LSB first.
+  }
+  input_value = input_value << 1;
+  wait();
+  PORT_CLK |= BIT_CLK;//digitalWrite(sclk_pin, HIGH);
+  wait();
+  PORT_CLK &= ~BIT_CLK;//digitalWrite(sclk_pin, LOW);
+  wait();
+  for (uint8_t i = 0; i < 7; i++) {
+    if(PIN_IO & BIT_IO)//digitalWrite(io_pin, !!(value & (1 << i)));
+    {
+      input_value |= 1;  // Bits are read LSB first.
+    }
+    else
+    {
+      input_value |= 0;  // Bits are read LSB first.
+    }
+    wait();
+    PORT_CLK |= BIT_CLK;//digitalWrite(sclk_pin, HIGH);
+    wait();
+    PORT_CLK &= ~BIT_CLK;//digitalWrite(sclk_pin, LOW);
+    wait();
+  }
+  return input_value;
 }
 
 void GetTime(Time* t) {
-	DDR_CLK |= BIT_CLK;//pinMode(sclk_pin, OUTPUT);
+  DDR_CLK |= BIT_CLK;//pinMode(sclk_pin, OUTPUT);
 
-	PORT_CLK &= ~BIT_CLK;//digitalWrite(sclk_pin, LOW);
-	PORT_CE |= BIT_CE;//digitalWrite(ce_pin, HIGH);
-	wait();//	delayMicroseconds(4);  // tCC
+  PORT_CLK &= ~BIT_CLK;//digitalWrite(sclk_pin, LOW);
+  PORT_CE |= BIT_CE;//digitalWrite(ce_pin, HIGH);
+  wait();//	delayMicroseconds(4);  // tCC
 
-	writeOut(kClockBurstRead);
-	t->sec = bcdToDec(readIn());
-	t->min = bcdToDec(readIn());
-	t->hr = bcdToDec(readIn());
-	t->date = bcdToDec(readIn());
-	t->mon = bcdToDec(readIn());
-	t->day = readIn();
-	t->yr = bcdToDec(readIn());
-	PORT_CE &= ~BIT_CE;//digitalWrite(ce_pin, LOW);
-	wait();
-	
-	DDR_CLK &= ~BIT_CLK;
+  writeOut(kClockBurstRead);
+  t->sec = bcdToDec(readIn());
+  t->min = bcdToDec(readIn());
+  t->hr = bcdToDec(readIn());
+  t->date = bcdToDec(readIn());
+  t->mon = bcdToDec(readIn());
+  t->day = readIn();
+  t->yr = bcdToDec(readIn());
+  PORT_CE &= ~BIT_CE;//digitalWrite(ce_pin, LOW);
+  wait();
+
+  DDR_CLK &= ~BIT_CLK;
 }
 
 void SetTime(Time* t) {
-	DDR_CLK |= BIT_CLK;//pinMode(sclk_pin, OUTPUT);
+  DDR_CLK |= BIT_CLK;//pinMode(sclk_pin, OUTPUT);
 
-	PORT_CLK &= ~BIT_CLK;//digitalWrite(sclk_pin, LOW);
-	PORT_CE |= BIT_CE;//digitalWrite(ce_pin, HIGH);
-	wait();//	delayMicroseconds(4);  // tCC
+  PORT_CLK &= ~BIT_CLK;//digitalWrite(sclk_pin, LOW);
+  PORT_CE |= BIT_CE;//digitalWrite(ce_pin, HIGH);
+  wait();//	delayMicroseconds(4);  // tCC
 
-	writeOut(kClockBurstWrite);
-	writeOut(decToBcd(t->sec));
-	writeOut(decToBcd(t->min));
-	writeOut(decToBcd(t->hr));
-	writeOut(decToBcd(t->date));
-	writeOut(decToBcd(t->mon));
-	writeOut(t->day);
-	writeOut(decToBcd(t->yr));
-	writeOut(0);  // Write protection register.
-	PORT_CE &= ~BIT_CE;//digitalWrite(ce_pin, LOW);
-	wait();//	delayMicroseconds(4);  // tCC
+  writeOut(kClockBurstWrite);
+  writeOut(decToBcd(t->sec));
+  writeOut(decToBcd(t->min));
+  writeOut(decToBcd(t->hr));
+  writeOut(decToBcd(t->date));
+  writeOut(decToBcd(t->mon));
+  writeOut(t->day);
+  writeOut(decToBcd(t->yr));
+  writeOut(0);  // Write protection register.
+  PORT_CE &= ~BIT_CE;//digitalWrite(ce_pin, LOW);
+  wait();//	delayMicroseconds(4);  // tCC
 }
 
 
 void setup() {
-	DDR_CE |= BIT_CE;
-	PORT_CE &= ~BIT_CE;
-	delay(5000);
-	Serial.begin(9600);
-	Serial.setTimeout(1000);
-	pinMode(13, OUTPUT);
+  DDR_CE |= BIT_CE;
+  PORT_CE &= ~BIT_CE;
+  delay(5000);
+  Serial.begin(9600);
+  Serial.setTimeout(1000);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-	digitalWrite(13, HIGH);
-	Time t;
+  digitalWrite(13, HIGH);
+  Time t;
 
-	GetTime(&t);
+  GetTime(&t);
 
-	char buf[60];
-	snprintf(buf, sizeof(buf), "DS1302:%02d 20%02d-%02d-%02d %02d:%02d:%02d",t.day, t.yr, t.mon, t.date, t.hr, t.min, t.sec);
+  char buf[60];
+  snprintf(buf, sizeof(buf), "DS1302:%02d 20%02d-%02d-%02d %02d:%02d:%02d",t.day, t.yr, t.mon, t.date, t.hr, t.min, t.sec);
 
-	// Print the formatted string to serial so we can see the time.
-	Serial.println(buf);
+  // Print the formatted string to serial so we can see the time.
+  Serial.println(buf);
 
-	//delay(1000);
+  //delay(1000);
 
-	digitalWrite(13, LOW);
-	int start = Serial.parseInt();
-	if(start == 33)
-	{
-		t.yr  = Serial.parseInt();
-		t.mon = Serial.parseInt();
-		t.date = Serial.parseInt();
-		t.hr  = Serial.parseInt();
-		t.min = Serial.parseInt();
-		t.sec = Serial.parseInt();
-		t.day= Serial.parseInt();
+  digitalWrite(13, LOW);
+  int start = Serial.parseInt();
+  if(start == 33)
+  {
+    t.yr  = Serial.parseInt();
+    t.mon = Serial.parseInt();
+    t.date = Serial.parseInt();
+    t.hr  = Serial.parseInt();
+    t.min = Serial.parseInt();
+    t.sec = Serial.parseInt();
+    t.day= Serial.parseInt();
 
 
-		SetTime(&t);
-	}
+    SetTime(&t);
+  }
 }
 //33,15,6,26,20,39,00,5,
+
 
 
 
