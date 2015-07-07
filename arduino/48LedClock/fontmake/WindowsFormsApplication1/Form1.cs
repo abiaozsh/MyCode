2e24f300ce8f35b92e2dfd9e9ff6c8eb5b0ac3a5
abiaozsh <abiaozsh@sina.cn>
2015-07-07 19:31:44 +0800

    1

--- arduino/48LedClock/fontmake/WindowsFormsApplication1/Form1.cs
+++ arduino/48LedClock/fontmake/WindowsFormsApplication1/Form1.cs
@@ -18,10 +18,11 @@ namespace WindowsFormsApplication1
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
-
+            aa();
+            bb();
 		}
 
-		private void timer1_Tick(object sender, EventArgs e)
+		private void aa()
 		{
 			Bitmap bmp = new Bitmap(@"..\..\..\..\font.png");
 			StringBuilder sb = new StringBuilder();
@@ -57,5 +58,57 @@ namespace WindowsFormsApplication1
 			}
 			textBox1.Text = sb.ToString();
 		}
-	}
+
+        private void bb()
+        {
+            Bitmap bmp = new Bitmap(@"..\..\..\..\bitmap.png");
+            StringBuilder sb = new StringBuilder();
+
+                for (int j = 0; j < 48; j++)
+                {
+                    string sbyte0 = "";
+                    string sbyte1 = "";
+                    string sbyte2 = "";
+                    string sbyte3 = "";
+                    string sbyte4 = "";
+                    string sbyte5 = "";
+                    int byte0 = 0;
+                    int byte1 = 0;
+                    int byte2 = 0;
+                    int byte3 = 0;
+                    int byte4 = 0;
+                    int byte5 = 0;
+                    for (int k = 0; k < 8; k++)
+                    {
+                        int val0 = bmp.GetPixel(j, k).R > 128 ? 0 : 1;
+                        sbyte0 = val0 + sbyte0;
+                        byte0 |= val0 << k;
+                        int val1 = bmp.GetPixel(j, k + 8).R > 128 ? 0 : 1;
+                        sbyte1 = val1 + sbyte1;
+                        byte1 |= val1 << k;
+                        int val2 = bmp.GetPixel(j, k + 16).R > 128 ? 0 : 1;
+                        sbyte2 = val2 + sbyte2;
+                        byte2 |= val2 << k;
+                        int val3 = bmp.GetPixel(j, k + 24).R > 128 ? 0 : 1;
+                        sbyte3 = val3 + sbyte3;
+                        byte3 |= val3 << k;
+                        int val4 = bmp.GetPixel(j, k + 32).R > 128 ? 0 : 1;
+                        sbyte4 = val4 + sbyte4;
+                        byte4 |= val4 << k;
+                        int val5 = bmp.GetPixel(j, k + 40).R > 128 ? 0 : 1;
+                        sbyte5 = val5 + sbyte5;
+                        byte5 |= val5 << k;
+                    }
+                    sb.Append("//" + sbyte0 + "," + sbyte1 + "," + sbyte2 + "," + sbyte3 + "," + sbyte4 + "," + sbyte5 + "\r\n");
+                    sb.Append(string.Format("0x{0:X2}", byte0) + ",");
+                    sb.Append(string.Format("0x{0:X2}", byte1) + ",");
+                    sb.Append(string.Format("0x{0:X2}", byte2) + ",");
+                    sb.Append(string.Format("0x{0:X2}", byte3) + ",");
+                    sb.Append(string.Format("0x{0:X2}", byte4) + ",");
+                    sb.Append(string.Format("0x{0:X2}", byte5) + ",");
+                    sb.Append("\r\n");
+                }
+            textBox2.Text = sb.ToString();
+        }
+    }
 }
--- arduino/SoftSerialmega328p/build328p.bat
+++ arduino/SoftSerialmega328p/build328p.bat
@@ -5,7 +5,7 @@ del serial.s
 del a.out
 set arduinopath=C:\Program Files (x86)\Arduino
 rem set arduinopath=C:\Program Files\Arduino
-set comPort=COM5
+set comPort=COM30
 rem "%arduinopath%\hardware\tools\avr\bin\avr-g++" -Os -mmcu=atmega328p -save-temps -fverbose-asm -I"%arduinopath%\hardware\arduino\cores\arduino" serial.cpp
 "%arduinopath%\hardware\tools\avr\bin\avr-g++" -Os -mmcu=atmega328p                           -I"%arduinopath%\hardware\arduino\cores\arduino" serial.cpp -o serial.o
 "%arduinopath%\hardware\tools\avr\bin\avr-objcopy" -O ihex serial.o serial.hex
