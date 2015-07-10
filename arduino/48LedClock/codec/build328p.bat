abc43cbf89c4b40b03f96612049d75360d65b750
shzheng <shzheng@Ctrip.com>
2015-07-10 17:57:52 +0800

    1

--- arduino/48LedClock/codec/build328p.bat
+++ arduino/48LedClock/codec/build328p.bat
@@ -9,6 +9,7 @@ set comPort=COM30
 rem "%arduinopath%\hardware\tools\avr\bin\avr-g++" -Os -mmcu=atmega328p -save-temps -fverbose-asm -I"%arduinopath%\hardware\arduino\cores\arduino" serial.cpp
 "%arduinopath%\hardware\tools\avr\bin\avr-g++" -Os -mmcu=atmega328p                           -I"%arduinopath%\hardware\arduino\cores\arduino" serial.cpp -o serial.o
 "%arduinopath%\hardware\tools\avr\bin\avr-objcopy" -O ihex serial.o serial.hex
+"%arduinopath%\hardware\tools\avr\bin\avr-size" serial.hex
 pause
 "%arduinopath%\hardware\tools\avr\bin\avrdude" -C"%arduinopath%\hardware\tools\avr\etc\avrdude.conf" -patmega328p -carduino -P\\.\%comPort% -b57600 -D -Uflash:w:serial.hex:i 
 pause
