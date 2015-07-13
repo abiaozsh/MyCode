177555254400fdd9db515f2a3e741798dfce629c
shzheng <shzheng@Ctrip.com>
2015-07-13 17:22:56 +0800

    1

--- arduino/Clock/buildt24.bat
+++ arduino/Clock/buildt24.bat
@@ -1,14 +1,16 @@
-del serial.hex
-del serial.ii
-del serial.o
-del serial.s
-del a.out
-rem set arduinopath=C:\Program Files (x86)\Arduino
-set arduinopath=C:\Program Files\Arduino
+set arduinopath=C:\Program Files (x86)\Arduino
+rem set arduinopath=C:\Program Files\Arduino
 set comPort=COM5
-"%arduinopath%\hardware\tools\avr\bin\avr-g++" -help
-"%arduinopath%\hardware\tools\avr\bin\avr-g++" -Os -mmcu=attiny24 -save-temps -fverbose-asm -I"%arduinopath%\hardware\arduino\cores\arduino" serial.cpp
-"%arduinopath%\hardware\tools\avr\bin\avr-g++" -Os -mmcu=attiny24                           -I"%arduinopath%\hardware\arduino\cores\arduino" serial.cpp -o serial.o
-"%arduinopath%\hardware\tools\avr\bin\avr-objcopy" -O ihex serial.o serial.hex
-"%arduinopath%\hardware\tools\avr\bin\avrdude" -C"%arduinopath%\hardware\tools\avr\etc\avrdude.conf" -pattiny24 -cstk500v1 -P\\.\%comPort% -b19200 -Uflash:w:serial.hex:i 
+set filename=serial
+
+del %filename%.hex
+del %filename%.ii
+del %filename%.o
+del %filename%.s
+del a.out
+"%arduinopath%\hardware\tools\avr\bin\avr-g++" -Os -mmcu=attiny24 -save-temps -fverbose-asm -I"%arduinopath%\hardware\arduino\cores\arduino" %filename%.cpp
+"%arduinopath%\hardware\tools\avr\bin\avr-g++" -Os -mmcu=attiny24                           -I"%arduinopath%\hardware\arduino\cores\arduino" %filename%.cpp -o %filename%.o
+"%arduinopath%\hardware\tools\avr\bin\avr-objcopy" -O ihex %filename%.o %filename%.hex
+"%arduinopath%\hardware\tools\avr\bin\avrdude" -C"%arduinopath%\hardware\tools\avr\etc\avrdude.conf" -pattiny24 -cstk500v1 -P\\.\%comPort% -b19200 -Uflash:w:%filename%.hex:i 
+"%arduinopath%\hardware\tools\avr\bin\avr-size" %filename%.hex
 pause
--- arduino/Clock/serial.cpp
+++ arduino/Clock/serial.cpp
@@ -1,60 +1,154 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
-	
-#define CUR_TIMING TIMING__8M_TCCR1B_1_115200
-#define TCCR1B_Value 1
 
-PROGMEM prog_uint16_t TIMING__8M_TCCR1B_3____300[] = {  416,  833, 1250, 1666, 2083, 2500, 2916, 3333, 3750, 4166};
-PROGMEM prog_uint16_t TIMING__8M_TCCR1B_2___1200[] = {  833, 1666, 2500, 3333, 4166, 5000, 5833, 6666, 7500, 8333};
-PROGMEM prog_uint16_t TIMING__8M_TCCR1B_2___2400[] = {  416,  833, 1250, 1666, 2083, 2500, 2916, 3333, 3750, 4166};
-PROGMEM prog_uint16_t TIMING__8M_TCCR1B_2___4800[] = {  208,  416,  625,  833, 1041, 1250, 1458, 1666, 1875, 2083};
-PROGMEM prog_uint16_t TIMING__8M_TCCR1B_1___9600[] = {  833, 1666, 2500, 3333, 4166, 5000, 5833, 6666, 7500, 8333};
-PROGMEM prog_uint16_t TIMING__8M_TCCR1B_1__14400[] = {  555, 1111, 1666, 2222, 2777, 3333, 3888, 4444, 5000, 5555};
-PROGMEM prog_uint16_t TIMING__8M_TCCR1B_1__19200[] = {  416,  833, 1250, 1666, 2083, 2500, 2916, 3333, 3750, 4166};
-PROGMEM prog_uint16_t TIMING__8M_TCCR1B_1__28800[] = {  277,  555,  833, 1111, 1388, 1666, 1944, 2222, 2500, 2777};
-PROGMEM prog_uint16_t TIMING__8M_TCCR1B_1__38400[] = {  208,  416,  625,  833, 1041, 1250, 1458, 1666, 1875, 2083};
-PROGMEM prog_uint16_t TIMING__8M_TCCR1B_1__57600[] = {  138,  277,  416,  555,  694,  833,  972, 1111, 1250, 1388};
-PROGMEM prog_uint16_t TIMING__8M_TCCR1B_1_115200[] = {   69,  138,  208,  277,  347,  416,  486,  555,  625,  694};
+/*
+   0
+1     2
+   3
+4     5
+   6
+data0 clk1 st2 oe3      4A
+clk0 data1 (scl0 sda1)  2B
+alarm2                  1B
+adj 4 5                 2A // 调整月日时分
+light sense 6           1A
+LED                     1A
+11
 
-uint8_t days = 0;
-uint8_t hour = 0;
-uint8_t miniute = 0;
-uint8_t second = 0;
-uint16_t tick = 0;//15625 tick/s
+1data                   1
+clk st oe               3
+(clk) data rst(sda scl) 2
+alarm                   1
+adj                     2
+light sense             1
+time/date               1
 
-uint8_t figure0;//days
-uint8_t figure1;//hours
-uint8_t figure2;
-uint8_t figure3;
-uint8_t figure4;
-uint8_t figure5;
-uint8_t figure6;
-uint8_t dimeSeconds=0;
+LED 10
+LED 420 （10+41）
+PNP 6 NPN 6
+NPN 7
+左上高，右下低
+
+*/
+
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+#define PORT_DAT_ON  PORTA |=  _BV(0)
+#define PORT_DAT_OFF PORTA &= ~_BV(0)
+
+#define PORT_CLK_ON  PORTA |=  _BV(1)
+#define PORT_CLK_OFF PORTA &= ~_BV(1)
+
+#define PORT_STR_ON  PORTA |=  _BV(2)
+#define PORT_STR_OFF PORTA &= ~_BV(2)
+
+#define PORT_OE_ON   PORTA |=  _BV(3)
+#define PORT_OE_OFF  PORTA &= ~_BV(3)
+
+#define PORT_IDX_ON  PORTA |=  _BV(7)
+#define PORT_IDX_OFF PORTA &= ~_BV(7)
+
+#define NUM_ADJ1 4
+#define NUM_ADJ2 5
+#define NUM_SENS 6
+
+#define PORT_ALM_ON  PORTB |=  _BV(2)
+#define PORT_ALM_OFF PORTB &= ~_BV(2)
+
+#define DS1307_CTRL_ID 0x68//B01101000  //DS1307
+
+//scl0 sda1
+#define BIT_SCL  _BV(0)
+#define DDR_SCL  DDRB
+#define PORT_SCL PORTB
+
+#define BIT_SDA  _BV(1)
+#define DDR_SDA  DDRB
+#define PORT_SDA PORTB
+#define PIN_SDA  PINB
+
+#define I2C_ACK 1
+#define I2C_NAK 0
+#define i2c_scl_release() DDR_SCL &= ~BIT_SCL
+#define i2c_sda_release() DDR_SDA &= ~BIT_SDA
+// sets SCL low and drives output
+#define i2c_scl_lo() PORT_SCL &= ~BIT_SCL; DDR_SCL |= BIT_SCL;
+// sets SDA low and drives output
+#define i2c_sda_lo() PORT_SDA &= ~BIT_SDA; DDR_SDA |= BIT_SDA;
+// set SCL high and to input (releases pin) (i.e. change to input,turnon pullup)
+#define i2c_scl_hi() DDR_SCL &=~ BIT_SCL;
+// set SDA high and to input (releases pin) (i.e. change to input,turnon pullup)
+#define i2c_sda_hi() DDR_SDA &=~ BIT_SDA;
 
 
-#define DDR_Send DDRA
-#define PORT_Send PORTA
-#define BIT_Send _BV(2)
-#define DDR_Recv DDRA
-#define PIN_Recv PINA
-#define BIT_Recv _BV(1)
+PROGMEM prog_uint8_t SEGS[] = {
+0x77,//0=01110111=
+0x12,//1=00010010=
+0x5D,//2=01011101=
+0x5B,//3=01011011=
+0x3A,//4=00111010=
+0x6B,//5=01101011=
+0x6F,//6=01101111=
+0x52,//7=01010010=
+0x7F,//8=01111111=
+0x7B,//9=01111011=
+};
 
 void ClockInit();
+
 void loop();
-void Timer0Init();
-void Timer1Init();
-void SerialInit();
-void SerialSend(uint8_t val);
-void IncTimer();
+void Init();
+void Page(uint8_t* vals);
+void SendWord(uint8_t data);
+
+void dly();
+void i2c_SoftI2CMaster();
+uint8_t i2c_beginTransmission(uint8_t address);
+uint8_t i2c_requestFrom(uint8_t address);
+uint8_t i2c_endTransmission(void);
+void i2c_write(uint8_t* data, uint8_t quantity);
+void i2c_writebit(uint8_t c);
+uint8_t i2c_readbit(void);
+void i2c_start(void);
+void i2c_stop(void);
+uint8_t i2c_write(uint8_t c);
+uint8_t i2c_read(uint8_t ack);
+uint8_t i2c_read();
+uint8_t i2c_readLast();
+
+uint8_t bcdToDec(const uint8_t bcd);
+uint8_t decToBcd(const uint8_t dec);
+void DS1307_read();
+void DS1307_save();
+
+int analogRead(uint8_t pin);
+
+
+uint8_t DS1307_SEC;// 0
+uint8_t DS1307_MIN;// 1
+uint8_t DS1307_HR;// 2
+uint8_t DS1307_DOW;// 3
+uint8_t DS1307_DATE;// 4
+uint8_t DS1307_MTH;// 5
+uint8_t DS1307_YR;// 6
+
 
 int main(void) {
 	ClockInit();
-	DDRA = _BV(0);//pin0
-	SerialInit();
-	Timer0Init();
-	Timer1Init();
+	DDRA|=_BV(0);
+	DDRA|=_BV(1);
+	DDRA|=_BV(2);
+	DDRA|=_BV(3);
+	
 	loop();
+	
+	//
+	analogRead(1);
+	DS1307_read();
+	DS1307_save();
 }
 
 void ClockInit() {
@@ -63,115 +157,291 @@ void ClockInit() {
 	CLKPR = 0;//1/1 //8MHz
 }
 
-uint8_t lastSecond = 0;
-
 void loop() {
 	for(;;)
 	{
-		// put your main code here, to run repeatedly: 
-		if(lastSecond!=second)
-		{
-			lastSecond=second;
-			PINA=_BV(0);
-		}
-		//delay
-		for(uint16_t i=0;i<10000;i++)
-		{
-			IncTimer();
-		}
+		uint8_t temp[6];
+		temp[0] = 8;
+		temp[1] = 8;
+		temp[2] = 8;
+		temp[3] = 8;
+		temp[4] = 8;
+		temp[5] = 8;
 		
-		IncTimer();SerialSend('0'+days);
-		IncTimer();SerialSend(',');
-		IncTimer();SerialSend('0'+figure1);
-		IncTimer();SerialSend('0'+figure2);
-		IncTimer();SerialSend(':');
-		IncTimer();SerialSend('0'+figure3);
-		IncTimer();SerialSend('0'+figure4);
-		IncTimer();SerialSend(':');
-		IncTimer();SerialSend('0'+figure5);
-		IncTimer();SerialSend('0'+figure6);
-		IncTimer();SerialSend('.');
-		IncTimer();SerialSend('0'+dimeSeconds);
-		IncTimer();SerialSend('\r');
-		IncTimer();SerialSend('\n');
+		Init();
+		Page(temp);
 	}
 }
 
-
-//0.016384 秒
-void Timer0Init() {
-	TCCR0A = 0;
-	TCCR0B = 5;
-	TIMSK0 = 0;
-}
-
-void Timer1Init() {
-	TCCR1A = 0;
-	TCCR1C = 0;
-	TIMSK1 = 0;
-}
-
-void SerialInit(){
-	//UCSR0B = 0;//not forget turnoff usart0 on mega328p
-	DDR_Send |= BIT_Send;
-	DDR_Recv &= ~BIT_Recv;
-	PORT_Send |= BIT_Send;
-}
-
-void SerialSend(uint8_t val)
-{
-	cli();
-	TCCR1B = TCCR1B_Value;
-	TCNT1 = 0;
-	uint16_t timing;
-	prog_uint16_t* pTiming = CUR_TIMING;
-	PORT_Send &= ~BIT_Send;timing = pgm_read_word_near(pTiming++);while(TCNT1<timing);//startbit
-	uint8_t chkbit = 0x01;
-	for(uint8_t i = 8 ; i > 0 ; i--)
+void Init(){
+	for(uint8_t i=0;i<16;i++)
 	{
-		if(val&chkbit){PORT_Send |= BIT_Send;}else{PORT_Send &= ~BIT_Send;}chkbit<<=1;timing = pgm_read_word_near(pTiming++);while(TCNT1<timing);
+		PORT_DAT_OFF;
+		PORT_CLK_ON; //shift clock up
+		PORT_CLK_OFF; //shift clock down
 	}
-	PORT_Send |= BIT_Send;timing = pgm_read_word_near(pTiming);while(TCNT1<timing);
-	sei();
+	PORT_STR_ON; //store clock up
+	PORT_STR_OFF; //store clock down
+	PORT_OE_ON;
 }
 
-uint8_t lastTick = 0;//15625 tick/s
-void IncTimer()
-{
-	uint8_t buffTick = TCNT0;
-	uint8_t diffTick = buffTick - lastTick;
-	lastTick = buffTick;
-	tick+=diffTick;
-	if(tick>=15625)
+void Page(uint8_t* vals){
+	for(uint8_t w=0;w<6;w++)
 	{
-		tick -= 15625;
-		second+=2;
-		if(second==60)
+		for(uint8_t l=0;l<7;l++)
 		{
-			second = 0;
-			miniute++;
-			if(miniute==60)
-			{
-				miniute = 0;
-				hour++;
-				if(hour==24)
-				{
-					hour=0;
-					days++;
-				}
-			}
+			uint8_t t = pgm_read_byte_near(SEGS+vals[w]);
+			SendWord(t&(1<<l));//位线
+			SendWord(1<<w);//字线
+			PORT_STR_ON; //store clock up
+			PORT_STR_OFF; //store clock down
 		}
 	}
+}
+void SendWord(uint8_t data){
+	PORT_CLK_OFF;
+	PORT_STR_OFF;
+	for(uint8_t i=0;i<8;i++)
+	{
+		if(data&0x80)
+		{
+			PORT_DAT_ON;
+		}
+		else
+		{
+			PORT_DAT_OFF;
+		}
+		data<<1;
+		PORT_CLK_ON; //shift clock up
+		PORT_CLK_OFF; //shift clock down
+	}
+}
+int analogRead(uint8_t pin){
+	uint8_t low, high;
 
-	figure1 = hour / 10;
-	figure2 = hour % 10;
+	ADCSRB |= _BV(ADLAR);
+  
+	ADMUX = (pin & 0x07);
 
-	figure3 = miniute / 10;
-	figure4 = miniute % 10;
+	// without a delay, we seem to read from the wrong channel
+	//delay(1);
 
-	figure5 = second / 10;
-	figure6 = second % 10;
+	// start the conversion
+	sbi(ADCSRA, ADSC);
 
-	uint32_t temp = tick;
-	dimeSeconds = (temp*10737)>>24;//0~9
+	// ADSC is cleared when the conversion finishes
+	while (bit_is_set(ADCSRA, ADSC));
+
+	// we have to read ADCL first; doing so locks both ADCL
+	// and ADCH until ADCH is read.  reading ADCL second would
+	// cause the results of each conversion to be discarded,
+	// as ADCL and ADCH would be locked when it completed.
+	low  = ADCL;
+	high = ADCH;
+
+	
+	// combine the two bytes
+	return (high << 8) | low;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void dly(){
+  for(uint8_t i=0;i<200;i++)
+  {
+    volatile uint8_t v=0;
+    v++;
+  }
+}
+void i2c_SoftI2CMaster(){
+  i2c_sda_hi();
+  i2c_scl_hi();
+  dly();
+}
+uint8_t i2c_beginTransmission(uint8_t address){
+  i2c_start();
+  uint8_t rc = i2c_write((address<<1) | 0); // clr read bit
+  return rc;
+}
+uint8_t i2c_requestFrom(uint8_t address){
+  i2c_start();
+  uint8_t rc = i2c_write((address<<1) | 1); // set read bit
+  return rc;
+}
+uint8_t i2c_endTransmission(void){
+  i2c_stop();
+  //return ret; // FIXME
+  return 0;
+}
+void i2c_write(uint8_t* data, uint8_t quantity){
+  for(uint8_t i = 0; i < quantity; ++i){
+    i2c_write(data[i]);
+  }
+}
+void i2c_writebit(uint8_t c){
+  if ( c > 0 ) {
+    i2c_sda_hi();
+  } 
+  else {
+    i2c_sda_lo();
+  }
+  i2c_scl_hi();
+  dly();
+  i2c_scl_lo();
+  dly();
+  if ( c > 0 ) {
+    i2c_sda_lo();
+  }
+  dly();
+}
+uint8_t i2c_readbit(void){
+  i2c_sda_hi();
+  i2c_scl_hi();
+  dly();
+  uint8_t c = PIN_SDA; // I2C_PIN;
+  i2c_scl_lo();
+  dly();
+  return ( c & BIT_SDA) ? 1 : 0;
+}
+void i2c_start(void){
+  // set both to high at the same time
+  i2c_sda_hi();
+  i2c_scl_hi();
+  dly();
+  i2c_sda_lo();
+  dly();
+  i2c_scl_lo();
+  dly();
+}
+void i2c_stop(void){
+  i2c_scl_hi();
+  dly();
+  i2c_sda_hi();
+  dly();
+}
+uint8_t i2c_write(uint8_t c){
+  for ( uint8_t i=0;i<8;i++) {
+    i2c_writebit( c & 128 );
+    c<<=1;
+  }
+  return i2c_readbit();
+}
+uint8_t i2c_read(uint8_t ack){
+  uint8_t res = 0;
+  for ( uint8_t i=0;i<8;i++) {
+    res <<= 1;
+    res |= i2c_readbit();
+  }
+  if ( ack )
+    i2c_writebit( 0 );
+  else
+    i2c_writebit( 1 );
+  dly();
+  return res;
+}
+uint8_t i2c_read(){
+  return i2c_read( I2C_ACK );
+}
+uint8_t i2c_readLast(){
+  return i2c_read( I2C_NAK );
+}
+
+
+
+
+
+
+
+
+uint8_t bcdToDec(const uint8_t bcd) {
+  return (10 * ((bcd & 0xF0) >> 4) + (bcd & 0x0F));
+}
+uint8_t decToBcd(const uint8_t dec) {
+  const uint8_t tens = dec / 10;
+  const uint8_t ones = dec % 10;
+  return (tens << 4) | ones;
+}
+
+// Aquire data from the RTC chip in BCD format
+// refresh the buffer
+void DS1307_read(){
+  // use the Wire lib to connect to tho rtc
+  // reset the resgiter pointer to zero
+  i2c_beginTransmission(DS1307_CTRL_ID);
+  i2c_write(0x00);
+  i2c_endTransmission();
+
+  // request the 7 bytes of data    (secs, min, hr, dow, date. mth, yr)
+  i2c_requestFrom(DS1307_CTRL_ID);
+  // store data in raw bcd format
+  DS1307_SEC=bcdToDec(i2c_read());// 0
+  DS1307_MIN=bcdToDec(i2c_read());// 1
+  DS1307_HR=bcdToDec(i2c_read());// 2
+  DS1307_DOW=i2c_read();// 3
+  DS1307_DATE=bcdToDec(i2c_read());// 4
+  DS1307_MTH=bcdToDec(i2c_read());// 5
+  DS1307_YR=bcdToDec(i2c_readLast());// 6
+}
+
+// update the data on the IC from the bcd formatted data in the buffer
+void DS1307_save(){
+  i2c_beginTransmission(DS1307_CTRL_ID);
+  i2c_write(0x00); // reset register pointer
+  i2c_write(decToBcd(DS1307_SEC));
+  i2c_write(decToBcd(DS1307_MIN));
+  i2c_write(decToBcd(DS1307_HR));
+  i2c_write(DS1307_DOW);
+  i2c_write(decToBcd(DS1307_DATE));
+  i2c_write(decToBcd(DS1307_MTH));
+  i2c_write(decToBcd(DS1307_YR));
+  i2c_endTransmission();
+}
+
+
+void setup()
+{
+  //delay(1000);
+  //Serial.begin(9600);
+  //Serial.setTimeout(1000);
+  //i2c_SoftI2CMaster();
+  //pinMode(13, OUTPUT);
+}
+
+void loopa()
+{
+  //digitalWrite(13, HIGH);
+  //DS1307_read();
+  //char buf[60];
+  //snprintf(buf, sizeof(buf), "DS1307:%02d 20%02d-%02d-%02d %02d:%02d:%02d",DS1307_DOW, DS1307_YR, DS1307_MTH, DS1307_DATE, DS1307_HR, DS1307_MIN, DS1307_SEC);
+  //Serial.println(buf);
+  //if(DS1307_SEC!=80)
+  //{
+  //  digitalWrite(13, LOW);
+  //}
+  //int start = Serial.parseInt();
+  //if(start == 33)
+  //{
+  //  DS1307_YR  = Serial.parseInt();
+  //  DS1307_MTH = Serial.parseInt();
+  //  DS1307_DATE = Serial.parseInt();
+  //  DS1307_HR  = Serial.parseInt();
+  //  DS1307_MIN = Serial.parseInt();
+  //  DS1307_SEC = Serial.parseInt();
+  //  DS1307_DOW= Serial.parseInt();
+  //
+  //  DS1307_save();
+  //}
 }
