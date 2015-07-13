6df2a8ca97d2b44b8772599884a8b8406a8f1175
shzheng <shzheng@Ctrip.com>
2015-07-13 17:22:44 +0800

    1

---
+++ arduino/Clock/analogTest/buildt24.bat
@@ -0,0 +1,16 @@
+set arduinopath=C:\Program Files (x86)\Arduino
+rem set arduinopath=C:\Program Files\Arduino
+set comPort=COM5
+set filename=serial
+
+del %filename%.hex
+del %filename%.ii
+del %filename%.o
+del %filename%.s
+del a.out
+"%arduinopath%\hardware\tools\avr\bin\avr-g++" -Os -mmcu=attiny24 -save-temps -fverbose-asm -I"%arduinopath%\hardware\arduino\cores\arduino" %filename%.cpp
+"%arduinopath%\hardware\tools\avr\bin\avr-g++" -Os -mmcu=attiny24                           -I"%arduinopath%\hardware\arduino\cores\arduino" %filename%.cpp -o %filename%.o
+"%arduinopath%\hardware\tools\avr\bin\avr-objcopy" -O ihex %filename%.o %filename%.hex
+"%arduinopath%\hardware\tools\avr\bin\avrdude" -C"%arduinopath%\hardware\tools\avr\etc\avrdude.conf" -pattiny24 -cstk500v1 -P\\.\%comPort% -b19200 -Uflash:w:%filename%.hex:i 
+"%arduinopath%\hardware\tools\avr\bin\avr-size" %filename%.hex
+pause
---
+++ arduino/Clock/analogTest/serial.cpp
@@ -0,0 +1,174 @@
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+#define CUR_TIMING TIMING__8M_TCCR0B_1_115200
+#define TCCR0B_Value 1
+
+PROGMEM prog_uint8_t TIMING__8M_TCCR0B_2___9600[] = {104,208, 56,160,  8,113,217, 65,169, 17};
+PROGMEM prog_uint8_t TIMING__8M_TCCR0B_2__14400[] = { 69,138,208, 21, 91,160,230, 43,113,182};
+PROGMEM prog_uint8_t TIMING__8M_TCCR0B_1_115200[] = { 69,138,208, 21, 91,160,230, 43,113,182};
+
+#define DDR_Send DDRA
+#define PORT_Send PORTA
+#define BIT_Send _BV(2)
+#define DDR_Recv DDRA
+#define PIN_Recv PINA
+#define BIT_Recv _BV(1)
+
+void ClockInit();
+void loop();
+void TimerInit();
+void SerialInit();
+void SerialSend(uint8_t val);
+void SendInt(uint32_t val);
+
+
+int main(void) {
+	ClockInit();
+	
+	ADCSRB |= _BV(ADLAR);
+	loop();
+}
+
+void ClockInit() {
+	CLKPR = 128;//The CLKPCE bit must be written to logic one to enable change of the CLKPS bits. The CLKPCE bit is only updated when the other bits in CLKPR are simultaniosly written to zero.
+	//CLKPR = 3;//1/8
+	CLKPR = 0;//1/1 //8MHz
+}
+
+void loop() {
+	uint8_t val3;
+	uint8_t val7;
+	for(;;)
+	{
+		val7 = ADCH;
+		ADMUX = 3;
+		sbi(ADCSRA, ADSC);
+		TCNT1=0;
+		while(TCNT1<2000);
+		SerialSend('v');
+		SerialSend('7');
+		SerialSend(':');
+		SendInt(val7);
+		SerialSend('\r');
+		SerialSend('\n');
+		
+		val3 = ADCH;
+		ADMUX = 7;
+		sbi(ADCSRA, ADSC);
+		TCNT1=0;
+		while(TCNT1<2000);
+		SerialSend('v');
+		SerialSend('3');
+		SerialSend(':');
+		SendInt(val3);
+		SerialSend('\r');
+		SerialSend('\n');
+	}
+}
+
+void TimerInit() {
+	TCCR0A = 0;
+	TIMSK0 = 0;
+	
+	TCCR1A = 0;
+	TCCR1B = 5;
+	TCCR1C = 0;
+	TIMSK1 = 0;
+}
+
+void SerialInit(){
+	//UCSR0B = 0;//not forget turnoff usart0 on mega328p
+	DDR_Send |= BIT_Send;
+	DDR_Recv &= ~BIT_Recv;
+	PORT_Send |= BIT_Send;
+}
+
+int analogRead(uint8_t pin){
+	uint8_t low, high;
+
+	ADCSRB |= _BV(ADLAR);
+  
+	ADMUX = (pin & 0x07);
+
+	// without a delay, we seem to read from the wrong channel
+	//delay(1);
+
+	// start the conversion
+	sbi(ADCSRA, ADSC);
+
+	// ADSC is cleared when the conversion finishes
+	while (bit_is_set(ADCSRA, ADSC));
+
+	// we have to read ADCL first; doing so locks both ADCL
+	// and ADCH until ADCH is read.  reading ADCL second would
+	// cause the results of each conversion to be discarded,
+	// as ADCL and ADCH would be locked when it completed.
+	low  = ADCL;
+	high = ADCH;
+
+	
+	// combine the two bytes
+	return (high << 8) | low;
+}
+
+
+
+
+PROGMEM prog_uint32_t num10s[] = {
+1000000000,
+100000000,
+10000000,
+1000000,
+100000,
+10000,
+1000,
+100,
+10,
+1,
+};
+
+void SendInt(uint32_t val)
+{
+	uint32_t num = val;
+	for(uint8_t idx = 0; idx < 10 ; idx++)
+	{
+		uint8_t outNum = 0;
+		uint32_t CmpNum = pgm_read_dword_near(num10s + idx);
+		for(uint8_t i = 0; i < 10 ; i++)
+		{
+			if(num>=CmpNum)
+			{
+				num -= CmpNum;
+				outNum++;
+			}
+			else
+			{
+				break;
+			}
+		}
+		SerialSend('0' + outNum);
+	}
+}
+
+void SerialSend(uint8_t val)
+{
+	cli();
+	TCCR0B = TCCR0B_Value;
+	TCNT0 = 0;
+	uint8_t timing;
+	prog_uint8_t* pTiming = CUR_TIMING;
+	PORT_Send &= ~BIT_Send;timing = pgm_read_byte_near(pTiming++);while(timing-TCNT0>0);//startbit
+	uint8_t chkbit = 0x01;
+	for(uint8_t i = 8 ; i > 0 ; i--)
+	{
+		if(val&chkbit){PORT_Send |= BIT_Send;}else{PORT_Send &= ~BIT_Send;}chkbit<<=1;timing = pgm_read_byte_near(pTiming++);while(timing-TCNT0>0);
+	}
+	PORT_Send |= BIT_Send;timing = pgm_read_byte_near(pTiming);while(timing-TCNT0>0);
+	sei();
+}
