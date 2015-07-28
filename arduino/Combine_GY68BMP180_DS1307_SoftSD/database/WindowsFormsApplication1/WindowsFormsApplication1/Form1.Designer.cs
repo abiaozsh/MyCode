129a5c5af6da4a5c23d33584f98633fbc93d4308
abiaozsh <abiaozsh@sina.cn>
2015-07-28 12:40:37 +0800

    1

---
+++ arduino/Combine_GY68BMP180_DS1307_SoftSD/database/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WindowsFormsApplication1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 40);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(13, 13);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 21);
+			this.textBox1.TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(119, 13);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 21);
+			this.textBox2.TabIndex = 1;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(13, 69);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(1000, 300);
+			this.pictureBox1.TabIndex = 2;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+			this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+			this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(1019, 83);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(75, 23);
+			this.button6.TabIndex = 8;
+			this.button6.Text = "s";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(1019, 331);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(75, 23);
+			this.button8.TabIndex = 10;
+			this.button8.Text = "s";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.button8_Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(225, 13);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(229, 21);
+			this.textBox3.TabIndex = 11;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(460, 12);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(229, 21);
+			this.textBox4.TabIndex = 11;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1221, 457);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseWheel);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+	}
+}
+
---
+++ arduino/Combine_GY68BMP180_DS1307_SoftSD/database/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		public struct DATAS
+		{
+			public DateTime time;
+			public int temp;
+			public int pres;
+		}
+
+		static int w = 1000;
+		static int h = 300;
+		double x = w / 2;
+		double y = h / 2;
+		double sx = 0.05;
+		double sy = 0.5;
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			textBox1.Text = DateTime.Today.AddDays(-1).ToString("yyyy-MM-dd");
+			textBox2.Text = DateTime.Today.ToString("yyyy-MM-dd");
+			textBox1.Text = ("2015-07-24");
+			textBox2.Text = ("2015-07-27");
+		}
+
+		private void button8_Click(object sender, EventArgs e)
+		{
+			sy *= 1.1; draw();
+		}
+		private void button6_Click(object sender, EventArgs e)
+		{
+			sy *= 0.9; draw();
+		}
+
+		List<DATAS> datas = new List<DATAS>();
+		DateTime?[] dates;
+		int?[] values;
+		static Color[] clist = { Color.Red, Color.Blue, Color.Green, Color.Purple, Color.Pink, Color.Yellow };
+		void draw()
+		{
+			Bitmap bmp = new Bitmap(w, h);
+			dates = new DateTime?[w];
+			values = new int?[w];
+			//bmp.SetPixel(100,100,Color.Red);
+			var starttime = DateTime.MaxValue;
+			var startval = int.MaxValue;
+			var endtime = DateTime.MinValue;
+			var endval = int.MinValue;
+			double Scale = 5;
+
+			foreach (var data in datas)
+			{
+				if (startval > data.pres)
+				{
+					startval = data.pres;
+				}
+				if (starttime > data.time)
+				{
+					starttime = data.time;
+				}
+				if (endval < data.pres)
+				{
+					endval = data.pres;
+				}
+				if (endtime < data.time)
+				{
+					endtime = data.time;
+				}
+			}
+			var starttime0 = starttime;
+			starttime = DateTime.FromBinary(starttime.ToBinary() / 2 + endtime.ToBinary() / 2);
+			startval = startval / 2 + endval / 2;
+			foreach (var data in datas)
+			{
+				double xxx = ((data.time.Ticks - starttime.Ticks) / 10 / 1000 / 1000) / Scale;
+				double yyy = (startval - data.pres) / 10f;
+
+				xxx *= sx;
+				yyy *= sy;
+				xxx += x;
+				yyy += y;
+
+				if (xxx >= 0 && xxx < w && yyy >= 0 && yyy < h)
+				{
+					bmp.SetPixel((int)xxx, (int)yyy, ((data.time.Date.Subtract(starttime0.Date).Days & 1) == 1) ? Color.Red : Color.Blue);
+				}
+				if (xxx >= 0 && xxx < w)
+				{
+					dates[(int)xxx] = data.time;
+					values[(int)xxx] = data.pres;
+				}
+			}
+
+			pictureBox1.Image = bmp;
+		}
+		void Form1_MouseWheel(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			if (e.Delta > 0)
+			{
+				sx *= 1.1;
+				sy *= 1.1;
+			}
+			else
+			{
+				sx *= 0.9;
+				sy *= 0.9;
+			}
+			draw();
+		}
+
+		int tx;
+		int ty;
+		void pictureBox1_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			x += (e.X - tx);
+			y += (e.Y - ty);
+			draw();
+		}
+
+		void pictureBox1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			tx = e.X;
+			ty = e.Y;
+		}
+
+		void pictureBox1_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+		{
+			if (dates != null && dates[e.X] != null)
+			{
+				textBox3.Text = dates[e.X].Value.ToString("yyyy-MM-dd HH:mm:ss");
+				textBox4.Text = values[e.X].Value.ToString();
+			}
+		}
+
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			datas = new List<DATAS>();
+			for (DateTime d = DateTime.Parse(textBox1.Text); d <= DateTime.Parse(textBox2.Text); d = d.AddDays(1))
+			{
+				string name = @"..\..\..\..\" + d.ToString("yyyyMMdd") + ".txt";
+				try
+				{
+					FileStream fs = new FileStream(name, FileMode.Open, FileAccess.Read);
+					StreamReader sr = new StreamReader(fs);
+					while (true)
+					{
+						string line = sr.ReadLine();
+						if (line == null) break;
+						if (line.Contains(":"))
+						{
+							DateTime t = DateTime.Parse(d.ToString("yyyy-MM-dd") + " " + line);
+							string line0 = sr.ReadLine();
+							int temp = int.Parse(line0.Split(',')[0]);
+							int pres = int.Parse(line0.Split(',')[1]);
+							int totalT = temp;
+							int totalP = pres;
+							for (int i = 1; i < 50; i++)
+							{
+								string linen = sr.ReadLine();
+								int tempn = temp + int.Parse(linen.Split(',')[0]);
+								int presn = pres + int.Parse(linen.Split(',')[1]);
+								totalT += tempn;
+								totalP += presn;
+							}
+							DATAS ds = new DATAS();
+							ds.time = t;
+							ds.pres = totalP / 5;
+							ds.temp = totalT / 5;
+							datas.Add(ds);
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+				}
+			}
+			draw();
+		}
+
+	}
+}
