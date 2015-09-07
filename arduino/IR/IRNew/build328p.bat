7165f8276d96ba5fbd465947cd0333cc1adeb66b
abiaozsh <abiaozsh@sina.cn>
2015-09-07 10:12:48 +0800

    1

---
+++ arduino/IR/IRNew/build328p.bat
@@ -0,0 +1,18 @@
+set arduinopath=C:\Program Files (x86)\Arduino
+rem set arduinopath=C:\Program Files\Arduino
+set comPort=COM30
+set filename=ir
+set ext=cpp
+
+del %filename%.hex
+del %filename%.ii
+del %filename%.o
+del %filename%.s
+del a.out
+"%arduinopath%\hardware\tools\avr\bin\avr-g++" -Os -mmcu=atmega328p -save-temps -fverbose-asm -I"%arduinopath%\hardware\arduino\cores\arduino" %filename%.%ext%
+"%arduinopath%\hardware\tools\avr\bin\avr-g++" -Os -mmcu=atmega328p                           -I"%arduinopath%\hardware\arduino\cores\arduino" %filename%.%ext% -o %filename%.o
+"%arduinopath%\hardware\tools\avr\bin\avr-objcopy" -O ihex %filename%.o %filename%.hex
+"%arduinopath%\hardware\tools\avr\bin\avr-size" %filename%.hex
+pause
+"%arduinopath%\hardware\tools\avr\bin\avrdude" -C"%arduinopath%\hardware\tools\avr\etc\avrdude.conf" -patmega328p -carduino -P\\.\%comPort% -b57600 -D -Uflash:w:%filename%.hex:i 
+pause
