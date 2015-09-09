34a07bb89ef6980f2625a435f2e6b25ea0414333
abiaozsh <abiaozsh@sina.cn>
2015-09-09 10:05:10 +0800

    1

---
+++ arduino/PompDriver/buildt24.bat
@@ -0,0 +1,18 @@
+@echo off
+rem set arduinopath=C:\Program Files (x86)\Arduino
+set arduinopath=C:\Program Files\Arduino
+set comPort=COM5
+set filename=timer
+
+del %filename%.hex
+del %filename%.i
+del %filename%.o
+del %filename%.s
+del a.out
+"%arduinopath%\hardware\tools\avr\bin\avr-gcc" -Os -mmcu=attiny24 -fno-inline -save-temps -fverbose-asm -I"%arduinopath%\hardware\arduino\cores\arduino" %filename%.c
+"%arduinopath%\hardware\tools\avr\bin\avr-gcc" -Os -mmcu=attiny24 -fno-inline                           -I"%arduinopath%\hardware\arduino\cores\arduino" %filename%.c -o %filename%.o
+"%arduinopath%\hardware\tools\avr\bin\avr-objcopy" -O ihex %filename%.o %filename%.hex
+rem "%arduinopath%\hardware\tools\avr\bin\avrdude" -C"%arduinopath%\hardware\tools\avr\etc\avrdude.conf" -pattiny24 -cstk500v1 -P\\.\%comPort% -b19200 -Uflash:w:%filename%.hex:i 
+"%arduinopath%\Uploader.exe" %comPort% %filename%.hex
+"%arduinopath%\hardware\tools\avr\bin\avr-size" %filename%.hex
+pause
