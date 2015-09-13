89fa24e4b4a97dfd39a42236895f5cda91b360bd
abiao <abiaozsh@sina.cn>
2015-09-13 19:53:34 +0800

    1

--- arduino/Lamp/DoorLamp/lamp.c
+++ arduino/Lamp/DoorLamp/lamp.c
@@ -2,31 +2,86 @@
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 
+#define currTick ((TIFR1 & _BV(TOV1))?0x0FFFF:TCNT1)
+
 void ClockInit();
 
 int main(void) {
 	ClockInit();
+	sei();
 
 	DDRA = 0;PORTA = 0;//all input
 	DDRB = 0;PORTB = 0;//all input
-
-	DDRA |= _BV(0);
 	
-	TCCR0A = 0;
-	TCCR0B = 3;
-	TIMSK0 = _BV(TOIE1);
-	
-	sei();
 	
 	PRR = _BV(PRADC) | _BV(PRUSI);
   MCUCR |= _BV(SE);
+  MCUCR |= _BV(SM1);
   ACSR |= _BV(ACD);
+
+  WDTCSR |= _BV(WDCE);
+ //WDP3 WDP2 WDP1 WDP0
+//Number of WDT Oscillator
+//Cycles
+//Typical Time-out at
+//VCC = 5.0V
+//0 0 0 0 2K cycles 16 ms
+//0 0 0 1 4K cycles 32 ms
+//WDTCSR |= _BV(WDP1);//0 0 1 0 8K cycles 64 ms
+//WDTCSR |= _BV(WDP1);WDTCSR |= _BV(WDP0);//0 0 1 1 16K cycles 0.125 s
+//0 1 0 0 32K cycles 0.25 s
+WDTCSR |= _BV(WDP2);WDTCSR |= _BV(WDP0);//0 1 0 1 64K cycles 0.5 s
+//0 1 1 0 128K cycles 1.0 s
+//0 1 1 1 256K cycles 2.0 s
+//1 0 0 0 512K cycles 4.0 s
+//1 0 0 1 1024K cycles 8.0 s
+ 
   
-	//翋悜遠
+  
+  //WDE WDIE Watchdog Timer State Action on Time-out
+//0 0 Stopped None
+WDTCSR |= _BV(WDIE);//0 1 Running Interrupt
+//1 0 Running Reset
+//1 1 Running Interrupt  
+
+
 	while(1)
 	{
-		asm volatile("sleep");
+	DDRA |= _BV(0);
+	//DDRA |= _BV(1);
+	
+	PORTA |= _BV(0);
+  // COM1A1 COM1A0 COM1B1 COM1B0 每 每 WGM11 WGM10
+  TCCR1A = 0;
+  //ICNC1 ICES1 每 WGM13 WGM12 CS12 CS11 CS10
+  //0 0 1 clkI/O/1 (No prescaling)
+  //0 1 0 clkI/O/8 (From prescaler)
+  //0 1 1 clkI/O/64 (From prescaler)
+  //1 0 0 clkI/O/256 (From prescaler)
+  //1 0 1 clkI/O/1024 (From prescaler)
+
+  //TCCR1B = 1;//  1/1
+  TCCR1B = 2;//  1/8
+  //TCCR1B = 3;//  1/64
+
+  //FOC1A FOC1B 每 每 每 每 每 每
+  TCCR1C = 0;
+  //每 每 ICIE1 每 每 OCIE1B OCIE1A TOIE1
+  TIMSK1 = 0;
+		TCNT1 = 0;//timer reset
+	TIFR1 |= _BV(TOV1);//overflow flg reset
+	while(currTick<10)
+	{
+		;
 	}
+
+	
+	PORTA &= ~_BV(0);
+	
+	DDRA &= ~_BV(0);
+	//DDRA &= ~_BV(1);
+	asm volatile("sleep");
+  }
 }
 
 void ClockInit() {
@@ -35,9 +90,7 @@ void ClockInit() {
 	CLKPR = _BV(CLKPS3);//1 0 0 0 1/256  31.25khz
 }
 
-ISR(TIM0_OVF_vect){
-	PINA |= _BV(0);
-}
+ISR(WDT_vect){return;}
 
 /*
 ISR(INT0_vect){return;}
