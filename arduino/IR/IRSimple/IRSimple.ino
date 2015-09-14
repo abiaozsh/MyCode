bfb25eebe1c902fcc7b2b4e3718d6ffd00ea955d
abiaozsh <abiaozsh@sina.cn>
2015-09-14 13:12:56 +0800

    1

--- arduino/IR/IRSimple/IRSimple.ino
+++ arduino/IR/IRSimple/IRSimple.ino
@@ -1,25 +1,45 @@
+#define IRPIN PIND & _BV(3)
+
 uint8_t data[32];
 
-while(IRPIN);//等待低电平 下降沿
-//9ms低电平
-while(!IRPIN);//等待高电平 上升沿
-//4.5ms高电平
-for (j=0;j<4;j++)         //收集四组数据
-{ 
-  for (k=0;k<8;k++)        //每组数据有8位
-  {
-    while(IRPIN);//等待低电平 下降沿
-    //0.56ms低电平
-    while(!IRPIN);//等待高电平 上升沿
-    //?ms高电平
-    uint8_t cnt = 0;
-    while(IRPIN)
+void Setup()
+{
+  Serial.Begin(9600);
+}
+
+void Loop()
+{
+  while(IRPIN);//等待低电平 下降沿
+  //9ms低电平
+  while(!IRPIN);//等待高电平 上升沿
+  //4.5ms高电平
+  for (int j=0;j<4;j++)         //收集四组数据
+  { 
+    for (int k=0;k<8;k++)        //每组数据有8位
     {
-      delay14();
-      cnt++;
-      if(cnt>100){break;}
+      while(IRPIN);//等待低电平 下降沿
+      //0.56ms低电平
+      while(!IRPIN);//等待高电平 上升沿
+      //?ms高电平
+      uint8_t cnt = 0;
+      while(IRPIN)
+      {
+        delay14();
+        cnt++;
+        if(cnt>100){break;}
+      }
+      data[j*8+k]=cnt;
     }
-    data[j*8+k]=cnt;
+  }
+  
+  for (int j=0;j<4;j++)         //收集四组数据
+  { 
+    for (int k=0;k<8;k++)        //每组数据有8位
+    {
+      Serial.print(data[j*8+k]);
+      Serial.print(",");
+    }
+    Serial.println();
   }
 }
 
--- arduino/Lamp/DoorLamp/lamp.c
+++ arduino/Lamp/DoorLamp/lamp.c
@@ -2,51 +2,100 @@
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 
-//2 daylight power out
-//3 switch power out
-//4 daylight analog in
-//5 switch in
-//B0 lamp out
+//1 daylight analog in
+//2 switch digital in
+//3 daylight power out
+//4 switch power out
+//B0 lamp out1
+//B1 lamp out2
 
-#define PowerOn DDRA |= _BV(3)
-#define PowerOn DDRA |= _BV(3)
+
+
+
+
+
+
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+
+#define currTick ((TIFR1 & _BV(TOV1))?0x0FFFF:TCNT1)
 
 void ClockInit();
+uint16_t ARead(uint8_t pin);
 
 int main(void) {
-  ClockInit();
+	ClockInit();
+	sei();
 
-  DDRA |= _BV(0);
-  
-  TCCR0A = 0;
-  TCCR0B = 3;
-  TIMSK0 = _BV(TOIE1);
-  
-  sei();
-  
-  PRR = _BV(PRADC) | _BV(PRUSI);
+	DDRA = 0;PORTA = 0;//all input
+	DDRB = 0;PORTB = 0;//all input
+	
+	uint8_t IsDaylight = 0;
+  uint8_t DaylightCount = 0;
+	PRR = _BV(PRADC) | _BV(PRUSI);
   MCUCR |= _BV(SE);
+  MCUCR |= _BV(SM1);
   ACSR |= _BV(ACD);
-  
-  //翋悜遠
+  WDTCSR |= _BV(WDCE);
+  WDTCSR |= _BV(WDP1);//0 0 1 0 8K cycles 0.0625 s
+  WDTCSR |= _BV(WDIE);//0 1 Running Interrupt
+
   while(1)
   {
+    DaylightCount++;
+    if(DaylightCount==32)//2s check daylight
+    {
+      DDRA |= _BV(3);
+      uint16_t val = ARead(1);
+      DDRA &= ~_BV(3);
+      if(val>512)
+      {
+        IsDaylight = 1;
+      }
+      else
+      {
+        IsDaylight = 0;
+      }
+      DaylightCount = 0;
+    }
+    if(IsDaylight)
+    {
+      DDRA |= _BV(4);
+      uint8_t doorOpen = PINA & _BV(2);
+      DDRA &= ~_BV(4);
+      if(doorOpen)
+      {
+        DDRB |= _BV(0);
+        DDRB |= _BV(1);
+
+        PORTA |= _BV(0);
+        TCCR1A = 0;
+        TCCR1B = 2;//  1/8
+        TCCR1C = 0;
+        TIMSK1 = 0;
+        TCNT1 = 0;//timer reset
+        TIFR1 |= _BV(TOV1);//overflow flg reset
+        while(currTick<10)
+        {
+          ;
+        }
+        PORTA &= ~_BV(0);
+
+        
+        
+        DDRB &= ~_BV(0);
+        DDRB &= ~_BV(1);
+      }
+    }
     asm volatile("sleep");
   }
 }
-
-uint16_t daylightCount = 0;
 
 void ClockInit() {
 	CLKPR = _BV(CLKPCE);//The CLKPCE bit must be written to logic one to enable change of the CLKPS bits. The CLKPCE bit is only updated when the other bits in CLKPR are simultaniosly written to zero.
 	//CLKPR = 0;//8Mhz
 	CLKPR = _BV(CLKPS3);//1 0 0 0 1/256  31.25khz
-}
-
-ISR(TIM0_OVF_vect){
-  daylightCount++;
-  if(daylightCount)
-	PINA |= _BV(0);
 }
 
 uint16_t ARead(uint8_t pin)
@@ -60,115 +109,6 @@ uint16_t ARead(uint8_t pin)
 
   ADCSRA &= ~_BV(ADEN);
 	return ADC;
-}
-
-/*
-ISR(INT0_vect){return;}
-ISR(PCINT0_vect){return;}
-ISR(PCINT1_vect){return;}
-ISR(WDT_vect){return;}
-ISR(TIMER1_CAPT_vect){return;}
-ISR(TIM1_COMPA_vect){return;}
-ISR(TIM1_COMPB_vect){return;}
-ISR(TIM1_OVF_vect){return;}
-ISR(TIM0_COMPA_vect){return;}
-ISR(TIM0_COMPB_vect){return;}
-ISR(TIM0_OVF_vect){return;}
-ISR(ANA_COMP_vect){return;}
-ISR(ADC_vect){return;}
-ISR(EE_RDY_vect){return;}
-ISR(USI_START_vect){return;}
-ISR(USI_OVF_vect){return;}
-*/#include <avr/io.h>
-#include <avr/pgmspace.h>
-#include <avr/interrupt.h>
-
-#define currTick ((TIFR1 & _BV(TOV1))?0x0FFFF:TCNT1)
-
-void ClockInit();
-
-int main(void) {
-	ClockInit();
-	sei();
-
-	DDRA = 0;PORTA = 0;//all input
-	DDRB = 0;PORTB = 0;//all input
-	
-	
-	PRR = _BV(PRADC) | _BV(PRUSI);
-  MCUCR |= _BV(SE);
-  MCUCR |= _BV(SM1);
-  ACSR |= _BV(ACD);
-
-  WDTCSR |= _BV(WDCE);
- //WDP3 WDP2 WDP1 WDP0
-//Number of WDT Oscillator
-//Cycles
-//Typical Time-out at
-//VCC = 5.0V
-//0 0 0 0 2K cycles 16 ms
-//0 0 0 1 4K cycles 32 ms
-//WDTCSR |= _BV(WDP1);//0 0 1 0 8K cycles 64 ms
-//WDTCSR |= _BV(WDP1);WDTCSR |= _BV(WDP0);//0 0 1 1 16K cycles 0.125 s
-//0 1 0 0 32K cycles 0.25 s
-WDTCSR |= _BV(WDP2);WDTCSR |= _BV(WDP0);//0 1 0 1 64K cycles 0.5 s
-//0 1 1 0 128K cycles 1.0 s
-//0 1 1 1 256K cycles 2.0 s
-//1 0 0 0 512K cycles 4.0 s
-//1 0 0 1 1024K cycles 8.0 s
- 
-  
-  
-  //WDE WDIE Watchdog Timer State Action on Time-out
-//0 0 Stopped None
-WDTCSR |= _BV(WDIE);//0 1 Running Interrupt
-//1 0 Running Reset
-//1 1 Running Interrupt  
-
-
-	while(1)
-	{
-	DDRA |= _BV(0);
-	//DDRA |= _BV(1);
-	
-	PORTA |= _BV(0);
-  // COM1A1 COM1A0 COM1B1 COM1B0 每 每 WGM11 WGM10
-  TCCR1A = 0;
-  //ICNC1 ICES1 每 WGM13 WGM12 CS12 CS11 CS10
-  //0 0 1 clkI/O/1 (No prescaling)
-  //0 1 0 clkI/O/8 (From prescaler)
-  //0 1 1 clkI/O/64 (From prescaler)
-  //1 0 0 clkI/O/256 (From prescaler)
-  //1 0 1 clkI/O/1024 (From prescaler)
-
-  //TCCR1B = 1;//  1/1
-  TCCR1B = 2;//  1/8
-  //TCCR1B = 3;//  1/64
-
-  //FOC1A FOC1B 每 每 每 每 每 每
-  TCCR1C = 0;
-  //每 每 ICIE1 每 每 OCIE1B OCIE1A TOIE1
-  TIMSK1 = 0;
-		TCNT1 = 0;//timer reset
-	TIFR1 |= _BV(TOV1);//overflow flg reset
-	while(currTick<10)
-	{
-		;
-	}
-
-	
-	PORTA &= ~_BV(0);
-	
-	DDRA &= ~_BV(0);
-	//DDRA &= ~_BV(1);
-	asm volatile("sleep");
-  }
-}
-
-void ClockInit() {
-	CLKPR = _BV(CLKPCE);//The CLKPCE bit must be written to logic one to enable change of the CLKPS bits. The CLKPCE bit is only updated when the other bits in CLKPR are simultaniosly written to zero.
-	//CLKPR = 0;//8Mhz
-	CLKPR = _BV(CLKPS3);//1 0 0 0 1/256  31.25khz
 }
 
 ISR(WDT_vect){return;}
