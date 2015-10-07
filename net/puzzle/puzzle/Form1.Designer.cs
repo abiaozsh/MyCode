be69f73c2dbb46a74d936ddf55f0986dfa4397ed
abiao <abiaozsh@sina.cn>
2015-10-07 11:32:47 +0800

    1

--- net/puzzle/puzzle/Form1.Designer.cs
+++ net/puzzle/puzzle/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -65,18 +66,163 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(234, 40);
+            this.textBox2.Location = new System.Drawing.Point(13, 288);
             this.textBox2.MaxLength = 0;
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(312, 413);
+            this.textBox2.Size = new System.Drawing.Size(200, 179);
             this.textBox2.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "0000000181000000\t0\t2\t4\t2\t",
+            "0000184000000000\t0\t2\t3\t4\t",
+            "0000000001808000\t0\t2\t4\t1\t",
+            "000000000000C020\t0\t2\t4\t0\t",
+            "0000000000006010\t0\t2\t3\t0\t",
+            "0000000000C04000\t0\t2\t3\t1\t",
+            "00000000C0800000\t0\t2\t3\t2\t",
+            "0000006080000000\t0\t2\t3\t3\t",
+            "0000000000180400\t0\t3\t0\t1\t",
+            "0000000006020000\t0\t2\t6\t1\t",
+            "0000010C00000000\t0\t0\t0\t4\t",
+            "0000000000000C01\t0\t3\t0\t0\t",
+            "0000000418000000\t0\t0\t0\t3\t",
+            "0000000000018040\t0\t2\t5\t0\t",
+            "000000C100000000\t0\t2\t4\t3\t",
+            "0000000302000000\t0\t2\t5\t2\t",
+            "0000000003010000\t0\t2\t5\t1\t",
+            "0000000030200000\t0\t2\t1\t2\t",
+            "0000000000301000\t0\t2\t1\t1\t",
+            "0000061000000000\t0\t2\t1\t4\t",
+            "0000001820000000\t0\t2\t1\t3\t",
+            "0002600000000000\t0\t0\t0\t6\t",
+            "0000620000000000\t0\t2\t0\t5\t",
+            "0000000000001804\t0\t2\t1\t0\t",
+            "0000230000000000\t0\t0\t0\t5\t",
+            "0000003040000000\t0\t2\t2\t3\t",
+            "0000000060400000\t0\t2\t2\t2\t",
+            "0001880000000000\t0\t2\t2\t5\t",
+            "00000C2000000000\t0\t2\t2\t4\t",
+            "000C800000000000\t0\t2\t1\t6\t",
+            "0000C40000000000\t0\t2\t1\t5\t",
+            "0000000000602000\t0\t2\t2\t1\t",
+            "0000000000003008\t0\t2\t2\t0\t",
+            "0000000000000403\t0\t0\t0\t0\t",
+            "0000006040000000\t0\t3\t3\t3\t",
+            "00000000C0400000\t0\t3\t3\t2\t",
+            "000000000000C010\t0\t3\t4\t0\t",
+            "0000182000000000\t0\t3\t3\t4\t",
+            "0001840000000000\t0\t3\t2\t5\t",
+            "00000C1000000000\t0\t3\t2\t4\t",
+            "0000000000C02000\t0\t3\t3\t1\t",
+            "0000000000006008\t0\t3\t3\t0\t",
+            "0000000301000000\t0\t3\t5\t2\t",
+            "0000000003008000\t0\t3\t5\t1\t",
+            "0000000006010000\t0\t3\t6\t1\t",
+            "0000000000080C00\t0\t0\t0\t1\t",
+            "0000000180800000\t0\t3\t4\t2\t",
+            "0000000001804000\t0\t3\t4\t1\t",
+            "0000000000018020\t0\t3\t5\t0\t",
+            "000000C080000000\t0\t3\t4\t3\t",
+            "0000003020000000\t0\t3\t2\t3\t",
+            "0000000008180000\t0\t0\t0\t2\t",
+            "0006200000000000\t0\t3\t0\t6\t",
+            "0000000000300800\t0\t3\t1\t1\t",
+            "0000000000001802\t0\t3\t1\t0\t",
+            "0000000C08000000\t0\t3\t0\t3\t",
+            "0000000018080000\t0\t3\t0\t2\t",
+            "0000610000000000\t0\t3\t0\t5\t",
+            "0000030400000000\t0\t3\t0\t4\t",
+            "0000000000003004\t0\t3\t2\t0\t",
+            "000C400000000000\t0\t3\t1\t6\t",
+            "0000000060200000\t0\t3\t2\t2\t",
+            "0000000000601000\t0\t3\t2\t1\t",
+            "0000001810000000\t0\t3\t1\t3\t",
+            "0000000030100000\t0\t3\t1\t2\t",
+            "0000C20000000000\t0\t3\t1\t5\t",
+            "0000060800000000\t0\t3\t1\t4\t",
+            "0000000206000000\t0\t0\t6\t2\t",
+            "0000021800000000\t0\t0\t1\t4\t",
+            "0000000004060000\t0\t0\t7\t1\t",
+            "0000000830000000\t0\t0\t1\t3\t",
+            "0000460000000000\t0\t0\t1\t5\t",
+            "0000000001018000\t0\t0\t5\t1\t",
+            "00000000000100C0\t0\t0\t6\t0\t",
+            "0000008300000000\t0\t0\t5\t3\t",
+            "0000020C00000000\t0\t1\t0\t4\t",
+            "0000000818000000\t0\t1\t0\t3\t",
+            "0000000010300000\t0\t0\t1\t2\t",
+            "0000430000000000\t0\t1\t0\t5\t",
+            "0000000000000803\t0\t1\t0\t0\t",
+            "0000000000040300\t0\t0\t8\t0\t",
+            "0000000010180000\t0\t1\t0\t2\t",
+            "0000000000100C00\t0\t1\t0\t1\t",
+            "0000000000008060\t0\t0\t5\t0\t",
+            "0009800000000000\t0\t0\t2\t6\t",
+            "000000000000100C\t0\t0\t2\t0\t",
+            "0000000000406000\t0\t0\t3\t1\t",
+            "0000000000002018\t0\t0\t3\t0\t",
+            "0000000020600000\t0\t0\t2\t2\t",
+            "0000000000203000\t0\t0\t2\t1\t",
+            "0000043000000000\t0\t0\t2\t4\t",
+            "0000001060000000\t0\t0\t2\t3\t",
+            "000000000080C000\t0\t0\t4\t1\t",
+            "0000000000004030\t0\t0\t4\t0\t",
+            "000010C000000000\t0\t0\t4\t4\t",
+            "0000000081800000\t0\t0\t4\t2\t",
+            "00000020C0000000\t0\t0\t3\t3\t",
+            "0000000040C00000\t0\t0\t3\t2\t",
+            "0001180000000000\t0\t0\t3\t5\t",
+            "002C000000000000\t0\t0\t1\t7\t",
+            "0000008180000000\t0\t1\t4\t3\t",
+            "0000000101800000\t0\t1\t4\t2\t",
+            "0000000002018000\t0\t1\t5\t1\t",
+            "0000000000010060\t0\t1\t5\t0\t",
+            "0000106000000000\t0\t1\t3\t4\t",
+            "00000040C0000000\t0\t1\t3\t3\t",
+            "000000000100C000\t0\t1\t4\t1\t",
+            "0000000000008030\t0\t1\t4\t0\t",
+            "0000000018100000\t0\t2\t0\t2\t",
+            "0000000000180800\t0\t2\t0\t1\t",
+            "0000030800000000\t0\t2\t0\t4\t",
+            "0000000C10000000\t0\t2\t0\t3\t",
+            "0000000000000806\t0\t0\t1\t0\t",
+            "0000000203000000\t0\t1\t5\t2\t",
+            "0070000000000000\t0\t0\t0\t8\t",
+            "0000000004030000\t0\t1\t6\t1\t",
+            "0000000080C00000\t0\t1\t3\t2\t",
+            "0000041800000000\t0\t1\t1\t4\t",
+            "0000001030000000\t0\t1\t1\t3\t",
+            "0008C00000000000\t0\t1\t1\t6\t",
+            "0000860000000000\t0\t1\t1\t5\t",
+            "0000000000001006\t0\t1\t1\t0\t",
+            "0000000000101800\t0\t0\t1\t1\t",
+            "0000000020300000\t0\t1\t1\t2\t",
+            "0000000000201800\t0\t1\t1\t1\t",
+            "00010C0000000000\t0\t1\t2\t5\t",
+            "0000083000000000\t0\t1\t2\t4\t",
+            "0000000000806000\t0\t1\t3\t1\t",
+            "0000000000004018\t0\t1\t3\t0\t",
+            "0000000000403000\t0\t1\t2\t1\t",
+            "000000000000200C\t0\t1\t2\t0\t",
+            "0000002060000000\t0\t1\t2\t3\t",
+            "0000000040600000\t0\t1\t2\t2\t"});
+            this.listBox1.Location = new System.Drawing.Point(234, 39);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(392, 412);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(579, 479);
+            this.ClientSize = new System.Drawing.Size(662, 479);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -96,6 +242,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
