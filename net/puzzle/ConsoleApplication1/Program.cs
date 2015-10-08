ec39f59f32b139702e5f66addf3def5cfed0270e
abiaozsh <abiaozsh@sina.cn>
2015-10-08 16:37:39 +0800

    1

---
+++ net/puzzle/ConsoleApplication1/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static List<ulong[]> Data;
+
+		static void init()
+		{
+			Data = new List<ulong[]>();
+
+			Data.Add(proc(CData.d0));
+			Data.Add(proc(CData.d1));
+			Data.Add(proc(CData.d2));
+			Data.Add(proc(CData.d3));
+			Data.Add(proc(CData.d4));
+			Data.Add(proc(CData.d5));
+			Data.Add(proc(CData.d6));
+			Data.Add(proc(CData.d7));
+			Data.Add(proc(CData.d8));
+			Data.Add(proc(CData.d9));
+			Data.Add(proc(CData.d10));
+			Data.Add(proc(CData.d11));
+
+		}
+		static ulong[] proc(string s)
+		{
+			int cnt = 0;
+			foreach (string s1 in s.Split('\n'))
+			{
+				string[] s2 = s1.Split(',');
+				if (s2[4] != "x")
+				{
+					cnt++;
+				}
+			}
+			ulong[] dat = new ulong[cnt];
+			cnt = 0;
+			foreach (string s1 in s.Split('\n'))
+			{
+				string[] s2 = s1.Split(',');
+				if (s2[4] != "x")
+				{
+					dat[cnt++] = Convert.ToUInt64("0x" + (s2[0].Substring(1)), 16);
+				}
+			}
+
+			return dat;
+		}
+
+
+		static void Main(string[] args)
+		{
+			init();
+			FileStream fs = new FileStream(args[2], FileMode.Append, FileAccess.Write);
+			StreamWriter sw = new StreamWriter(fs);
+
+			int[] Idx = new int[12];
+			{
+				string s = args[0];
+				for (int i = 0; i < 12; i++)
+				{
+					Idx[i] = int.Parse(s.Split(',')[i]);
+				}
+			}
+			int[] Idxend = new int[12];
+			{
+				string s = args[1];
+				for (int i = 0; i < 12; i++)
+				{
+					Idxend[i] = int.Parse(s.Split(',')[i]);
+				}
+			}
+
+			while (true)
+			{
+				for (int times = 0; times < 100000000; times++)
+				{
+					ulong board = 0;
+					int last = 0;
+					int error = 0;
+					for (int i = 0; i < 12; i++)
+					{
+						ulong mask = Data[i][Idx[i]];
+						if ((board & mask) == 0)
+						{
+							board |= mask;
+						}
+						else
+						{
+							last = i;
+							error = 1;
+							break;
+						}
+					}
+					if (error != 0)
+					{
+						if (last < 11)
+						{
+							Idx[last + 1] = 0;
+						}
+						for (int i = last; i >= 0; i--)
+						{
+							Idx[i]++;
+							if (Idx[i] >= Data[i].Length)
+							{
+								Idx[i] = 0;
+							}
+							else
+							{
+								break;
+							}
+						}
+					}
+					else
+					{
+						string s = "";
+						for (int i = 0; i < 12; i++)
+						{
+							s += Idx[i] + ",";
+						}
+						Console.WriteLine("bingo:" + s);
+						sw.WriteLine(s);
+						sw.Flush();
+						fs.Flush();
+						for (int i = 11; i >= 0; i--)
+						{
+							Idx[i]++;
+							if (Idx[i] >= Data[i].Length)
+							{
+								Idx[i] = 0;
+							}
+							else
+							{
+								break;
+							}
+						}
+					}
+				}
+				{
+					string s = "";
+					for (int i = 0; i < 12; i++)
+					{
+						s += Idx[i] + ",";
+					}
+					Console.WriteLine(s);
+				}
+				bool isEnd = true;
+				for (int i = 0; i < 12; i++)
+				{
+					if (Idx[i] < Idxend[i])
+					{
+						isEnd = false;
+						break;
+					}
+					else if (Idx[i] > Idxend[i])
+					{
+						isEnd = true;
+						break;
+					}
+				}
+				if (isEnd)
+				{
+					return;
+				}
+			}
+
+		}
+	}
+}
--- net/puzzle/puzzle/Form1.Designer.cs
+++ net/puzzle/puzzle/Form1.Designer.cs
@@ -28,56 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
-            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(234, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(312, 21);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(27, 249);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(13, 288);
-            this.textBox2.MaxLength = 0;
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(200, 179);
-            this.textBox2.TabIndex = 3;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 12;
-            this.listBox1.Items.AddRange(new object[] {
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(234, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(392, 21);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 219);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(13, 288);
+			this.textBox2.MaxLength = 0;
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(200, 179);
+			this.textBox2.TabIndex = 3;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 12;
+			this.listBox1.Items.AddRange(new object[] {
             "0000000181000000\t0\t2\t4\t2\t",
             "0000184000000000\t0\t2\t3\t4\t",
             "0000000001808000\t0\t2\t4\t1\t",
@@ -211,28 +213,48 @@
             "000000000000200C\t0\t1\t2\t0\t",
             "0000002060000000\t0\t1\t2\t3\t",
             "0000000040600000\t0\t1\t2\t2\t"});
-            this.listBox1.Location = new System.Drawing.Point(234, 39);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(392, 412);
-            this.listBox1.TabIndex = 4;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(662, 479);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.pictureBox1);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.listBox1.Location = new System.Drawing.Point(234, 39);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(392, 172);
+			this.listBox1.TabIndex = 4;
+			this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(13, 248);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "init";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(93, 217);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(392, 21);
+			this.textBox3.TabIndex = 6;
+			this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(662, 479);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -243,6 +265,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox3;
     }
 }
 
