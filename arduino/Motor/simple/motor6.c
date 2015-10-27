3da2a2b1f3bc665cfbd19ba5305f283cddf0a420
abiaozsh <abiaozsh@sina.cn>
2015-10-27 17:26:22 +0800

    1

---
+++ arduino/Motor/simple/motor6.c
@@ -0,0 +1,71 @@
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+
+//2 1 0
+//5 4 3 2 1 0
+#define PORT6O PORTA
+#define DDR6O DDRA
+
+#define PORT3I PORTB
+#define DDR3I DDRB
+#define PIN3I PINB
+
+#define BP1D _BV(5)
+#define BP1U _BV(4)
+#define BP1A _BV(0)
+#define BP2D _BV(3)
+#define BP2U _BV(2)
+#define BP2A _BV(1)
+#define BP3D _BV(1)
+#define BP3U _BV(0)
+#define BP3A _BV(2)
+
+uint8_t NextStep[] = {
+  1,  2,  3,  4,  5,  0
+};
+
+//下臂常开
+uint8_t PWR_ON[] = {
+	BP1U + BP1D + BP3D, // 1-2
+	BP1U + BP1D + BP2D, // 1-3
+	BP2U + BP1D + BP2D, // 2-3
+	BP2U + BP3D + BP2D, // 2-1
+	BP3U + BP3D + BP2D, // 3-1
+	BP3U + BP1D + BP3D  // 3-2
+};
+uint8_t PWR_OFF[] = {
+	0    + BP1D + BP3D, // 1-2
+	0    + BP1D + BP2D, // 1-3
+	0    + BP1D + BP2D, // 2-3
+	0    + BP3D + BP2D, // 2-1
+	0    + BP3D + BP2D, // 3-1
+	0    + BP1D + BP3D  // 3-2
+};
+
+uint8_t DigitRead[] =        {BP3A,  BP2A,  BP1A,  BP3A,  BP2A,  BP1A};
+uint8_t DigitReadBaseVal[] = {BP3A,     0,  BP1A,     0,  BP2A,     0};
+
+int main(void) {
+  CLKPR = 128;//The CLKPCE bit must be written to logic one to enable change of the CLKPS bits. The CLKPCE bit is only updated when the other bits in CLKPR are simultaniosly written to zero.
+  CLKPR = 0;//1/1 //8MHz
+
+  //打开输出端口
+  DDR6O = BP1U | BP1D | BP2U | BP2D | BP3U | BP3D;
+  
+  //初始化输出端口
+  PORT6O = BP1D | BP2D | BP3D;
+  uint8_t Step = 0;
+  //主循环
+  for(;;) 
+  {
+    PORT6O = PWR_ON[Step];//CmdPWROn;
+    //等待过零
+    uint8_t valbase = DigitReadBaseVal[Step];
+    uint8_t drMask = DigitRead[Step];
+    while(valbase == PIN3I & drMask);
+    PORT6O = PWR_OFF[Step];//CmdPWROff;
+    Step = NextStep[Step];//CmdNextStep;
+  }
+}
+
