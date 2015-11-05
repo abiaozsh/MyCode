a13de754385bbe5e6ec810a6769a5a4f9a1d6d21
shzheng <shzheng@Ctrip.com>
2015-11-05 15:08:23 +0800

    taobao

---
+++ net/Taobao/LogTool/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace LogTool
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.n1 = new System.Windows.Forms.TextBox();
+			this.t1 = new System.Windows.Forms.TextBox();
+			this.t2 = new System.Windows.Forms.TextBox();
+			this.n2 = new System.Windows.Forms.TextBox();
+			this.t4 = new System.Windows.Forms.TextBox();
+			this.n4 = new System.Windows.Forms.TextBox();
+			this.t3 = new System.Windows.Forms.TextBox();
+			this.n3 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(293, 129);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 38;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click_1);
+			// 
+			// n1
+			// 
+			this.n1.Location = new System.Drawing.Point(12, 39);
+			this.n1.Name = "n1";
+			this.n1.Size = new System.Drawing.Size(128, 21);
+			this.n1.TabIndex = 39;
+			// 
+			// t1
+			// 
+			this.t1.Location = new System.Drawing.Point(12, 12);
+			this.t1.Name = "t1";
+			this.t1.Size = new System.Drawing.Size(128, 21);
+			this.t1.TabIndex = 40;
+			// 
+			// t2
+			// 
+			this.t2.Location = new System.Drawing.Point(183, 12);
+			this.t2.Name = "t2";
+			this.t2.Size = new System.Drawing.Size(128, 21);
+			this.t2.TabIndex = 42;
+			// 
+			// n2
+			// 
+			this.n2.Location = new System.Drawing.Point(183, 39);
+			this.n2.Name = "n2";
+			this.n2.Size = new System.Drawing.Size(128, 21);
+			this.n2.TabIndex = 41;
+			// 
+			// t4
+			// 
+			this.t4.Location = new System.Drawing.Point(528, 12);
+			this.t4.Name = "t4";
+			this.t4.Size = new System.Drawing.Size(128, 21);
+			this.t4.TabIndex = 46;
+			// 
+			// n4
+			// 
+			this.n4.Location = new System.Drawing.Point(528, 39);
+			this.n4.Name = "n4";
+			this.n4.Size = new System.Drawing.Size(128, 21);
+			this.n4.TabIndex = 45;
+			// 
+			// t3
+			// 
+			this.t3.Location = new System.Drawing.Point(357, 12);
+			this.t3.Name = "t3";
+			this.t3.Size = new System.Drawing.Size(128, 21);
+			this.t3.TabIndex = 44;
+			// 
+			// n3
+			// 
+			this.n3.Location = new System.Drawing.Point(357, 39);
+			this.n3.Name = "n3";
+			this.n3.Size = new System.Drawing.Size(128, 21);
+			this.n3.TabIndex = 43;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(971, 644);
+			this.Controls.Add(this.t4);
+			this.Controls.Add(this.n4);
+			this.Controls.Add(this.t3);
+			this.Controls.Add(this.n3);
+			this.Controls.Add(this.t2);
+			this.Controls.Add(this.n2);
+			this.Controls.Add(this.t1);
+			this.Controls.Add(this.n1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Log";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox n1;
+		private System.Windows.Forms.TextBox t1;
+		private System.Windows.Forms.TextBox t2;
+		private System.Windows.Forms.TextBox n2;
+		private System.Windows.Forms.TextBox t4;
+		private System.Windows.Forms.TextBox n4;
+		private System.Windows.Forms.TextBox t3;
+		private System.Windows.Forms.TextBox n3;
+	}
+}
+
---
+++ net/Taobao/LogTool/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Threading;
+using System.Net;
+using System.Diagnostics;
+using System.Runtime.Serialization.Json;
+
+namespace LogTool
+{
+	public partial class Form1 : Form
+	{
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+		}
+
+		public void ShowPage(List<Taobao.Mods.ItemList.Data.Auction> data)
+		{
+			if (data.Count <= 0) return;
+			string temp = System.IO.Path.GetTempFileName();
+			string newfile = temp.Substring(0, temp.Length - 3) + "html";
+			FileStream fs = new FileStream(newfile, FileMode.Create, FileAccess.Write);
+			StreamWriter sw = new StreamWriter(fs);
+
+			sw.WriteLine("<html>");
+			sw.WriteLine("<head>");
+			sw.WriteLine("<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />");
+			sw.WriteLine("</head>");
+			sw.WriteLine("<body>");
+
+			sw.WriteLine("<script>");
+			sw.WriteLine("function show(n)");
+			sw.WriteLine("{");
+			sw.WriteLine("  document.getElementById(n).src=document.getElementById(n).getAttribute(\"data\");");
+			sw.WriteLine("  document.getElementById(n).height=100;");
+			sw.WriteLine("  document.getElementById(n).width=100;");
+			sw.WriteLine("}");
+			sw.WriteLine("</script>");
+
+			sw.WriteLine("<table border='1'>");
+			string bg = "";
+
+			foreach (var item in data)
+			{
+				sw.WriteLine("<tr" + bg + ">");
+
+				sw.WriteLine("<td>");
+				sw.WriteLine("<a href='http:" + item.detail_url + "' target='_blank'>" + item.raw_title + "</a>");
+				sw.WriteLine("</td>");
+
+				sw.WriteLine("<td>");
+				//sw.WriteLine("<img src='http:" + item.pic_url + "'>");
+				sw.WriteLine("<img id='n" + item.nid + "' data='http:" + item.pic_url + "'>");
+				sw.WriteLine("</td>");
+
+				sw.WriteLine("<td>");
+				//sw.WriteLine(item.view_price);
+				sw.WriteLine("<a href='javascript:show(\"n" + item.nid + "\")'>" + item.view_price + "</a>");
+				sw.WriteLine("</td>");
+				sw.WriteLine("<td>");
+				sw.WriteLine(item.reserve_price);
+				sw.WriteLine("</td>");
+				sw.WriteLine("<td>");
+				sw.WriteLine(item.view_fee);
+				sw.WriteLine("</td>");
+
+				sw.WriteLine("<td>");
+				sw.WriteLine(item.item_loc);
+				sw.WriteLine("</td>");
+
+				sw.WriteLine("<td>");
+				sw.WriteLine(item.comment_count);
+				sw.WriteLine("</td>");
+
+				sw.WriteLine("<td>");
+				sw.WriteLine("<a href='http:" + item.shopcard.shopLink + "' target='_blank'>" + item.nick + "</a>");
+				sw.WriteLine("</td>");
+
+				sw.WriteLine("</tr>");
+
+				if (bg == "")
+				{
+					bg = " style='background:#DDD;'";
+				}
+				else
+				{
+					bg = "";
+				}
+
+			}
+			sw.WriteLine("</table>");
+			sw.WriteLine("</body>");
+			sw.WriteLine("</html>");
+
+			sw.Flush();
+			fs.Close();
+			Process p = new Process();
+			p.StartInfo = new ProcessStartInfo(newfile);
+			p.Start();
+		}
+
+		private void button1_Click_1(object sender, EventArgs e)
+		{
+			List<Taobao.Mods.ItemList.Data.Auction> List1 = new List<Taobao.Mods.ItemList.Data.Auction>();
+			List<Taobao.Mods.ItemList.Data.Auction> List2 = new List<Taobao.Mods.ItemList.Data.Auction>();
+			List<Taobao.Mods.ItemList.Data.Auction> List3 = new List<Taobao.Mods.ItemList.Data.Auction>();
+			List<Taobao.Mods.ItemList.Data.Auction> List4 = new List<Taobao.Mods.ItemList.Data.Auction>();
+
+			proc(n1.Text, t1.Text, List1);
+			proc(n2.Text, t2.Text, List2);
+			proc(n3.Text, t3.Text, List3);
+			proc(n4.Text, t4.Text, List4);
+
+			List1 = filt(List1, List2, List3, List4);
+			List2 = filt(List2, List1, List3, List4);
+			List3 = filt(List3, List2, List1, List4);
+			List4 = filt(List4, List2, List3, List1);
+
+			ShowPage(List1);
+			ShowPage(List2);
+			ShowPage(List3);
+			ShowPage(List4);
+		}
+
+		private void proc(string n, string t, List<Taobao.Mods.ItemList.Data.Auction> l)
+		{
+			if (!string.IsNullOrEmpty(n))
+			{
+				for (int i = 0; i < int.Parse(n); i++)
+				{
+					var tb = Request(t, i);
+					l.AddRange(tb.mods.itemlist.data.auctions);
+				}
+			}
+		}
+		private List<Taobao.Mods.ItemList.Data.Auction> filt(List<Taobao.Mods.ItemList.Data.Auction> l, List<Taobao.Mods.ItemList.Data.Auction> l1, List<Taobao.Mods.ItemList.Data.Auction> l2, List<Taobao.Mods.ItemList.Data.Auction> l3)
+		{
+			List<Taobao.Mods.ItemList.Data.Auction> ret = new List<Taobao.Mods.ItemList.Data.Auction>();
+			foreach (var item in l)
+			{
+				bool exist = false;
+				if (l1.Count > 0)
+				{
+					foreach (var item1 in l1)
+					{
+						if (item1.nick == item.nick)
+						{
+							exist = true;
+						}
+					}
+				}
+				else
+				{
+					exist = true;
+				}
+
+				if (exist)
+				{
+					exist = false;
+					if (l2.Count > 0)
+					{
+						foreach (var item2 in l2)
+						{
+							if (item2.nick == item.nick)
+							{
+								exist = true;
+							}
+						}
+					}
+					else
+					{
+						exist = true;
+					}
+
+				}
+				if (exist)
+				{
+					exist = false;
+					if (l3.Count > 0)
+					{
+						foreach (var item3 in l3)
+						{
+							if (item3.nick == item.nick)
+							{
+								exist = true;
+							}
+						}
+					}
+					else
+					{
+						exist = true;
+					}
+
+				}
+
+				if (exist)
+				{
+					ret.Add(item);
+				}
+			}
+
+			return ret;
+		}
+
+		[Serializable]
+		public class Taobao
+		{
+			public Mods mods;
+			public class Mods
+			{
+				public ItemList itemlist;
+
+				public class ItemList
+				{
+					public Data data;
+
+					public class Data
+					{
+						public List<Auction> auctions;
+
+						public class Auction
+						{
+							public string nid;
+							public string raw_title;
+							public string nick;
+							public string detail_url;
+							public string pic_url;
+							public string view_price;
+							public string reserve_price;
+							public string view_fee;
+							public string item_loc;
+							public string comment_count;
+							public Shopcard shopcard;
+
+							public class Shopcard
+							{
+								public string shopLink;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		public Taobao Request(string search, int no)
+		{
+			this.Text = search + " " + no;
+			string _url;
+			if (no == 0)
+			{
+				_url = "https://s.taobao.com/search?q=" + search + "&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20151105&ie=utf8";
+			}
+			else if (no == 1)
+			{
+				_url = "https://s.taobao.com/search?q=" + search + "&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20151105&ie=utf8&bcoffset=0&p4plefttype=3%2C1&p4pleftnum=1%2C3&s=44";
+			}
+			else
+			{
+				_url = "https://s.taobao.com/search?q=" + search + "&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20151105&ie=utf8&bcoffset=-3&p4plefttype=3%2C1&p4pleftnum=1%2C3&ntoffset=-3&s=" + (no * 44);
+			}
+			System.Net.HttpWebRequest Myrq = (System.Net.HttpWebRequest)System.Net.HttpWebRequest.Create(_url);
+			System.Net.HttpWebResponse myrp = (System.Net.HttpWebResponse)Myrq.GetResponse();
+			System.IO.Stream st = myrp.GetResponseStream();
+			StreamReader sr = new StreamReader(st);
+			while (true)
+			{
+				string line = sr.ReadLine();
+				if (line == null) break;
+				if (line.Trim().StartsWith("g_page_config"))
+				{
+					sr.Close();
+					st.Close();
+					myrp.Close();
+					Myrq.Abort();
+					return DeSerial(line.Trim().Substring("g_page_config = ".Length).TrimEnd(';'));
+				}
+			}
+
+			sr.Close();
+			st.Close();
+			myrp.Close();
+			Myrq.Abort();
+			return null;
+		}
+		private Taobao DeSerial(string jsonString)
+		{
+			DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(Taobao));
+			MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+			Taobao obj = (Taobao)ser.ReadObject(ms);
+			return obj;
+		}
+
+	}
+
+}
