82d752f5a21bf235ab6de9c544ab8ba98189df7e
abiaozsh <abiaozsh@sina.cn>
2016-01-11 22:48:45 +0800

    1

--- arduino/Motor/PCINT Driven Controler/code C tiny/motor6.c
+++ arduino/Motor/PCINT Driven Controler/code C tiny/motor6.c
@@ -2,10 +2,14 @@
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 	
-#define CUR_TIMING TIMING__8M_TCCR0B_1_115200
-#define TCCR0B_Value 1
+//#define CUR_TIMING TIMING__8M_TCCR0B_1_115200
+//#define TCCR0B_Value 1
+//PROGMEM prog_uint8_t TIMING__8M_TCCR0B_1_115200[] = { 69,138,208, 21, 91,160,230, 43,113,182};
 
-PROGMEM prog_uint8_t TIMING__8M_TCCR0B_1_115200[] = { 69,138,208, 21, 91,160,230, 43,113,182};
+
+#define CUR_TIMING TIMING__8M_TCCR1B_1_115200
+#define TCCR1B_Value 1
+PROGMEM prog_uint16_t TIMING__8M_TCCR1B_1_115200[] = {   69,  138,  208,  277,  347,  416,  486,  555,  625,  694};
 
 //#define DDR_Send DDRA
 //#define PORT_Send PORTA
@@ -41,18 +45,22 @@ void ClockInit() {
 	CLKPR = 0;//1/1 //8MHz
 }
 
-uint8_t buff[8];
 void loop() {
 	for(;;)
 	{
     uint8_t data = SerialRead();
+  PORTA = 0xFF;
     PORTA = data;
 	}
 }
 
 void TimerInit() {
-	TCCR0A = 0;
-	TIMSK0 = 0;
+	TCCR1A = 0;
+	TCCR1C = 0;
+	TIMSK1 = 0;
+
+	//TCCR0A = 0;
+	//TIMSK0 = 0;
 }
 
 void SerialInit(){
@@ -62,19 +70,36 @@ void SerialInit(){
 	PORT_Send |= BIT_Send;
 }
 
+//uint8_t SerialRead()
+//{
+//	uint8_t val = 0;
+//	while(PIN_Recv&BIT_Recv);
+//	TCCR0B = TCCR0B_Value;
+//	TCNT0 = 0;
+//	uint8_t timing;
+//	prog_uint8_t* pTiming = CUR_TIMING;
+//
+//  uint8_t i;
+//	for(i = 8 ; i > 0 ; i--)
+//	{
+//		timing = pgm_read_byte_near(pTiming++);while(timing-TCNT0<128);val>>=1;val |= (PIN_Recv&BIT_Recv?0x80:0);
+//	}
+//	return val;
+//}
+
 uint8_t SerialRead()
 {
+	TCCR1B = TCCR1B_Value;
 	uint8_t val = 0;
-	while(PIN_Recv&BIT_Recv);
-	TCCR0B = TCCR0B_Value;
-	TCNT0 = 0;
-	uint8_t timing;
-	prog_uint8_t* pTiming = CUR_TIMING;
+	while(PIN_Recv&BIT_Recv){}
+	TCNT1 = 0;
+	uint16_t timing;
+	timing = pgm_read_word_near(CUR_TIMING);while(TCNT1<timing);//startbit
 
   uint8_t i;
-	for(i = 8 ; i > 0 ; i--)
+	for(i = 1;i<=8;i++)
 	{
-		timing = pgm_read_word_near(pTiming++);while(timing-TCNT0<128);val>>=1;val |= (PIN_Recv&BIT_Recv?0x80:0);
+		val>>=1;val |= (PIN_Recv&BIT_Recv?0x80:0);timing = pgm_read_word_near(CUR_TIMING + i);while(TCNT1<timing);
 	}
 	return val;
 }
