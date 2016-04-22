95e589d87fa2ef981b2fbc9b1a11467d9ae43343
abiaozsh <abiaozsh@sina.cn>
2016-04-23 06:54:58 +0800

    1

---
+++ android/AutoCameraControl/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace WindowsFormsApplication4
+{
+	public partial class Form1 : Form
+	{
+		public static string DATA;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		//public string PhoneIP = "192.168.0.5";
+		//public string PhoneIP = "192.168.43.1";
+        public string PhoneIP = "192.168.0.8";
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+		}
+
+		void Form1_FormClosed(object sender, System.Windows.Forms.FormClosedEventArgs e)
+		{
+			Application.Exit();
+		}
+
+
+		public string Send(byte cmd, byte data, bool twopart, out string part2)
+		{
+			string ret = "";
+			part2 = "";
+			try
+			{
+				int port = 5000;
+				string host = PhoneIP;//服务器端ip地址
+
+				IPAddress ip = IPAddress.Parse(host);
+				IPEndPoint ipe = new IPEndPoint(ip, port);
+
+				Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				clientSocket.Connect(ipe);
+
+				byte[] snd = new byte[2];
+				snd[0] = cmd;
+				snd[1] = data;
+				clientSocket.Send(snd);
+
+				ret = receive(clientSocket);
+
+				if (twopart)
+				{
+					part2 = receive(clientSocket);
+				}
+				clientSocket.Close();
+			}
+			catch (Exception ex)
+			{
+				string s = ex.ToString();
+			}
+			return ret;
+		}
+
+		private string receive(Socket clientSocket)
+		{
+			byte[] data = new byte[1];
+
+			byte[] receive = new byte[3];
+			int len3 = 0;
+			while (len3 < 3)
+			{
+				len3 += clientSocket.Receive(receive, len3, 3 - len3, SocketFlags.None);
+			}
+			int len = (int)receive[0] + ((int)receive[1] << 8) + ((int)receive[2] << 16);
+
+			byte[] receiveBuff = new byte[len];
+			int len2 = 0;
+			while (len2 < len)
+			{
+				len2 += clientSocket.Receive(receiveBuff, len2, len - len2, SocketFlags.None);
+			}
+			string s = Encoding.Default.GetString(receiveBuff);
+
+			return s;
+		}
+
+		public byte[] SendBin(byte cmd, byte data, out string part2)
+		{
+			part2 = "";
+			byte[] ret = null;
+			try
+			{
+				int port = 5000;
+				string host = PhoneIP;//服务器端ip地址
+
+				IPAddress ip = IPAddress.Parse(host);
+				IPEndPoint ipe = new IPEndPoint(ip, port);
+
+				Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+				clientSocket.Connect(ipe);
+
+				byte[] snd = new byte[2];
+				snd[0] = cmd;
+				snd[1] = data;
+				clientSocket.Send(snd);
+
+				part2 = receive(clientSocket);
+
+				ret = receiveBin(clientSocket);
+
+				clientSocket.Close();
+			}
+			catch (Exception ex)
+			{
+				string s = ex.ToString();
+			}
+			return ret;
+		}
+
+		private byte[] receiveBin(Socket clientSocket)
+		{
+			byte[] data = new byte[1];
+
+			byte[] receive = new byte[3];
+			int len3 = 0;
+			while (len3 < 3)
+			{
+				len3 += clientSocket.Receive(receive, len3, 3 - len3, SocketFlags.None);
+			}
+			int len = (int)receive[0] + ((int)receive[1] << 8) + ((int)receive[2] << 16);
+
+			byte[] receiveBuff = new byte[len];
+			int len2 = 0;
+			while (len2 < len)
+			{
+				len2 += clientSocket.Receive(receiveBuff, len2, len - len2, SocketFlags.None);
+			}
+			return receiveBuff;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string part2;
+			string dir = Send(12, 0, false, out part2);
+			textBox1.Text = dir;
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			string content;
+			string file = Send(13, (byte)int.Parse(textBox2.Text), true, out content);
+
+			textBox2.Text = file;
+			textBox3.Text = content;
+			DATA = content;
+			//string dir = Send(12, 0, false);
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			string part2;
+			string file = Send(11, (byte)int.Parse(textBox2.Text), false, out part2);
+			textBox2.Text = file;
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			string part2;
+			string current = Send(10, 0, false, out part2);
+			textBox3.Text = current;
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			Form2 f = new Form2();
+			f.Show();
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+
+			while (true)
+			{
+				string part2;
+				string dir = Send(12, 0, false, out part2);
+				if (string.IsNullOrEmpty(dir)) break;
+				string filename;
+				byte[] data = SendBin(13, (byte)0, out filename);
+
+				FileStream fs = new FileStream(@"d:\dataCam\" + filename, FileMode.Create, FileAccess.Write);
+				fs.Write(data, 0, data.Length);
+				fs.Flush();
+				fs.Close();
+
+				string file = Send(11, (byte)0, false, out part2);
+				if (file != filename)
+				{
+					textBox1.Text = "error";
+				}
+			}
+
+		}
+
+
+	}
+}
---
+++ android/AutoCameraControl/WindowsFormsApplication4/Form2.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication4
+{
+	public partial class Form2 : Form
+	{
+		public Form2()
+		{
+			InitializeComponent();
+		}
+
+		struct Data
+		{
+			public DateTime t;
+			public double p;
+			public long raw;
+			public long glitch;
+			public long range;
+		}
+
+		List<Data> datas = new List<Data>();
+		double start;
+		double end;
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string dir = @"e:\data\";
+			datas = new List<Data>();
+			string[] files = Directory.GetFiles(dir);
+
+			foreach (string file in files)
+			{
+				LoadData(file);
+			}
+
+			datas.Sort(delegate(Data a, Data b)
+			{
+				return a.raw.CompareTo(b.raw);
+			});
+
+			long lastval = 0;
+			for (int i = 0; i < datas.Count; i++)
+			{
+				Data d = datas[i];
+				d.p = 2250000.0 / (d.raw - lastval);// (w)
+				datas[i] = d;
+				lastval = d.raw;
+			}
+
+
+
+
+			start = datas[0].raw;
+			end = datas[datas.Count - 1].raw;
+
+			Draw();
+
+			pictureBox1.MouseMove += new MouseEventHandler(pictureBox1_MouseMove);
+			pictureBox1.MouseDown += new MouseEventHandler(pictureBox1_MouseDown);
+			pictureBox1.MouseUp += new MouseEventHandler(pictureBox1_MouseUp);
+			pictureBox1.MouseWheel += new MouseEventHandler(pictureBox1_MouseWheel);
+			this.MouseWheel += new MouseEventHandler(Form2_MouseWheel);
+		}
+
+		void Form2_MouseWheel(object sender, MouseEventArgs e)
+		{
+			double ms = end - start;
+			ms *= 0.1;
+			if (e.Delta > 0)
+			{
+				start = start + ms;
+				end = end - ms;
+			}
+			else
+			{
+				start = start - ms;
+				end = end + ms;
+			}
+			Draw();
+
+		}
+
+		void LoadData(string file)
+		{
+			FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read);
+			StreamReader sr = new StreamReader(fs);
+
+			while (true)
+			{
+				string line = sr.ReadLine();
+				if (line == null) break;
+				long val = long.Parse(line);
+
+				Data d;
+				d.t = new DateTime(1970, 1, 1).AddHours(8).AddMilliseconds(val);
+				d.raw = val;
+				d.p = 0;
+				d.range = 0;
+				d.glitch = 0;
+
+				datas.Add(d);
+			}
+			fs.Close();
+		}
+
+		void pictureBox1_MouseWheel(object sender, MouseEventArgs e)
+		{
+		}
+
+		void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+		{
+			double ms = end - start;
+			ms = ms * (oldx - e.X) / 1024;
+			start = start + ms;
+			end = end + ms;
+			Draw();
+		}
+
+		int oldx;
+		void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+		{
+			oldx = e.X;
+
+			double ms = end - start;
+			double currentms = start + ms * e.X / 1024.0;
+			DateTime current = new DateTime(1970, 1, 1).AddHours(8).AddMilliseconds(datas[FindTop(currentms)].raw);
+			textBox1.Text = current.ToString("MM-dd HH:mm:ss fff") + "  " + datas[FindTop(currentms)].raw.ToString();
+		}
+
+		void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+		{
+			double ms = end - start;
+			double currentms = start + ms * e.X / 1024.0;
+			DateTime current = new DateTime(1970, 1, 1).AddHours(8).AddMilliseconds(currentms);
+			label1.Text = current.ToString("MM-dd HH:mm:ss fff") + "\r\n" + tempData[e.X].ToString();
+			label1.Top = e.Y;
+			label1.Left = e.X;
+		}
+
+
+		double[] tempData = new double[1024];
+
+		void Form2_KeyPress(object sender, KeyPressEventArgs e)
+		{
+		}
+
+		void Draw()
+		{
+			Bitmap b = new Bitmap(1024, 500);
+			Graphics g = Graphics.FromImage(b);
+			Pen p1 = new Pen(Color.Red);
+			Pen p2 = new Pen(Color.Green);
+
+			double ms = end - start;
+			for (int i = 0; i < 1024; i++)
+			{
+				double currentms = ms * i / 1024.0;
+				double nextms = ms * (i + 1) / 1024.0;
+
+				double current = start + currentms;
+
+				double power = 0;
+				int idxs = FindTop(start + currentms);
+				int idxe = FindBottom(start + nextms);
+
+				if (idxs != -1 && idxe != -1)
+				{
+					long diff = datas[idxe].raw - datas[idxs].raw;
+					power = 2250000.0 / (diff / (idxe - idxs));
+				}
+
+				tempData[i] = power;
+
+				if (((long)Math.Floor(((currentms + start) / 1000 / 3600 + 8) / 24)) % 2 == 0)
+				{
+					g.DrawLine(p1, i, 500, i, (float)(500 - power / 10));
+				}
+				else
+				{
+					g.DrawLine(p2, i, 500, i, (float)(500 - power / 10));
+				}
+			}
+			g.Flush();
+			pictureBox1.Image = b;
+		}
+
+		private int FindTop(double val)
+		{
+			int idx = datas.Count / 2;
+			int max = datas.Count;
+			int min = 0;
+			while (true)
+			{
+				if (idx < 0 || idx + 1 >= datas.Count)
+				{
+					return -1;
+				}
+				if (val > datas[idx].raw && val <= datas[idx + 1].raw)
+				{
+					return idx;
+				}
+				else if (val <= datas[idx].raw)
+				{
+					int tmp = (min + idx) / 2;
+					max = idx;
+					if (tmp == idx) return -1;
+					idx = tmp;
+				}
+				else if (val > datas[idx + 1].raw)
+				{
+					int tmp = (max + idx) / 2;
+					min = idx;
+					if (tmp == idx) return -1;
+					idx = tmp;
+				}
+			}
+		}
+		private int FindBottom(double val)
+		{
+			int idx = datas.Count / 2;
+			int max = datas.Count;
+			int min = 0;
+			while (true)
+			{
+				if (idx - 1 < 0 || idx >= datas.Count)
+				{
+					return -1;
+				}
+				if (val < datas[idx].raw && val >= datas[idx - 1].raw)
+				{
+					return idx;
+				}
+				else if (val >= datas[idx].raw)
+				{
+					int tmp = (max + idx) / 2;
+					min = idx;
+					if (tmp == idx) return -1;
+					idx = tmp;
+				}
+				else if (val < datas[idx - 1].raw)
+				{
+					int tmp = (min + idx) / 2;
+					max = idx;
+					if (tmp == idx) return -1;
+					idx = tmp;
+				}
+			}
+		}
+
+
+
+		private void Form2_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+
+			long start = 1460856147337;//04-17 09:22:27 337  
+			long end = 1460856278385;//04-17 09:24:38 385
+
+			long start1 = 1460856296202;//04-17 09:24:56 202
+			long end1 = 1460856341316;//04-17 09:25:41 316
+			FileStream fs = new FileStream(@"e:\data\0417.txt", FileMode.Open, FileAccess.Read);
+			StreamReader sr = new StreamReader(fs);
+
+			while (true)
+			{
+				string line = sr.ReadLine();
+				if (line == null) break;
+				long val = long.Parse(line);
+
+				Data d;
+				d.t = new DateTime(1970, 1, 1).AddHours(8).AddMilliseconds(val);
+				d.raw = val;
+				d.p = 0;
+				d.range = 0;
+				d.glitch = 0;
+
+				datas.Add(d);
+			}
+			fs.Close();
+			datas.Sort(delegate(Data a, Data b)
+			{
+				return a.raw.CompareTo(b.raw);
+			});
+
+			long lastval = 0;
+			for (int i = 0; i < datas.Count; i++)
+			{
+				Data d = datas[i];
+				d.p = 2250000.0 / (d.raw - lastval);// (w)
+				datas[i] = d;
+				lastval = d.raw;
+			}
+
+			int idxs = FindTop(start);
+			int idxe = FindBottom(end);
+
+			double power = 0;
+			if (idxs != -1 && idxe != -1)
+			{
+				long diff = datas[idxe].raw - datas[idxs].raw;
+				power = 2250000.0 / (diff / (idxe - idxs));
+			}
+
+			double diff2 = 2250000.0 / power;
+
+			long cnt = (long)((end1 - start1) / diff2);
+
+			StringBuilder sb = new StringBuilder();
+			for(int i=0;i<cnt;i++){
+				long val = start1 + ((end1 - start1) * i / cnt);
+				sb.AppendLine(val.ToString());
+			}
+			textBox2.Text = sb.ToString();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			string[] data = Form1.DATA.Split('\n');
+			datas = new List<Data>();
+			foreach (string line in data)
+			{
+				if (line == "") continue;
+				long val = long.Parse(line.Trim());
+
+				Data d;
+				d.t = new DateTime(1970, 1, 1).AddHours(8).AddMilliseconds(val);
+				d.raw = val;
+				d.p = 0;
+				d.range = 0;
+				d.glitch = 0;
+
+				datas.Add(d);
+			}
+
+			datas.Sort(delegate(Data a, Data b)
+			{
+				return a.raw.CompareTo(b.raw);
+			});
+
+			long lastval = 0;
+			for (int i = 0; i < datas.Count; i++)
+			{
+				Data d = datas[i];
+				d.p = 2250000.0 / (d.raw - lastval);// (w)
+				datas[i] = d;
+				lastval = d.raw;
+			}
+
+
+
+
+			start = datas[0].raw;
+			end = datas[datas.Count - 1].raw;
+
+			Draw();
+
+			pictureBox1.MouseMove += new MouseEventHandler(pictureBox1_MouseMove);
+			pictureBox1.MouseDown += new MouseEventHandler(pictureBox1_MouseDown);
+			pictureBox1.MouseUp += new MouseEventHandler(pictureBox1_MouseUp);
+			pictureBox1.MouseWheel += new MouseEventHandler(pictureBox1_MouseWheel);
+			this.MouseWheel += new MouseEventHandler(Form2_MouseWheel);
+
+		}
+
+	}
+}
