0bb045932064e5e5adf800435e783256cd9c4826
abiaozsh <abiaozsh@sina.cn>
2017-01-05 21:17:59 +0800

    1

---
+++ java/jmtp2/src/Folder.java
@@ -0,0 +1,209 @@
+
+
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.type.TypeFactory;
+
+public class Folder {
+	final static SimpleDateFormat dateformatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+	public static class StaticInfo {
+		public static TRoot statictf;
+		public static String arg1;
+	}
+
+	public static class TFile {
+		public String name;
+		public long size;
+		public String datetime;
+		public String md5;
+		public TFolder parent;
+
+		public Date _GetDatetime() {
+			try {
+				return dateformatter.parse(datetime);
+			} catch (Throwable t) {
+				return null;
+			}
+		}
+
+		public void _SetDatetime(Date d) {
+			if(d==null){
+				datetime = "1999-01-01 11:11:11.111";
+				return;
+			}
+			Calendar c = Calendar.getInstance();
+			c.setTime(d);
+			c.add(Calendar.YEAR, -1900);
+			datetime = dateformatter.format(c.getTime());
+		}
+
+		public String _ToString() {
+			return name;
+		}
+
+		public String _GetFullPath() {
+			String path = "";
+			TFolder f = this.parent;
+			while (f != null) {
+				path = f.name + "\\" + path;
+				f = f.parent;
+			}
+			return path + this.name;
+		}
+	}
+
+	public static class TFolder extends TFile {
+		public int filecount;
+		public List<TFile> fileList;
+		public List<TFolder> folderList;
+
+		public String _ToString() {
+			return "<" + name + ">";
+		}
+
+		public void setParent() {
+			for (TFile file : fileList) {
+				file.parent = this;
+			}
+			for (TFolder folder : folderList) {
+				folder.parent = this;
+				folder.setParent();
+			}
+		}
+
+		public void clearParent() {
+			for (TFile file : fileList) {
+				file.parent = null;
+			}
+			for (TFolder folder : folderList) {
+				folder.parent = null;
+				folder.clearParent();
+			}
+		}
+
+	}
+
+	public static class TRoot extends TFolder {
+		public String root;
+
+		public void save(String file) throws Throwable {
+
+			clearParent();
+
+			FileOutputStream fs = new FileOutputStream(file);
+			OutputStreamWriter sw = new OutputStreamWriter(fs, "utf-8");
+			String jsonString = serialize(this);
+
+			//jsonString = formatJson(jsonString);
+
+			sw.write(jsonString);
+			sw.flush();
+			fs.close();
+
+		}
+
+	}
+
+	// public static TRoot load(String file) {
+	// DataContractJsonSerializer mySerializer = new
+	// DataContractJsonSerializer(typeof(TRoot));
+	// FileStream myFileStream = new FileStream(file, FileMode.Open,
+	// FileAccess.Read, FileShare.Read);
+	// object myObject = mySerializer.ReadObject(myFileStream);
+	// myFileStream.Close();
+	// myFileStream.Dispose();
+	// TRoot tRoot = (TRoot) myObject;
+	// tRoot.setParent();
+	// return tRoot;
+	//
+	// }
+
+	public static String serialize(Object object) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Writer write = new StringWriter();
+		try {
+			// objectMapper.configure(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS,
+			// false);
+			objectMapper.writeValue(write, object);
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		return write.toString();
+	}
+
+	public static <T> T deserialize(String json, Class<T> clazz) {
+		ObjectMapper objectMapper = new ObjectMapper();
+		Object object = null;
+		try {
+			object = objectMapper.readValue(json, TypeFactory.rawClass(clazz));
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		return (T) object;
+	}
+
+	// public static <T> T deserialize(String json, TypeReference<T> typeRef) {
+	// try {
+	// return (T) objectMapper.readValue(json, typeRef);
+	// } catch (Throwable e) {
+	// e.printStackTrace();
+	// }
+	// return null;
+	// }
+
+	public static String formatJson(String jsonStr) {
+		if (null == jsonStr || "".equals(jsonStr))
+			return "";
+		StringBuilder sb = new StringBuilder();
+		char last = '\0';
+		char current = '\0';
+		int indent = 0;
+		for (int i = 0; i < jsonStr.length(); i++) {
+			last = current;
+			current = jsonStr.charAt(i);
+			switch (current) {
+			case '{':
+			case '[':
+				sb.append(current);
+				sb.append("\r\n");
+				indent++;
+				addIndentBlank(sb, indent);
+				break;
+			case '}':
+			case ']':
+				sb.append("\r\n");
+				indent--;
+				addIndentBlank(sb, indent);
+				sb.append(current);
+				break;
+			case ',':
+				sb.append(current);
+				if (last != '\\') {
+					sb.append("\r\n");
+					addIndentBlank(sb, indent);
+				}
+				break;
+			default:
+				sb.append(current);
+			}
+		}
+
+		return sb.toString();
+	}
+
+	private static void addIndentBlank(StringBuilder sb, int indent) {
+		for (int i = 0; i < indent; i++) {
+			sb.append("\t");
+		}
+	}
+
+}
---
+++ java/jmtp2/src/Sample.java
@@ -0,0 +1,116 @@
+import java.util.ArrayList;
+
+import jmtp.PortableDevice;
+import jmtp.PortableDeviceFolderObject;
+import jmtp.PortableDeviceManager;
+import jmtp.PortableDeviceObject;
+import jmtp.PortableDeviceStorageObject;
+
+public class Sample {
+	static {
+		System.loadLibrary("jmtp"); // 注意：加载动态链接库，不写扩展名，名字要与dll的文件名一样
+	}
+
+	// static Folder.TRoot tRoot;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Throwable {
+
+		// tRoot = new Folder.TRoot();
+
+		Folder.TRoot root = GetFolder();
+
+		root.save("memcard2.json");
+
+	}
+
+	public static Folder.TRoot GetFolder() {
+		Folder.TRoot tRoot = new Folder.TRoot();
+		PortableDeviceStorageObject root = getRoot(1);
+		Folder.TFolder tFolder = GetFolder(root);
+
+		tRoot.root = root.getName();
+
+		tRoot.fileList = tFolder.fileList;
+		tRoot.folderList = tFolder.folderList;
+		tRoot.name = tFolder.name;
+		tRoot.filecount = tFolder.filecount;
+		tRoot.datetime = tFolder.datetime;
+		tRoot.size = tFolder.size;
+
+		return tRoot;
+	}
+
+	public static Folder.TFolder GetFolder(PortableDeviceObject folder) {
+		try {
+			Folder.TFolder tf = new Folder.TFolder();
+			tf.folderList = new ArrayList<Folder.TFolder>();
+			tf.fileList = new ArrayList<Folder.TFile>();
+
+			PortableDeviceObject[] objs = null;
+			if (PortableDeviceStorageObject.class.isAssignableFrom(folder.getClass())) {
+				PortableDeviceStorageObject f = (PortableDeviceStorageObject) folder;
+				objs = f.getChildObjects();
+				tf._SetDatetime(f.getDateModified());
+				tf.name = f.getName();
+
+			} else if (PortableDeviceFolderObject.class.isAssignableFrom(folder.getClass())) {
+				PortableDeviceFolderObject f = (PortableDeviceFolderObject) folder;
+				objs = f.getChildObjects();
+				tf._SetDatetime(f.getDateModified());
+				tf.name = f.getOriginalFileName();
+
+			} else {
+				return null;
+			}
+
+			System.out.println(tf.name);
+			long size = 0;
+			int filecount = 0;
+			for (int i = 0; i < objs.length; i++) {
+				PortableDeviceObject obj = objs[i];
+
+				if (PortableDeviceFolderObject.class.isAssignableFrom(obj.getClass())) {
+					Folder.TFolder tempf = GetFolder((PortableDeviceFolderObject) obj);
+					tf.folderList.add(tempf);
+					size += tempf.size;
+					filecount += tempf.filecount;
+				} else {
+					Folder.TFile tfile = new Folder.TFile();
+					tfile.name = obj.getOriginalFileName();
+					System.out.println(tfile._GetFullPath());
+					tfile.size = obj.getSize().intValue();
+					tfile._SetDatetime(obj.getDateModified());
+					tf.fileList.add(tfile);
+					size += obj.getSize().intValue();
+					filecount++;
+				}
+			}
+			tf.size = size;
+			tf.filecount = filecount;
+			return tf;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	static PortableDeviceStorageObject getRoot(int idx) {
+
+		PortableDeviceManager manager = new PortableDeviceManager();
+		for (PortableDevice device : manager) {
+			device.open();
+			PortableDeviceObject[] roots = device.getRootObjects();
+			for (int i = 0; i < roots.length; i++) {
+				if (i == idx) {
+					PortableDeviceStorageObject root = (PortableDeviceStorageObject) roots[i];
+					return root;
+				}
+			}
+		}
+		return null;
+	}
+
+}
