dfe6db525204e1cae5527f9e648d9f50fa307dc6
abiaozsh <abiaozsh@sina.cn>
2017-07-19 14:08:47 +0800

    11

--- ESP8266/HelloServer/HelloServer.ino
+++ ESP8266/HelloServer/HelloServer.ino
@@ -1,7 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
 
 /*
   analogWriteFreq(20000);
@@ -24,6 +23,7 @@
 
   VIN GND RST EN 3V3 GND CLK SDO CMD SD1 SD2 SD3 NC ADC A0
   3V3 GND TX  RX D8  D7  D6  D5  GND 3V3  D4  D3 D2 D1  D0
+D0-led-vcc
 
 
   static const uint8_t SDA = 4;
@@ -31,6 +31,18 @@
 
   static const uint8_t LED_BUILTIN = 2;//D4
   static const uint8_t BUILTIN_LED = 2;//D4
+
+  static const uint8_t D3   = 0;
+  static const uint8_t TX   = 1;
+  static const uint8_t D4   = 2;
+  static const uint8_t RX   = 3;
+  static const uint8_t D2   = 4;
+  static const uint8_t D1   = 5;
+  static const uint8_t D6   = 12;
+  static const uint8_t D7   = 13;
+  static const uint8_t D5   = 14;
+  static const uint8_t D8   = 15;
+  static const uint8_t D0   = 16;
 
   static const uint8_t D0   = 16;
   static const uint8_t D1   = 5;
@@ -47,6 +59,8 @@
 const char* t_p = "text/plain";
 const char* ssid = "zsh2";
 const char* password = "92066200";
+
+ESP8266WebServer server(80);
 
 int getPin(String v) {
   if (v == "D0")return D0;
@@ -89,67 +103,80 @@ int getMode(String v) {
   if (v == "OUOD")return OUTPUT_OPEN_DRAIN;
 }
 
-int _parseInt(String v){
-  int ret = (v.charAt(0)-'0') + 
-  (v.charAt(1)-'0')*10 + 
-  (v.charAt(2)-'0')*100 + 
-  (v.charAt(3)-'0')*1000 + 
-  (v.charAt(4)-'0')*10000 + 
-  (v.charAt(5)-'0')*100000;
-}
-ESP8266WebServer server(80);
-
-const int led = LED_BUILTIN;
-
 void handleRoot() {
-  server.sendContent("HTTP/1.1 200 OK\r\n");
-  server.sendContent("Content-Type: text/html\r\n");
-server.sendContent("Connection: close\r\n");
-server.sendContent("Access-Control-Allow-Origin: *\r\n");
-server.sendContent("\r\n");
-server.sendContent("\
-  <html>\
-  <input id=\"bbbbbbbbbbb\" type=\"button\" value=\"val113421423\">\
-  <input id=\"btnbbb\" type=\"button\" value=\"val0\">\
-  <input id=\"btnccc\" type=\"button\" value=\"out\">\
-  <script src=\"base.js\" ></script>\
-  <script>");
-server.sendContent("document.getElementById(\"btnaaa\").onclick=function(){\
-    _ajaxGet(\"/digitalWrite\",\"pin=D2&val=1\",function(data){alert(data)});\
-  };\
-  document.getElementById(\"btnbbb\").onclick=function(){\
-    _ajaxGet(\"/digitalWrite\",\"pin=D2&val=0\",function(data){alert(data)});\
-  };\
-  document.getElementById(\"btnccc\").onclick=function(){\
-    _ajaxGet(\"/pinMode\",\"pin=D2&mode=OU\",function(data){alert(data)});\
-  };\
-  </script>\
-  </html>\
-\
-");
+	String ret = "";
+  ret+="<html>";
+  ret+="return:<input id=\"J_read\" type=\"text\" value=\"\"><br/>";
+  ret+="<input id=\"J_pin\" type=\"text\" value=\"D0\"><br/>";
+  ret+="<input id=\"J_freq\" type=\"text\" value=\"020000\">";
+  ret+="<input id=\"J_range\" type=\"text\" value=\"001023\">";
+  ret+="<input id=\"J_val\" type=\"text\" value=\"000012\"><br/>";
+  ret+="<input id=\"J_in\" type=\"button\" value=\"__IN__\">";
+  ret+="<input id=\"J_out\" type=\"button\" value=\"__OUT__\">";
+  ret+="<input id=\"J_inpu\" type=\"button\" value=\"__INPU__\">";
+  ret+="<input id=\"J_outod\" type=\"button\" value=\"__OUOD__\"><br/>";
+  ret+="<input id=\"J_hi\" type=\"button\" value=\"__HI__\">";
+  ret+="<input id=\"J_lo\" type=\"button\" value=\"__LO__\"><br/>";
+  ret+="<input id=\"J_dread\" type=\"button\" value=\"_D_Read__\">";
+  ret+="<input id=\"J_aread\" type=\"button\" value=\"_A_Read__\"><br/>";
+  ret+="<input id=\"J_aWrite\" type=\"button\" value=\"_A_Write__\"><br/>";
+  ret+="<script src=\"base.js\" ></script>";
+  ret+="<script>";
+  ret+="JQ(\"J_in\").onclick=function(){pinmode(\"IN\");};";
+  ret+="JQ(\"J_out\").onclick=function(){pinmode(\"OU\");};";
+  ret+="JQ(\"J_inpu\").onclick=function(){pinmode(\"INPU\");};";
+  ret+="JQ(\"J_outod\").onclick=function(){pinmode(\"OUOD\");};";
+  ret+="var pinmode=function(mode){";
+  ret+="_ajaxGet(\"/pinMode\",\"pin=\"+JQ(\"J_pin\").value+\"&mode=\"+mode,function(data){JQ(\"J_read\").value = data;});";
+  ret+="};";
+  ret+="JQ(\"J_hi\").onclick=function(){digitalWrite(\"1\")};";
+  ret+="JQ(\"J_lo\").onclick=function(){digitalWrite(\"0\")};";
+  ret+="var digitalWrite=function(val){";
+  ret+="_ajaxGet(\"/digitalWrite\",\"pin=\"+JQ(\"J_pin\").value+\"&val=\"+val,function(data){JQ(\"J_read\").value = data;});";
+  ret+="};";
+  ret+="JQ(\"J_dread\").onclick=function(){";
+  ret+="_ajaxGet(\"/digitalRead\",\"pin=\"+JQ(\"J_pin\").value,function(data){JQ(\"J_read\").value = data;});";
+  ret+="};";
+  ret+="JQ(\"J_aread\").onclick=function(){";
+  ret+="_ajaxGet(\"/analogRead\",\"\",function(data){JQ(\"J_read\").value = data;});";
+  ret+="};";
+  ret+="JQ(\"J_aWrite\").onclick=function(){";
+  ret+="  _ajaxGet(\"/analogWrite\",";
+  ret+="\"pin=\"+JQ(\"J_pin\").value+";
+  ret+="\"&freq=\"+JQ(\"J_freq\").value+";
+  ret+="\"&range=\"+JQ(\"J_range\").value+";
+  ret+="\"&val=\"+JQ(\"J_val\").value";
+  ret+=",function(data){JQ(\"J_read\").value = data;});";
+  ret+="};";
+  ret+="</script>";
+  ret+="end</html>";
+  server.send(200, "text/html", ret);
 }
 
 void basejs() {
-  server.send(200, "text/html", "\
-function getxhr() {\
-var a=false;\
-try{a=new ActiveXObject(\"Msxml2.XMLHTTP\");}catch (e){\
-try{a=new ActiveXObject(\"Microsoft.XMLHTTP\");} catch (e2) {\
-a = false;}}\
-if (!a && typeof XMLHttpRequest != 'undefined') {a = new XMLHttpRequest();}\
-return a;}\
-var xhr = getxhr();\
-function _ajaxGet(url,cont,cb) {\
-xhr.open(\"POST\",url,true);\
-xhr.setRequestHeader(\"Content-Type\",\"application/x-www-form-urlencoded\");\
-xhr.setRequestHeader(\"If-Modified-Since\", \"0\");\
-xhr.onreadystatechange = wacthing;\
-function wacthing() {\
-if(xhr.readyState==4) {\
-if(xhr.status==200) {\
-var res = xhr.responseText;\
-cb(res);}}}\
-xhr.send(cont);}");
+  String ret = "";
+  ret +="function getxhr() {";
+  ret +="var a=false;";
+  ret +="try{a=new ActiveXObject(\"Msxml2.XMLHTTP\");}catch (e){";
+  ret +="try{a=new ActiveXObject(\"Microsoft.XMLHTTP\");} catch (e2) {";
+  ret +="a = false;}}";
+  ret +="if (!a && typeof XMLHttpRequest != 'undefined') {a = new XMLHttpRequest();}";
+  ret +="return a;}";
+  ret +="var xhr = getxhr();";
+  ret +="function _ajaxGet(url,cont,cb) {";
+  ret +="xhr.open(\"POST\",url,true);";
+  ret +="xhr.setRequestHeader(\"Content-Type\",\"application/x-www-form-urlencoded\");";
+  ret +="xhr.setRequestHeader(\"If-Modified-Since\", \"0\");";
+  ret +="xhr.onreadystatechange = wacthing;";
+  ret +="function wacthing() {";
+  ret +="if(xhr.readyState==4) {";
+  ret +="if(xhr.status==200) {";
+  ret +="var res = xhr.responseText;";
+  ret +="cb(res);}}}";
+  ret +="xhr.send(cont);}";
+  ret +="var JQ = function(id){return document.getElementById(id);}";
+
+  server.send(200, "text/html", ret);
 }
 
 void my_pinMode() {
@@ -171,6 +198,22 @@ void my_pinMode() {
   server.send(200, t_p, ret);
   pinMode(pin, mode);
 }
+
+volatile int ison = 0;
+unsigned long currentMillis = 0;
+
+void test() {
+  int pin = D0;
+  int val = 1;
+
+  String ret = "ok";
+  server.send(200, t_p, ret);
+  
+  digitalWrite(D0, 1);
+  ison = 1;
+  currentMillis = millis();
+}
+
 void my_digitalWrite() {
   int pin;
   int val;
@@ -190,6 +233,7 @@ void my_digitalWrite() {
   server.send(200, t_p, ret);
   digitalWrite(pin, val);
 }
+
 void my_analogRead() {
   int analog = analogRead(0);
   String ret = "ok analogVal:";
@@ -208,6 +252,16 @@ void my_digitalRead() {
   String ret = "ok digitalVal:";
   ret += val;
   server.send(200, t_p, ret);
+}
+
+int _parseInt(String v){
+  int ret = ((int)(v.charAt(5)-'0')) + 
+  ((int)(v.charAt(4)-'0'))*10 + 
+  ((int)(v.charAt(3)-'0'))*100 + 
+  ((int)(v.charAt(2)-'0'))*1000 + 
+  ((int)(v.charAt(1)-'0'))*10000 + 
+  ((int)(v.charAt(0)-'0'))*100000;
+  return ret;
 }
 
 void my_analogWrite() {
@@ -230,12 +284,19 @@ void my_analogWrite() {
     }
   }
   
+  String ret = "ok pin:";
+  ret += pin;
+  ret += ",freq:";
+  ret += freq;
+  ret += ",range:";
+  ret += range;
+  ret += ",val:";
+  ret += val;
+
   analogWriteFreq(freq);//20000
   analogWriteRange(range);//1023
   analogWrite(pin,val);
 
-  String ret = "ok analogVal:";
-  ret += val;
   server.send(200, t_p, ret);
 }
 
@@ -253,6 +314,7 @@ void setup(void) {
     delay(500);
     Serial.print(".");
   }
+  
   Serial.println("");
   Serial.print("To:");
   Serial.println(ssid);
@@ -266,11 +328,12 @@ void setup(void) {
   server.on("/digitalRead", my_digitalRead);
   server.on("/analogRead", my_analogRead);
   server.on("/analogWrite", my_analogWrite);
+  server.on("/test", test);
 
-  server.on("/inline", []() {
-    digitalWrite(led, 0);
-    server.send(200, t_p, "this works as well");
-  });
+//  server.on("/inline", []() {
+//    digitalWrite(led, 0);
+//    server.send(200, t_p, "this works as well");
+//  });
 
   server.onNotFound(handleNotFound);
 
@@ -280,4 +343,11 @@ void setup(void) {
 
 void loop(void) {
   server.handleClient();
+  if(ison){
+    unsigned long mills = millis();  
+    if(mills - currentMillis > 2000){
+      digitalWrite(D0, 0);
+      ison = 0;
+    }
+  }
 }
