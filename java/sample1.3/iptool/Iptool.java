2dfbb1761b7474b13b658a0496e40a22f1fc9233
abiaozsh <abiaozsh@sina.cn>
2017-08-19 14:35:41 +0800

    1

--- java/sample1.3/iptool/Iptool.java
+++ java/sample1.3/iptool/Iptool.java
@@ -17,55 +17,16 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class Iptool {
+	static String oldip = "";
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
-		String oldip = "";
 		while (true) {
 			try {
-				// http://192.168.0.1/RST_status.htm
-				URL url = new URL("http://192.168.0.1/RST_status.htm");// "http://localhost:81/test"
-				HttpURLConnection urlcon = (HttpURLConnection) url.openConnection();
-
-				String loginPassword = "admin:920662";// "aa" + ":" + "bb";
-				String encoded = new sun.misc.BASE64Encoder().encode(loginPassword.getBytes());
-
-				urlcon.setRequestProperty("Authorization", "Basic " + encoded);
-
-				InputStream is = urlcon.getInputStream();
-				InputStreamReader isr = new InputStreamReader(is, "utf-8");
-
-				StringBuilder sb = new StringBuilder();
-				while (true) {
-					int c = isr.read();
-					if (c == -1)
-						break;
-					sb.append((char) c);
-				}
-				String s1 = sb.toString();
-
-				String target = "<tr><td width=\"60%\"><b>IP地址</b></td><td>";
-				String t2 = "</td></tr>";
-
-				int index1 = s1.indexOf(target);
-				String s2 = s1.substring(index1 + target.length());
-				int index2 = s2.indexOf(t2);
-				String ip = s2.substring(0, index2);
-
-				System.out.println("curr IP：" + ip);
-				if (!oldip.equals(ip)) {
-					oldip = ip;
-					SimpleMailSender mail = new SimpleMailSender("abiaozsh@sina.cn", "920662zsh");
-
-					try {
-						mail.send("abiaozsh@sina.cn", "ip", ip);
-					} catch (Throwable t) {
-						t.printStackTrace();
-					}
-				}
+				V2();
 
 				Thread.sleep(60000);
 			} catch (Throwable t) {
@@ -74,6 +35,95 @@ public class Iptool {
 		}
 
 	}
+
+	static void V1() throws Throwable {
+		// http://192.168.0.1/RST_status.htm
+		URL url = new URL("http://192.168.0.1/RST_status.htm");// "http://localhost:81/test"
+		HttpURLConnection urlcon = (HttpURLConnection) url.openConnection();
+
+		String loginPassword = "admin:920662";// "aa" + ":" + "bb";
+		String encoded = new sun.misc.BASE64Encoder().encode(loginPassword.getBytes());
+
+		urlcon.setRequestProperty("Authorization", "Basic " + encoded);
+
+		InputStream is = urlcon.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "utf-8");
+
+		StringBuilder sb = new StringBuilder();
+		while (true) {
+			int c = isr.read();
+			if (c == -1)
+				break;
+			sb.append((char) c);
+		}
+		String s1 = sb.toString();
+
+		String target = "<tr><td width=\"60%\"><b>IP地址</b></td><td>";
+		String t2 = "</td></tr>";
+
+		int index1 = s1.indexOf(target);
+		String s2 = s1.substring(index1 + target.length());
+		int index2 = s2.indexOf(t2);
+		String ip = s2.substring(0, index2);
+
+		MailIp(ip);
+
+	}
+
+	static void V2() throws Throwable {
+		// http://192.168.0.1/RST_status.htm
+		URL url = new URL("http://www.ip.cn/");// "http://localhost:81/test"
+		HttpURLConnection urlcon = (HttpURLConnection) url.openConnection();
+
+		// urlcon.setRequestProperty("Authorization", "Basic " + encoded);
+		urlcon.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8");
+		urlcon.setRequestProperty("Accept-Encoding", "deflate");
+		urlcon.setRequestProperty("Accept-Language", "zh-CN,zh;q=0.8");
+		urlcon.setRequestProperty("Cache-Control", "max-age=0");
+		urlcon.setRequestProperty("Connection", "keep-alive");
+		urlcon.setRequestProperty("Cookie", "UM_distinctid=15df927390e1b1-0345d371bc3e2e-3f63440c-1fa400-15df927390f39d; CNZZDATA123770=cnzz_eid%3D654128537-1503123642-%26ntime%3D1503123642");
+		urlcon.setRequestProperty("Host", "www.ip.cn");
+		urlcon.setRequestProperty("Upgrade-Insecure-Requests", "1");
+		urlcon.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36");
+
+		InputStream is = urlcon.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "utf-8");
+
+		StringBuilder sb = new StringBuilder();
+		while (true) {
+			int c = isr.read();
+			if (c == -1)
+				break;
+			sb.append((char) c);
+		}
+		String s1 = sb.toString();
+
+		String target = "<p>您现在的 IP：<code>";
+		String t2 = "</code></p><p>";
+
+		int index1 = s1.indexOf(target);
+		String s2 = s1.substring(index1 + target.length());
+		int index2 = s2.indexOf(t2);
+		String ip = s2.substring(0, index2);
+
+		MailIp(ip);
+	}
+
+	static void MailIp(String ip) {
+		System.out.println("curr IP：" + ip);
+		if (!oldip.equals(ip)) {
+			oldip = ip;
+			SimpleMailSender mail = new SimpleMailSender("abiaozsh@sina.cn", "920662zsh");
+
+			try {
+				mail.send("abiaozsh@sina.cn", "ip", ip);
+			} catch (Throwable t) {
+				t.printStackTrace();
+			}
+		}
+
+	}
+
 }
 
 class MailAuthenticator extends Authenticator {
