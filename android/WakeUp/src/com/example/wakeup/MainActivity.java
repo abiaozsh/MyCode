2816b26a2bd7bb927c1633277792994140d13f7b
abiaozsh <abiaozsh@sina.cn>
2018-03-07 20:27:26 +0800

    wake

--- android/WakeUp/src/com/example/wakeup/MainActivity.java
+++ android/WakeUp/src/com/example/wakeup/MainActivity.java
@@ -1,58 +1,126 @@
 package com.example.wakeup;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
-
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-public class MainActivity extends Activity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		String mac = "";
+		String ip = "";
+		try {
+			FileInputStream fis = this.openFileInput("config.txt");
+			InputStreamReader isr = new InputStreamReader(fis);
+			BufferedReader br = new BufferedReader(isr);
+			mac = br.readLine();
+			ip = br.readLine();
+			br.close();
+			isr.close();
+			fis.close();
+		} catch (Throwable t) {
+		}
+
+		if ("".equals(mac)) {
+			mac = "00-50-04-1D-A2-B4";
+
+		}
+		if ("".equals(ip)) {
+			ip = "192.168.1.40";
+		}
+
+		{
+			EditText et = (EditText) findViewById(R.id.editText1);
+			et.setText(mac);
+		}
+		{
+			EditText et = (EditText) findViewById(R.id.editText2);
+			et.setText(ip);
+		}
+
+		{
+			Button b = (Button) findViewById(R.id.buttonok);
+			b.setOnClickListener(this);
+		}
+
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onClick(View v) {
+		String mac = "";
+		String ip = "";
+		{
+			EditText et = (EditText) findViewById(R.id.editText1);
+			mac = et.getText().toString();
+		}
+		{
+			EditText et = (EditText) findViewById(R.id.editText2);
+			ip = et.getText().toString();
+		}
+
+		TestOpenOS w = new TestOpenOS();
+		w.mac = mac;
+		w.ipAddress = ip;
+		w.sendOpenOS();
+
+		try {
+			FileOutputStream fos = this.openFileOutput("config.txt", MODE_PRIVATE);
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			osw.write(mac + "\r\n");
+			osw.write(ip + "\r\n");
+			osw.close();
+			fos.close();
+			Toast.makeText(MainActivity.this, "ok!" + mac + " " + ip, Toast.LENGTH_LONG).show();
+		} catch (Throwable t) {
+		}
+
 	}
 
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int item_id = item.getItemId();
-		switch (item_id) {
-		case R.id.Wake10: {
-			TestOpenOS w = new TestOpenOS();
-			w.mac = "00-50-04-1D-A2-B4";
-			w.ipAddress = "192.168.0.10";
-			w.sendOpenOS();
-		}
-			break;
-		case R.id.Wake30: {
-			TestOpenOS w = new TestOpenOS();
-			w.mac = "00-50-04-1D-A2-B4";
-			w.ipAddress = "192.168.0.30";
-			w.sendOpenOS();
-		}
-			break;
-		default:
-			break;
-		}
-		return true;
-	}
+	// public boolean onOptionsItemSelected(MenuItem item) {
+	// int item_id = item.getItemId();
+	// switch (item_id) {
+	// case R.id.Wake10: {
+	// TestOpenOS w = new TestOpenOS();
+	// w.mac = "00-50-04-1D-A2-B4";
+	// w.ipAddress = "192.168.0.10";
+	// w.sendOpenOS();
+	// }
+	// break;
+	// case R.id.Wake30: {
+	// TestOpenOS w = new TestOpenOS();
+	// w.mac = "00-50-04-1D-A2-B4";
+	// w.ipAddress = "192.168.0.30";
+	// w.sendOpenOS();
+	// }
+	// break;
+	// default:
+	// break;
+	// }
+	// return true;
+	// }
 
 }
 
 class TestOpenOS {
 	public int port = 10000;
-	public String mac = "00-50-04-1D-A2-B4";
-	public String ipAddress = "255.255.255.255";
+	public String mac = "";
+	public String ipAddress = "";
 
 	public void sendOpenOS() {
 		InetAddress destHost = null;
