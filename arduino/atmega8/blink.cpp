936037427e5cb3c9c0a3ae7a410cabb4ea5b58e4
abiaozsh <abiaozsh@sina.cn>
2019-12-07 16:10:23 +0800

    1

---
+++ arduino/atmega8/blink.cpp
@@ -0,0 +1,57 @@
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+
+#define blinkDDR DDRC |=  _BV(0);
+#define blinkH PORTC |=  _BV(0);
+#define blinkL PORTC &= ~_BV(0);
+
+void TimerInit()
+{
+  // COM1A1 COM1A0 COM1B1 COM1B0 ¨C ¨C WGM11 WGM10
+  TCCR1A = 0;
+  //ICNC1 ICES1 ¨C WGM13 WGM12 CS12 CS11 CS10
+  
+  
+  //0 0 1 clkI/O/1 (No prescaling)
+  //0 1 0 clkI/O/8 (From prescaler)
+  //0 1 1 clkI/O/64 (From prescaler)
+  //1 0 0 clkI/O/256 (From prescaler)
+  //1 0 1 clkI/O/1024 (From prescaler)
+  //TCCR1B = 1;//  1/1
+  TCCR1B = 2;//  1/8 //1Mhz
+  //TCCR1B = 3;//  1/64
+
+  
+}
+
+void wait(uint16_t ticks)
+{
+	TCNT1 = 0;//timer reset
+	while(TCNT1<ticks)
+	{
+		;
+	}
+}
+
+int main()
+{
+	TimerInit(); //do later
+
+	blinkDDR;
+	
+	for (;;) {
+		blinkH;
+		for(uint16_t i=0;i<1000;i++)
+		{
+			wait(1000);//1000ms
+		}
+		blinkL;
+		for(uint16_t i=0;i<1000;i++)
+		{
+			wait(1000);//1000ms
+		}
+	}
+        
+	return 0;
+}
