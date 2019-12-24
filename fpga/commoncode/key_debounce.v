a0bb6e03f08c520d2695fa9e3c2740579829956e
abiaozsh <abiaozsh@sina.cn>
2019-12-24 21:15:11 +0800

    1

--- fpga/commoncode/key_debounce.v
+++ fpga/commoncode/key_debounce.v
@@ -1,43 +1,31 @@
 
 module key_debounce(
-		input            sys_clk,     
-		input            sys_rst_n,   
-		input            key,         
-		output reg       key_value 
-		);
+  input            sys_clk,
+  input            sys_rst_n,
+  input            key,
+  output reg       key_value
+);
 
-	//reg define    
-	//reg [31:0] counter;
-	//localparam zero =32'd0;
-	//localparam top =32'd50000000;
-	
-	
-	
-	reg [19:0] counter;
-	localparam zero =20'd0;
-	localparam top =20'd1000000;
-	
-	always @(posedge sys_clk or negedge sys_rst_n) begin 
-		if (!sys_rst_n) begin 
-			key_value <= key;
-			counter <= zero;
-		end else begin
-			if(counter==0)begin
-				if(key_value!=key) begin
-					key_value <= key;
-					counter <= counter+1'b1;
-				end else begin
-					key_value <= key;
-					counter<= counter;
-				end
-			end else if(counter==top)begin
-				key_value <= key;
-				counter <= zero;
-			end else begin
-				key_value <= key_value;
-				counter <= counter+1'b1;
-			end
-		end
-	end
-endmodule 
+  reg [19:0] counter;
+  localparam top =20'd1000000;//50000000/1000000 = 1/50 s
+
+  always @(posedge sys_clk or negedge sys_rst_n) begin 
+    if (!sys_rst_n) begin 
+      key_value <= key;
+      counter <= 0;
+    end else begin
+      if(counter==0)begin
+        key_value <= key;
+        if(key_value!=key) begin //按键变动触发计时+锁止
+          counter <= counter+1'b1;
+        end
+      end else if(counter==top)begin//计时到顶 解锁
+        key_value <= key;
+        counter <= 0;
+      end else begin
+        counter <= counter+1'b1;
+      end
+    end
+  end
+endmodule
 
