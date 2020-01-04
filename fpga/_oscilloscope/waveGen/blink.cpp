e1a7f5b38809ca74193f46a78b24315142cd2acb
abiaozsh <abiaozsh@sina.cn>
2020-01-04 21:31:51 +0800

    1

--- fpga/_oscilloscope/waveGen/blink.cpp
+++ fpga/_oscilloscope/waveGen/blink.cpp
@@ -13,89 +13,61 @@ int main()
   DDRC |= _BV(PIN);
   
   while(1){
-    while(TCNT1<60000);
-    HIGH;TCNT1 = 0;
-    while(TCNT1<60000);
     LOW;TCNT1 = 0;
-    
-    while(TCNT1<30000);
-    HIGH;TCNT1 = 0;
-    while(TCNT1<30000);
-    LOW;TCNT1 = 0;
-    
-    while(TCNT1<10000);
-    HIGH;TCNT1 = 0;
-    while(TCNT1<10000);
-    LOW;TCNT1 = 0;
-    
-    while(TCNT1<5000);
-    HIGH;TCNT1 = 0;
-    while(TCNT1<5000);
-    LOW;TCNT1 = 0;
-    
-    while(TCNT1<2000);
-    HIGH;TCNT1 = 0;
-    while(TCNT1<2000);
-    LOW;TCNT1 = 0;
-    
-    while(TCNT1<1000);
-    HIGH;TCNT1 = 0;
-    while(TCNT1<1000);
-    LOW;TCNT1 = 0;
-    
     while(TCNT1<100);
-    HIGH;TCNT1 = 0;
-    while(TCNT1<100);
-    LOW;TCNT1 = 0;
-    
-    while(TCNT1<10);
+
     HIGH;TCNT1 = 0;
     while(TCNT1<10);
     LOW;TCNT1 = 0;
-    
-    while(TCNT1<5);
+    while(TCNT1<10);
+
     HIGH;TCNT1 = 0;
     while(TCNT1<5);
     LOW;TCNT1 = 0;
+    while(TCNT1<5);
+
+    for(uint8_t i=0;i<10;i++){
+      HIGH;
+      LOW;
+      HIGH;
+      LOW;
+    }
+    for(uint8_t i=0;i<10;i++){
+      HIGH;
+      LOW;
+      HIGH;
+      LOW;
+    }
+    for(uint8_t i=0;i<10;i++){
+      HIGH;
+      LOW;
+      HIGH;
+      LOW;
+      HIGH;
+      LOW;
+      HIGH;
+      LOW;
+      HIGH;
+      LOW;
+      HIGH;
+      LOW;
+      HIGH;
+      LOW;
+      HIGH;
+      LOW;
+      HIGH;
+      LOW;
+      HIGH;
+      LOW;
+      HIGH;
+      LOW;
+      HIGH;
+      LOW;
+    }
     
-    for(uint8_t i=0;i<10;i++){
-      HIGH;
-      LOW;
-      HIGH;
-      LOW;
-    }
-    for(uint8_t i=0;i<10;i++){
-      HIGH;
-      LOW;
-      HIGH;
-      LOW;
-    }
-    for(uint8_t i=0;i<10;i++){
-      HIGH;
-      LOW;
-      HIGH;
-      LOW;
-      HIGH;
-      LOW;
-      HIGH;
-      LOW;
-      HIGH;
-      LOW;
-      HIGH;
-      LOW;
-      HIGH;
-      LOW;
-      HIGH;
-      LOW;
-      HIGH;
-      LOW;
-      HIGH;
-      LOW;
-      HIGH;
-      LOW;
-      HIGH;
-      LOW;
-    }
+    HIGH;TCNT1 = 0;
+    while(TCNT1<100);
+
   }
 	return 0;
 }
