ab97b8795f2b1f306ca7f12abcc8ae1ee0e90e75
abiaozsh <abiaozsh@sina.cn>
2020-01-06 20:35:50 +0800

    1

--- fpga/_oscilloscope/java/HelloWorld.c
+++ fpga/_oscilloscope/java/HelloWorld.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "HelloWorld.h"
+#include "wiringPi.c"
 
 //find / -name "jni.h"
 // /usr/lib/jvm/jdk-8-oracle-arm32-vfp-hflt/include/jni.h
@@ -29,5 +30,95 @@ JNIEXPORT jint JNICALL Java_HelloWorld_sayHello(JNIEnv* env, jobject obj, jshort
         return val;
 }
 
+static int vvv;
+
+int test0(int val){
+  
+  vvv = val;
+}
+
+int test1(){
+  
+  return vvv;
+}
+
 //JNIEXPORT jint JNICALL Java_HelloWorld_sayHello
 //  (JNIEnv *, jclass, jint);
+
+int mydigitalRead (int pin)
+{
+
+    if ((*(gpio + 13) & (1 << (pin & 31))) != 0)
+      return HIGH ;
+    else
+      return LOW ;
+}
+
+void mydigitalWrite (int pin, int value)
+{
+
+    if (value == LOW)
+      *(gpio + 10) = 1 << (pin & 31) ;
+    else
+      *(gpio + 7) = 1 << (pin & 31) ;
+}
+
+
+void setCmd(volatile unsigned int* gpio,int cmd){
+  mydigitalWrite(gpio, CMD0, (cmd & 0x01)?HIGH:LOW);
+  mydigitalWrite(gpio, CMD1, (cmd & 0x02)?HIGH:LOW);
+  mydigitalWrite(gpio, CMD2, (cmd & 0x04)?HIGH:LOW);
+  mydigitalWrite(gpio, CMD3, (cmd & 0x08)?HIGH:LOW);
+}
+
+int init(){
+  
+  
+  
+  volatile unsigned int* gpio = wiringPiSetup();
+  
+    for(int i=0;i<32;i++){
+    pinMode(i, INPUT);
+  }
+  
+  pinMode(REQ, OUTPUT);
+  pinMode(RST, OUTPUT);
+  pinMode(CMD0, OUTPUT);
+  pinMode(CMD1, OUTPUT);
+  pinMode(CMD2, OUTPUT);
+  pinMode(CMD3, OUTPUT);
+
+  digitalWrite(REQ, LOW);
+  digitalWrite(RST, HIGH);
+  digitalWrite(RST, LOW);
+
+  return (long)gpio;
+}
+
+int test(){
+  
+  setCmd(volatile unsigned int* gpio,int cmd){
+}
+
+#define PINACK ((*gpio13) & 1 << ACK)
+#define REQHIGH *(gpio7) = 1 << (REQ)
+#define REQLOW  *(gpio10) = 1 << (REQ)
+
+int dump1(){
+  volatile unsigned int* gpio = ??;
+  
+  volatile unsigned int* gpio13 = gpio + 13;
+  volatile unsigned int* gpio10 = gpio + 10;
+  volatile unsigned int* gpio7 = gpio + 7;
+
+    for(int i=0;i<pagesize;i++){
+      REQHIGH;//digitalWrite(REQ, HIGH);
+      while(!PINACK);// digitalRead(ACK)==LOW
+      short data = (short)(*(gpio + 13));//read16();
+      buff[j*pagesize+i]=data;
+      REQLOW;//digitalWrite(REQ, LOW);
+      while(PINACK);//digitalRead(ACK)==HIGH
+    }
+    printf("%d\r\n",j);
+
+}
--- fpga/_oscilloscope/java/HelloWorld.java
+++ fpga/_oscilloscope/java/HelloWorld.java
@@ -1,86 +1,143 @@
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Shape;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
 
 //javac HelloWorld.java
 //javah -jni HelloWorld
 
 //java -Djava.library.path=. HelloWorld
-public class HelloWorld
-{      
-    static
-    {
+public class HelloWorld {
+    static {
+        try {
             System.loadLibrary("HelloWorld");
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
     }
+
     private static native int sayHello(short[] array);
-    public static void main(String[] args)
-    {
-      short[] array = new short[100];
-      array[0] = 111;
-            int val = sayHello(array);//new HelloWorld().
+
+    
+    private static native long init();
+    
+    //return position
+    private static native int dumpData(long gpio,short[] array);
+    
+    private static native int fetchData(long gpio);
+    
+    private static native int startRecord(long gpio);
+    
+    private static native int stopRecord(long gpio);
+    
+    private static native int test(long gpio);
+    
+    private static native int test0(int val);
+    private static native int test1();
+    
+    public static void main(String[] args) {
+        try {
+            short[] array = new short[100];
+            array[0] = 111;
+            int val = sayHello(array);
             System.out.println(array[2]);
             System.out.println(array[3]);
             System.out.println(val);
             
+            long gpio = init();
+            int testv = test(gpio);
+            System.out.println(testv);
             
             
-            
-                    // TODO Auto-generated method stub
-        DrawFrame df=new DrawFrame();
-        df.setVisible(true);
-            
-            
-            
-            
-            
-    }
-    
-    
-    public static class DrawFrame extends JFrame{
+            test0(123);
+            int vvv = test1();
+            System.out.println(vvv);
 
-    public DrawFrame(){
-        super();
-        initialize();//调用初始化方法
-    }
-    //初始化方法
-    public void initialize(){
-        this.setSize(300, 200);//设置窗体大小
-        //设置窗体的关闭方式
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        add(new CanvasPanel());//设置窗体面板为绘图面板对象
-        this.setTitle("绘图实例2");//设置窗体标题
-    }
-    
-    //创建内部类
-    class CanvasPanel extends JPanel{
-        public void paint(Graphics g){
-            super.paint(g);
-            Graphics2D g2=(Graphics2D)g;//强制类型转换位Graphics2D类型
-            //Graphics2D是推荐使用的绘图类，但是程序设计中提供的绘图对象大多是
-            //Graphics类的实例，这时应该使用强制类型转换将其转换为Graphics
-            Shape[] shapes=new Shape[4];//声明图形数组
-            shapes[0]=new Ellipse2D.Double(5, 5, 100, 100);//创建圆形对象
-            //创建矩形对象
-            shapes[1]=new Rectangle2D.Double(110,5,100,100);//
-            shapes[2]=new Rectangle2D.Double(15, 15, 80, 80);//创建矩形对象
-            shapes[3]=new Ellipse2D.Double(120, 15, 80, 80);//创建圆形对象
-            for(Shape shape:shapes){//遍历图型数组
-                Rectangle2D bounds=shape.getBounds2D();
-                if(bounds.getWidth()==80){
-                    g2.fill(shape);//填充图形
-                }else{
-                    g2.draw(shape);//绘制图形
-                }
-            }
-            
+        } catch (Throwable t) {
+            t.printStackTrace();
         }
+
+        DrawFrame form = new DrawFrame();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                form.refresh();
+            }
+        };
+        Timer timer = new Timer();
+        // 定义开始等待时间  --- 等待 5 秒
+        // 1000ms = 1s
+        long delay = 100;
+        // 定义每次执行的间隔时间
+        long intevalPeriod = 100;
+        // schedules the task to be run in an interval
+        // 安排任务在一段时间内运行
+        timer.scheduleAtFixedRate(task, delay, intevalPeriod);
     }
 
-}
+    static int i = 0;
+
+    public static class DrawFrame extends Frame {
+        private static final long serialVersionUID = 1L;
+
+        public void refresh(){
+            this.repaint();
+        }
+        public DrawFrame() {
+            DrawFrame self = this;
+            this.setSize(basex + 1024 + 20, basey + 512 + 20);
+            this.addWindowListener(new WindowAdapter() {
+                public void windowClosing(WindowEvent e) {
+                    System.exit(0);
+                }
+            });
+
+            this.addKeyListener(new KeyAdapter() {
+                public void keyPressed(KeyEvent e) {
+                    int code = e.getKeyCode();
+
+                    System.out.println(code);
+
+                }
+            });
+
+            this.addMouseListener(new MouseAdapter() {
+                public void mouseEntered(MouseEvent e) {
+                    //self.validate();
+                    //self.repaint();
+                }
+
+                public void mouseClicked(MouseEvent e) {
+                    //self.repaint();
+                }
+
+                public void mouseWheelMoved(MouseWheelEvent e) {
+                    //self.validate();
+                    //self.repaint();
+
+                }
+            });
+            self.setVisible(true);
+        }
+
+        static final int basex = 15;
+        static final int basey = 140;
+
+        public void paint(Graphics g) {
+            super.paint(g);
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setColor(Color.LIGHT_GRAY);
+            g2.fillRect(basex, basey, 1024, 512);
+            g2.setColor(Color.GRAY);
+            g2.drawRect(basex, basey, 1024, 256);
+            g2.drawRect(basex, basey + 256, 1024, 256);
+
+            g2.setColor(Color.BLACK);
+            g2.drawString("fdsa", 60, 50);
+            g2.drawLine(50, 50, 55, 55 + i);
+            i++;
+
+        }
+
+    }
+
 }
