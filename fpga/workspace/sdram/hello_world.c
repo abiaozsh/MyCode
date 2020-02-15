058b6966e0f76d96d58a5ee271ee7288b2d07541
abiaozsh <abiaozsh@sina.cn>
2020-02-15 15:23:43 +0800

    1

---
+++ fpga/workspace/sdram/hello_world.c
@@ -0,0 +1,19 @@
+//#include <stdio.h>
+#include "system.h"
+#include "altera_avalon_pio_regs.h"
+
+int main() {
+	int data = 0;
+	while (1) {
+		int val = IORD(PIO_1_BASE, 0);
+
+		if(val & 1){
+			data++;
+		}
+
+		IOWR(PIO_0_BASE, 0, data);
+
+	}
+
+	return 0;
+}
