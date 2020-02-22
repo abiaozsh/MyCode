2d8bff4acdd4119b15f67036aee5699a7ef9c788
abiaozsh <abiaozsh@sina.cn>
2020-02-22 10:04:16 +0800

    1

--- fpga/commoncode/uart_recv_hs.v
+++ fpga/commoncode/uart_recv_hs.v
@@ -7,78 +7,54 @@ module uart_recv_hs(
     output reg uart_rec,//接收一帧数据完成标志信号 上升沿表示接收到
     output reg [7:0] uart_data_out//接收的数据
   );
+
+//parameter define
+localparam BPS_CNT  = 25;        //2000000 bps  50000000/2000000
+localparam BPS_CNT_HALF  = 12;        //2000000 bps  50000000/2000000
+
+reg uart_rxd_last1;
+reg uart_rxd_last2;
+reg rx_flag; //接收过程标志信号
+reg [7:0] clk_cnt; //系统时钟计数器
+
+//根据接收数据计数器来寄存uart接收端口数据
+always @(posedge sys_clk or negedge sys_rst_n) begin 
+  if ( !sys_rst_n) begin
+    rx_flag <= 0;
+    clk_cnt <= 0;
+    uart_rxd_last1 <= 1;
+    uart_rxd_last2 <= 1;
+    uart_data_out <= 8'd0;
     
-  //parameter define
-  localparam BPS_CNT  = 25;        //2000000 bps  50000000/2000000
-  localparam BPS_CNT_HALF  = 12;        //2000000 bps  50000000/2000000
+  end else begin
+    uart_rxd_last1 <= uart_rxd;
+    uart_rxd_last2 <= uart_rxd_last1;
 
-  //需要对系统时钟计数BPS_CNT次
-  reg [ 7:0] clk_cnt;                             //系统时钟计数器
-
-  //wire define
-  wire start_flag;
-
-  //捕获接收端口下降沿(起始位)，得到一个时钟周期的脉冲信号
-  assign start_flag = uart_rxd_last & (~uart_rxd);
-
-  reg uart_rxd_last;
-  //对UART接收端口的数据延迟两个时钟周期
-  always @(posedge sys_clk or negedge sys_rst_n) begin 
-    if (!sys_rst_n) begin 
-      uart_rxd_last <= 1;
-    end else begin
-      uart_rxd_last  <= uart_rxd;
+    if(uart_rxd_last2 && !uart_rxd_last1) begin//检测到起始位
+      rx_flag <= 1; //进入接收过程，标志位rx_flag拉高
+    end else if(clk_cnt == (BPS_CNT * 9 + BPS_CNT_HALF)) begin
+      rx_flag <= 0; //计数到停止位中间时，停止接收过程
     end
-  end
 
-  reg rx_flag; //接收过程标志信号
-  //当脉冲信号start_flag到达时，进入接收过程
-  always @(posedge sys_clk or negedge sys_rst_n) begin
-    if (!sys_rst_n) begin
-      rx_flag <= 0;
+    
+    if(rx_flag) begin //系统处于接收过程
+      clk_cnt <= clk_cnt + 1;
+      case ( clk_cnt )
+        (1*BPS_CNT+BPS_CNT_HALF) : uart_data_out[0] <= uart_rxd_last2; //寄存数据位最低位
+        (2*BPS_CNT+BPS_CNT_HALF) : uart_data_out[1] <= uart_rxd_last2;
+        (3*BPS_CNT+BPS_CNT_HALF) : uart_data_out[2] <= uart_rxd_last2;
+        (4*BPS_CNT+BPS_CNT_HALF) : uart_data_out[3] <= uart_rxd_last2;
+        (5*BPS_CNT+BPS_CNT_HALF) : uart_data_out[4] <= uart_rxd_last2;
+        (6*BPS_CNT+BPS_CNT_HALF) : uart_data_out[5] <= uart_rxd_last2;
+        (7*BPS_CNT+BPS_CNT_HALF) : uart_data_out[6] <= uart_rxd_last2;
+        (8*BPS_CNT+BPS_CNT_HALF) : begin uart_data_out[7] <= uart_rxd_last2;uart_rec <= 1; end //寄存数据位最高位
+        default:uart_rec <= 0;
+      endcase
     end else begin
-      if(start_flag) begin//检测到起始位
-        rx_flag <= 1; //进入接收过程，标志位rx_flag拉高
-      end else if(clk_cnt == (BPS_CNT * 9 + BPS_CNT_HALF)) begin
-        rx_flag <= 0; //计数到停止位中间时，停止接收过程
-      end
-    end
-  end
-
-  //进入接收过程后，启动系统时钟计数器与接收数据计数器
-  always @(posedge sys_clk or negedge sys_rst_n) begin
-    if (!sys_rst_n) begin
       clk_cnt <= 0;
-    end else begin
-      if ( rx_flag ) begin //处于接收过程
-        clk_cnt <= clk_cnt + 1;
-      end else begin //接收过程结束，计数器清零
-        clk_cnt <= 0;
-      end
+      uart_rec <= 0;
     end
   end
-
-  //根据接收数据计数器来寄存uart接收端口数据
-  always @(posedge sys_clk or negedge sys_rst_n) begin 
-    if ( !sys_rst_n) begin
-      uart_data_out <= 8'd0;
-    end else begin
-      if(rx_flag) begin //系统处于接收过程
-        case ( clk_cnt )
-          (1*BPS_CNT+BPS_CNT_HALF) : uart_data_out[0] <= uart_rxd; //寄存数据位最低位
-          (2*BPS_CNT+BPS_CNT_HALF) : uart_data_out[1] <= uart_rxd;
-          (3*BPS_CNT+BPS_CNT_HALF) : uart_data_out[2] <= uart_rxd;
-          (4*BPS_CNT+BPS_CNT_HALF) : uart_data_out[3] <= uart_rxd;
-          (5*BPS_CNT+BPS_CNT_HALF) : uart_data_out[4] <= uart_rxd;
-          (6*BPS_CNT+BPS_CNT_HALF) : uart_data_out[5] <= uart_rxd;
-          (7*BPS_CNT+BPS_CNT_HALF) : uart_data_out[6] <= uart_rxd;
-          (8*BPS_CNT+BPS_CNT_HALF) : begin uart_data_out[7] <= uart_rxd;uart_rec <= 1; end //寄存数据位最高位
-          default:uart_rec <= 0;
-        endcase
-      end else begin
-        uart_rec <= 0;
-      end
-    end
-  end
+end
 
 endmodule
--- fpga/commoncode/uart_send_hs.v
+++ fpga/commoncode/uart_send_hs.v
@@ -1,4 +1,3 @@
-
 module uart_send_hs(
     input sys_clk,
     input sys_rst_n,
@@ -9,64 +8,38 @@ module uart_send_hs(
     input [7:0] uart_data_in//待发送数据
   );
 
-  //parameter define
-  localparam BPS_CNT  = 25; //2000000 bps  50000000/2000000
-  localparam BPS_CNT_HALF  = 12;        //2000000 bps  50000000/2000000
+//parameter define
+localparam BPS_CNT  = 25; //2000000 bps  50000000/2000000
+localparam BPS_CNT_HALF  = 12;        //2000000 bps  50000000/2000000
 
+reg uart_send_last1;
+reg uart_send_last2;
+reg tx_flag;//发送过程标志信号
+reg [7:0] tx_data;//寄存发送数据
+reg [7:0] clk_cnt;//系统时钟计数器
 
-  //wire define
-  wire       en_flag;
-  //捕获uart_en上升沿，得到一个时钟周期的脉冲信号
-  assign en_flag = (~uart_send_last) & uart_send;
+//根据发送数据计数器来给uart发送端口赋值
+always @(posedge sys_clk or negedge sys_rst_n) begin
+  if (!sys_rst_n) begin
+    tx_flag <= 0;
+    clk_cnt <= 0;
+    uart_send_last1 <= 0;
+    uart_send_last2 <= 0;
+    uart_txd <= 1'b1;
+    tx_data <= 0;
+  end else begin
+    uart_send_last1 <= uart_send;
+    uart_send_last2 <= uart_send_last1;
 
-  reg uart_send_last;
-  //对发送使能信号uart_en延迟两个时钟周期
-  always @(posedge sys_clk or negedge sys_rst_n) begin
-    if (!sys_rst_n) begin
-      uart_send_last <= 0;
-    end else begin
-      uart_send_last <= uart_send;
+    if(uart_send_last2 && !uart_send_last1) begin //检测到发送使能上升沿
+      tx_flag <= 1; //进入发送过程，标志位tx_flag拉高
+      tx_data <= uart_data_in; //寄存待发送的数据
+    end else if ((clk_cnt == (9 * BPS_CNT + BPS_CNT_HALF))) begin//计数到停止位中间时，停止发送过程
+      tx_flag <= 0; //发送过程结束，标志位tx_flag拉低
     end
-  end
 
-  reg tx_flag;//发送过程标志信号
-  reg [ 7:0] tx_data;//寄存发送数据
-  //当脉冲信号en_flag到达时,寄存待发送的数据，并进入发送过程
-  always @(posedge sys_clk or negedge sys_rst_n) begin
-    if (!sys_rst_n) begin
-      tx_flag <= 0;
-      tx_data <= 0;
-    end else begin
-      if (en_flag) begin //检测到发送使能上升沿
-        tx_flag <= 1; //进入发送过程，标志位tx_flag拉高
-        tx_data <= uart_data_in; //寄存待发送的数据
-      end else if ((clk_cnt == (9 * BPS_CNT + BPS_CNT_HALF))) begin//计数到停止位中间时，停止发送过程
-        tx_flag <= 0; //发送过程结束，标志位tx_flag拉低
-      end
-    end 
-  end
-
-  reg [7:0] clk_cnt;//系统时钟计数器
-  //reg [ 3:0] tx_cnt;//发送数据计数器
-  //进入发送过程后，启动系统时钟计数器与发送数据计数器
-  always @(posedge sys_clk or negedge sys_rst_n) begin
-    if (!sys_rst_n) begin
-      clk_cnt <= 0;
-    end else begin
-      if (tx_flag) begin//处于发送过程
-        clk_cnt <= clk_cnt + 1'b1;
-      end else begin
-        clk_cnt <= 0;//对系统时钟计数达一个波特率周期后清零
-      end
-    end
-  end
-
-  //uart_txd
-  //根据发送数据计数器来给uart发送端口赋值
-  always @(posedge sys_clk or negedge sys_rst_n) begin
-    if (!sys_rst_n) begin
-      uart_txd <= 1'b1;        
-    end else if (tx_flag) begin
+    if (tx_flag) begin
+      clk_cnt <= clk_cnt + 1'b1;
       case(clk_cnt)
         (0 * BPS_CNT): uart_txd <= 1'b0; //起始位 
         (1 * BPS_CNT): uart_txd <= tx_data[0]; //数据位最低位
@@ -81,9 +54,11 @@ module uart_send_hs(
         default: ;
       endcase
     end else begin
+      clk_cnt <= 0;//对系统时钟计数达一个波特率周期后清零
       uart_txd <= 1'b1; //空闲时发送端口为高电平
     end
   end
+end
 
 endmodule
 
