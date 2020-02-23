91d487016a5f7c53a9b359de2b9a421c01ba0a82
abiaozsh <abiaozsh@sina.cn>
2020-02-23 16:49:58 +0800

    1

--- fpga/workspace/qsys/hello_world.c
+++ fpga/workspace/qsys/hello_world.c
@@ -2,18 +2,34 @@
 #include "system.h"
 #include "altera_avalon_pio_regs.h"
 
+void fdsa(int pp);
+
+volatile int aa = 0;
 int main() {
 	int data = 0;
-	while (1) {
+	int oldval = 0;
+	//while (1) {
 		int val = IORD(PIO_1_BASE, 0);
 
-		if(val & 1){
+		aa = IORD(0x0030, 0);
+
+		if(val != oldval){
 			data++;
+			IOWR(0x0030, 0, data);
+			IOWR(PIO_0_BASE, 0, data);
 		}
+		oldval = val;
 
-		IOWR(PIO_0_BASE, 0, data);
 
-	}
-
+	//}
+	fdsa(aa);
 	return 0;
+}
+
+
+
+volatile int bb;
+
+void fdsa(int pp){
+	bb = pp;
 }
---
+++ fpga/workspace/qsys/makehex.bat
@@ -0,0 +1,2 @@
+Z:\altera\13.0sp1\nios2eds\bin\gnu\H-i686-mingw32\bin\nios2-elf-objcopy.exe -O ihex qsys.elf output_file.hex
+pause
