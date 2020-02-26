b146b0243a34a927256d2d1d84dcc620b8fa2032
abiaozsh <abiaozsh@sina.cn>
2020-02-26 22:00:17 +0800

    1

---
+++ fpga/dotnetSerialProgLoader/WindowsFormsApplication1/DASM.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+	public class DASM
+	{
+
+		public enum Instype
+		{
+			call = 0x00,
+			jmpi = 0x01,
+			ldbu = 0x03,
+			addi = 0x04,
+			stb = 0x05,
+			br = 0x06,
+			ldb = 0x07,
+			cmpgei = 0x08,
+			ldhu = 0x0B,
+			andi = 0x0C,
+			sth = 0x0D,
+			bge = 0x0E,
+			ldh = 0x0F,
+			cmplti = 0x10,
+			initda = 0x13,
+			ori = 0x14,
+			stw = 0x15,
+			blt = 0x16,
+			ldw = 0x17,
+			cmpnei = 0x18,
+			flushda = 0x1B,
+			xori = 0x1C,
+			bne = 0x1E,
+			cmpeqi = 0x20,
+			ldbuio = 0x23,
+			muli = 0x24,
+			stbio = 0x25,
+			beq = 0x26,
+			ldbio = 0x27,
+			cmpgeui = 0x28,
+			ldhuio = 0x2B,
+			andhi = 0x2C,
+			sthio = 0x2D,
+			bgeu = 0x2E,
+			ldhio = 0x2F,
+			cmpltui = 0x30,
+			custom = 0x32,
+			initd = 0x33,
+			orhi = 0x34,
+			stwio = 0x35,
+			bltu = 0x36,
+			ldwio = 0x37,
+			rdprs = 0x38,
+			Rtype = 0x3A,
+			flushd = 0x3B,
+			xorhi = 0x3C,
+		}
+		public enum Rtype
+		{
+			none = 0x00,
+			cmplt = 0x10,
+			cmpeq = 0x20,
+			cmpltu = 0x30,
+			eret = 0x01,
+			add = 0x31,
+			roli = 0x02,
+			slli = 0x12,
+			rol = 0x03,
+			sll = 0x13,
+			flushp = 0x04,
+			wrprs = 0x14,
+			divu = 0x24,
+			break_ = 0x34,
+			ret = 0x05,
+			div = 0x25,
+			nor = 0x06,
+			or = 0x16,
+			rdctl = 0x26,
+			sync = 0x36,
+			mulxuu = 0x07,
+			mulxsu = 0x17,
+			mul = 0x27,
+			cmpge = 0x08,
+			cmpne = 0x18,
+			cmpgeu = 0x28,
+			bret = 0x09,
+			initi = 0x29,
+			sub = 0x39,
+			srli = 0x1A,
+			srai = 0x3A,
+			ror = 0x0B,
+			srl = 0x1B,
+			sra = 0x3B,
+			flushi = 0x0C,
+			nextpc = 0x1C,
+			jmp = 0x0D,
+			callr = 0x1D,
+			trap = 0x2D,
+			and = 0x0E,
+			xor = 0x1E,
+			wrctl = 0x2E,
+			mulxss = 0x1F,
+		}
+
+		static string[] convt = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+		private static string getHex4(int val)//"ll"+"hh"
+		{
+			return "0x" + convt[((val & 0xF000) >> 12)] + convt[((val & 0x0F00) >> 8)] + convt[((val & 0xF0) >> 4)] + convt[((val & 0x0F))];
+		}
+		public static string getHex2(byte val)
+		{
+			return convt[((val & 0xF0) >> 4)] + convt[((val & 0x0F))];
+		}
+
+		private static string Reg(uint val)
+		{
+			if (val == 26)
+			{
+				return "gp";
+			}
+			else if (val == 27)
+			{
+				return "sp";
+			}
+			else if (val == 28)
+			{
+				return "fp";
+			}
+			else
+			{
+				return "r" + val;
+			}
+		}
+
+		private static string CReg(uint val)
+		{/*
+Register 	Name 	Register Contents
+0 	status 	Refer to The status Register
+1 	estatus 	Refer to The estatus Register
+2 	bstatus 	Refer to The bstatus Register
+3 	ienable 	Internal interrupt-enable bits
+
+The ienable Register
+
+Available only when the external interrupt controller interface is not present. Otherwise reserved.
+4 	ipending 	Pending internal interrupt bits
+
+The ipending Register
+
+Available only when the external interrupt controller interface is not present. Otherwise reserved.
+5 	cpuid 	Unique processor identifier
+6 	Reserved 	Reserved
+7 	exception 	Refer to The exception Register
+8 	pteaddr 	Refer to The pteaddr Register
+
+Available only when the MMU is present. Otherwise reserved.
+9 	tlbacc 	Refer to The tlbacc Register
+
+Available only when the MMU is present. Otherwise reserved.
+10 	tlbmisc 	Refer to The tlbmisc Register
+
+Available only when the MMU is present. Otherwise reserved.
+11 	eccinj 	Refer to The eccinj Register
+
+Available only when ECC is present.
+12 	badaddr 	Refer to The badaddr Register
+13 	config 	Refer to The config Register
+
+Available only when the MPU or ECC is present. Otherwise reserved.
+14 	mpubase 	Refer to The mpubase Register
+
+Available only when the MPU is present. Otherwise reserved.
+15 	mpuacc 	Refer to The mpuacc Register for MASK variations table.
+
+Available only when the MPU is present. Otherwise reserved.*/
+			if (val == 0)
+			{
+				return "ctl_0_status";
+			}
+			else if (val == 3)
+			{
+				return "ctl_3_ienable";
+			}
+			else
+			{
+				return "ctl_" + val;
+			}
+		}
+
+        public static string decode(byte[] data,uint baseaddr)
+		{
+			/*
+	Register 	Name 	Function 	Register 	Name 	Function
+	r0 	zero 	0x00000000 	r16 	  	Callee-saved register
+	r1 	at 	Assembler temporary 	r17 	  	Callee-saved register
+	r2 	  	Return value 	r18 	  	Callee-saved register
+	r3 	  	Return value 	r19 	  	Callee-saved register
+	r4 	  	Register arguments 	r20 	  	Callee-saved register
+	r5 	  	Register arguments 	r21 	  	Callee-saved register
+	r6 	  	Register arguments 	r22 	  	Callee-saved register
+	r7 	  	Register arguments 	r23 	  	Callee-saved register
+	r8 	  	Caller-saved register 	r24 	et 	Exception temporary
+	r9 	  	Caller-saved register 	r25 	bt 	Breakpoint temporary
+	r10 	  	Caller-saved register 	r26 	gp 	Global pointer
+	r11 	  	Caller-saved register 	r27 	sp 	Stack pointer
+	r12 	  	Caller-saved register 	r28 	fp 	Frame pointer
+	r13 	  	Caller-saved register 	r29 	ea 	Exception return address
+	r14 	  	Caller-saved register 	r30 	sstatus 	Status register
+	r15 	  	Caller-saved register 	r31 	ra 	Return address
+	Note: r25 is used exclusively by the JTAG debug module. It is used as the breakpoint temporary (bt) register in the normal register set. In shadow register sets, r25 is reserved.
+	Note: r30 is used as the breakpoint return address (ba) in the normal register set, and as the shadow register set status (sstatus) in each shadow register set. For details about sstatus, refer to The Status Register section.
+	For more information, refer to the Application Binary Interface chapter of the Nios® II Processor Reference Handbook.
+			 */
+            if (data.Length % 4 != 0) return "str";
+			StringBuilder sb = new StringBuilder();
+			int pos = 0;
+			for (int i = 0; i < data.Length; i += 4)
+			{
+				uint ins = (uint)(data[i + 0]) | (uint)(data[i + 1] << 8) | (uint)(data[i + 2] << 16) | (uint)(data[i + 3] << 24);
+				string hex = getHex2(data[i + 0]) + getHex2(data[i + 1]) + getHex2(data[i + 2]) + getHex2(data[i + 3]);
+				string sIns = "";
+
+				Instype itype = (Instype)(ins & 0x3F);
+				Rtype rtype = Rtype.none;
+				if (itype == Instype.Rtype)
+				{
+					rtype = (Rtype)((ins >> (6 + 5)) & 0x3F);
+					string A = Reg(ins >> (32 - 5));
+					string B = Reg((ins >> (32 - 10)) & 0x1F);
+					string C = Reg((ins >> (32 - 15)) & 0x1F);
+					string CR = CReg((ins >> (6)) & 0x1F);
+
+					sIns = rtype.ToString() + " ";
+					if (rtype == Rtype.add)
+					{
+						sIns += C + " <- " + A + "+" + B;
+					}
+					else if (rtype == Rtype.cmpeq)
+					{
+						sIns += C + " <- (" + A + " == " + B + " ? 1 : 0)";
+					}
+					else if (rtype == Rtype.cmplt)
+					{
+						sIns += C + " <- ((signed)" + A + " < (signed)" + B + " ? 1 : 0)";
+					}
+					else if (rtype == Rtype.cmpge)
+					{
+						sIns += C + " <- ((signed)" + A + " >= (signed)" + B + " ? 1 : 0)";
+					}
+					else if (rtype == Rtype.sll)
+					{
+						sIns += C + " <- " + A + " << " + B + "[4:0]";
+					}
+					else if (rtype == Rtype.callr)
+					{
+						sIns += "PC <- " + A;
+					}
+					else if (rtype == Rtype.jmp)
+					{
+						sIns += "PC <- " + A;
+					}
+					else if (rtype == Rtype.wrctl)
+					{
+						sIns += CR + " <- " + A;
+					}
+					else if (rtype == Rtype.ret)
+					{
+						sIns += "PC <- " + A;
+					}
+					else
+					{
+						sIns += "--------------------------------------";
+					}
+				}
+				else
+				{
+					string A = Reg(ins >> (32 - 5));
+					string B = Reg((ins >> (32 - 10)) & 0x1F);
+					uint IMM = (ins >> (32 - 26)) & 0xFFFF;
+
+					sIns = itype.ToString() + " ";
+
+					if (itype == Instype.ori)
+					{
+						sIns += B + " <- " + A + " | " + getHex4((int)IMM);
+					}
+					else if (itype == Instype.orhi)
+					{
+						sIns += B + " <- " + A + " | " + getHex4((int)IMM) + "0000";
+					}
+					else if (itype == Instype.addi)
+					{
+						sIns += B + " <- " + A + (((short)IMM) > 0 ? "+" : "") + ((short)IMM);
+					}
+					else if (itype == Instype.andi)
+					{
+						sIns += B + " <- " + A + "&" + getHex4((int)IMM);
+					}
+					else if (itype == Instype.cmplti)
+					{
+						sIns += B + " <- ((signed)" + A + "<" + ((short)IMM) + " ? 1 : 0 )";
+					}
+					else if (itype == Instype.stw || itype == Instype.stwio)
+					{
+						sIns += "[" + A + "+" + getHex4((int)IMM) + "] <- " + B;
+					}
+					else if (itype == Instype.ldw || itype == Instype.ldwio)
+					{
+						sIns += B + " <- [" + A + "+" + getHex4((int)IMM) + "]";
+					}
+					else if (itype == Instype.br)
+					{
+						sIns += ((short)IMM / 4) + "(" + (pos + 1 + ((short)IMM / 4)) + ")";
+					}
+					else if (itype == Instype.beq)
+					{
+						sIns += A + "==" + B + "," + ((short)IMM / 4) + "(" + (pos + 1 + ((short)IMM / 4)) + ")";
+					}
+					else if (itype == Instype.bne)
+					{
+						sIns += A + "!=" + B + "," + ((short)IMM / 4) + "(" + (pos + 1 + ((short)IMM / 4)) + ")";
+					}
+					else if (itype == Instype.bltu || itype == Instype.blt)
+					{
+						sIns += A + "<" + B + "," + ((short)IMM / 4) + "(" + (pos + 1 + ((short)IMM / 4)) + ")";
+					}
+					else if (itype == Instype.call || itype == Instype.jmpi)
+					{
+                        sIns += ((ins >> 6)) + "(" + (((ins >> 6)) - (baseaddr / 4)) + ")";
+					}
+					else
+					{
+						sIns += A + "," + B + "," + getHex4((int)IMM) + "--------------------------------------";
+					}
+				}
+				sb.Append(pos + "[" + hex + "]" + " : " + sIns + "\r\n");
+				pos++;
+			}
+			return sb.ToString();
+		}
+	}
+}
---
+++ fpga/dotnetSerialProgLoader/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+using System.Globalization;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+        SerialPort port;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormClosed += new FormClosedEventHandler(Form1_FormClosed);
+        }
+
+        void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (port != null && port.IsOpen)
+            {
+                port.Close();
+            }
+        }
+        string portName;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String[] list = SerialPort.GetPortNames();
+            foreach (String s in list)
+            {
+                this.comboBox1.Items.Add(s);
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            portName = (string)this.comboBox1.SelectedItem;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (port == null)
+            {
+                //COM4为Arduino使用的串口号，需根据实际情况调整  115200
+                port = new SerialPort(portName, 2000000, Parity.None, 8, StopBits.One);
+                port.Open();
+            }
+
+        }
+
+        byte[] readFromPort(int count)
+        {
+            if (count <= 0) return null;
+            byte[] buff = new byte[count];
+            int idx = 0;
+            long t = DateTime.Now.Ticks;
+            while (true)
+            {
+                if (port.BytesToRead > 0)
+                {
+                    buff[idx] = (byte)port.ReadByte();
+                    idx++;
+                    count--;
+                    if (count <= 0) return buff;
+                }
+                var diff = DateTime.Now.Ticks - t;
+                if (diff > 250000)
+                {
+                    return buff;
+                    //throw new Exception();
+                }
+            }
+        }
+        bool readFromPort(int count, byte[] buff, int idx)
+        {
+            if (count <= 0) return false;
+            if (port == null) return false;
+            int pos = 0;
+            long t = DateTime.Now.Ticks;
+            while (true)
+            {
+                int num = port.BytesToRead;
+                if (num > 0)
+                {
+                    if (num > count)
+                    {
+                        num = count;
+                        count = 0;
+                    }
+                    else
+                    {
+                        count -= num;
+                    }
+                    port.Read(buff, idx + pos, num);
+                    pos += num;
+                    if (count <= 0) return true;
+                }
+                var diff = DateTime.Now.Ticks - t;
+                if (diff > 250000)
+                {
+                    throw new Exception();
+                }
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (port != null && port.IsOpen)
+            {
+                port.Close();
+                port = null;
+            }
+        }
+        void portWrite(byte val1, byte val2)
+        {
+            byte[] buff = new byte[2];
+            buff[0] = val1;
+            buff[1] = val2;
+            if (port != null)
+            {
+                port.Write(buff, 0, 2);
+            }
+        }
+        void portWrite(byte val1)
+        {
+            byte[] buff = new byte[1];
+            buff[0] = val1;
+            if (port != null)
+            {
+                port.Write(buff, 0, 1);
+            }
+        }
+
+        void portWrite(byte val1, byte val2, Stream s)
+        {
+            s.WriteByte(val1);
+            s.WriteByte(val2);
+
+        }
+        void portWrite(byte val1, Stream s)
+        {
+            s.WriteByte(val1);
+        }
+
+        void sendall(byte[] buff)
+        {
+            port.Write(buff, 0, buff.Length);
+        }
+
+
+        string[] convt = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+        private string getHex4(int val)//"ll"+"hh"
+        {
+            return convt[((val & 0xF0) >> 4)] + convt[((val & 0x0F))] + convt[((val & 0xF000) >> 12)] + convt[((val & 0x0F00) >> 8)];
+        }
+        private string getHex2(int val)//"ll"+"hh"
+        {
+            return convt[((val & 0xF0) >> 4)] + convt[((val & 0x0F))];
+        }
+        private string getHex(byte[] buff, int idx, int len)//"ll"+"hh"
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = idx; i < idx + len; i++)
+            {
+                byte val = buff[i];
+                sb.Append(convt[((val & 0xF0) >> 4)] + convt[((val & 0x0F))]);
+            }
+            return sb.ToString();
+        }
+
+
+        private string getdata(int addr)
+        {
+            int addr0 = (addr) & 0xFF;
+            int addr1 = (addr >> 8) & 0xFF;
+            int addr2 = (addr >> 16) & 0xFF;
+            portWrite((byte)(0x10), (byte)addr0);
+            portWrite((byte)(0x11), (byte)addr1);
+            portWrite((byte)(0x12), (byte)addr2);
+
+            portWrite((byte)(0x30), (byte)0x00);
+
+            string data = "";
+            portWrite((byte)(0x20), (byte)0x00);
+            data += getHex2(readFromPort(1)[0]);
+            portWrite((byte)(0x21), (byte)0x00);
+            data += getHex2(readFromPort(1)[0]);
+            return data;
+        }
+        private void writedata(int addr, int data)
+        {
+            //portWrite((byte)(0x10), (byte)(0x30/4));
+            int addr0 = (addr) & 0xFF;
+            int addr1 = (addr >> 8) & 0xFF;
+            int addr2 = (addr >> 16) & 0xFF;
+            portWrite((byte)(0x10), (byte)addr0);
+            portWrite((byte)(0x11), (byte)addr1);
+            portWrite((byte)(0x12), (byte)addr2);
+
+            int data0 = (data) & 0xFF;
+            int data1 = (data >> 8) & 0xFF;
+            portWrite((byte)(0x14), (byte)data0);
+            portWrite((byte)(0x15), (byte)data1);
+
+            portWrite((byte)(0x31), (byte)0x00);
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            writedata(22, 0x2232);
+            writedata(23, 0x3333);
+            writedata(24, 0x4434);
+            writedata(25, 0x5535);
+            writedata(26, 0x6636);
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string temp = "";
+            temp += getdata(21) + " ";
+            temp += getdata(22) + " ";
+            temp += getdata(23) + " ";
+            temp += getdata(24) + " ";
+            temp += getdata(25) + " ";
+            temp += getdata(26) + " ";
+            this.textBox1.Text = temp;
+        }
+
+
+    }
+}
