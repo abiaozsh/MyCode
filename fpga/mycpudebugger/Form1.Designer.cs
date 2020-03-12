5aa8338c87c516bf640017ab2480a54aed97aeb0
abiaozsh <abiaozsh@sina.cn>
2020-03-12 22:18:40 +0800

    1

---
+++ fpga/mycpudebugger/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿using System.Windows.Forms;
+namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.button1 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(105, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(61, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "connect";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(172, 13);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(82, 23);
+			this.button4.TabIndex = 7;
+			this.button4.Text = "getstatus";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// textBox4
+			// 
+			this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox4.Location = new System.Drawing.Point(12, 42);
+			this.textBox4.Multiline = true;
+			this.textBox4.Name = "textBox4";
+			this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox4.Size = new System.Drawing.Size(400, 328);
+			this.textBox4.TabIndex = 8;
+			this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(12, 13);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(76, 20);
+			this.comboBox1.TabIndex = 17;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(424, 382);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.ComboBox comboBox1;
+
+	}
+}
+
---
+++ fpga/mycpudebugger/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+using System.Globalization;
+
+namespace WindowsFormsApplication1
+{
+
+	public sealed partial class Form1 : Base
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			this.FormClosed += new FormClosedEventHandler(Form1_FormClosed);
+		}
+
+		void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			if (port != null && port.IsOpen)
+			{
+				port.Close();
+			}
+		}
+		string portName;
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			String[] list = SerialPort.GetPortNames();
+			foreach (String s in list)
+			{
+				this.comboBox1.Items.Add(s);
+			}
+		}
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			portName = (string)this.comboBox1.SelectedItem;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			connect(portName);
+		}
+
+
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (port != null && port.IsOpen)
+			{
+				port.Close();
+				port = null;
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			//test uart
+			Random r = new Random();
+			bool err = false;
+			int count = 0;
+			for (int i = 0; i < 100000; i++)
+			{
+				byte[] buff = new byte[1];
+				r.NextBytes(buff);
+				port.Write(buff, 0, 1);
+
+				byte[] buff2 = readFromPort(1);
+
+				if (buff[0] != buff2[0])
+				{
+					err = true;
+					count = i;
+					break;
+				}
+			}
+			this.Text = "error:" + count + "," + err.ToString();
+		}
+
+
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append("eax:");
+			portWrite((byte)(0xA3), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xA2), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xA1), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xA0), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			sb.AppendLine();
+
+			sb.Append("ebx:");
+			portWrite((byte)(0xA7), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xA6), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xA5), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xA4), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			sb.AppendLine();
+
+			sb.Append("ecx:");
+			portWrite((byte)(0xAB), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xAA), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xA9), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xA8), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			sb.AppendLine();
+
+			sb.Append("edx:");
+			portWrite((byte)(0xAF), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xAE), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xAD), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xAC), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			sb.AppendLine();
+
+			sb.Append("ebp:");
+			portWrite((byte)(0xB3), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xB2), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xB1), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xB0), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			sb.AppendLine();
+
+			sb.Append("esp:");
+			portWrite((byte)(0xB7), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xB6), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xB5), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xB4), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			sb.AppendLine();
+
+			sb.Append("status: ");
+			//...
+			byte statusbyte0;
+			portWrite((byte)(0xE0), (byte)0x00); statusbyte0 = (readFromPort(1)[0]);
+
+			sb.Append(((statusbyte0 & 0x08) != 0) ? "overflow," : "no overflow,");
+			sb.Append(((statusbyte0 & 0x04) != 0) ? "sign neg," : "sign pos,");
+			sb.Append(((statusbyte0 & 0x02) != 0) ? "carry," : "no carry,");
+			sb.Append(((statusbyte0 & 0x01) != 0) ? "zero," : "no zero,");
+			sb.AppendLine();
+
+			sb.Append("pc:");
+			portWrite((byte)(0xE7), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xE6), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xE5), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			portWrite((byte)(0xE4), (byte)0x00); sb.Append(getHex2(readFromPort(1)[0]));
+			sb.AppendLine();
+
+			this.textBox4.Text = sb.ToString();
+
+		}
+
+		private void textBox4_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+
+	}
+}
