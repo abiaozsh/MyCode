3c24d90587ede4d3fe705831fa7bcfc5b6940fbb
abiaozsh <abiaozsh@sina.cn>
2020-03-15 17:55:52 +0800

    1

--- fpga/mycpuproj/b.c
+++ fpga/mycpuproj/b.c
@@ -3,11 +3,15 @@
 //中断向量
 //浮点数
 
-volatile int temp;
+volatile char temp1;
+volatile short temp2;
+volatile int temp3;
+volatile long long temp4;
 int cnt = 0;
+int cnt1 = 0;
 
 int __main(){
-  
+  asm("aaamov ra,rb");
   return;
 }
 
@@ -20,14 +24,20 @@ void test(int* p1,int* p2){
       val=val1;
       cnt++;
       *p2 = cnt;
-      temp = cnt;
+      temp1 = (char)cnt;
+      temp2 = (short)cnt;
+      temp3 = (int)cnt;
+      temp4 = (long long)cnt;
     }
   }
 
 }
 
+int cnt2 = 0;
+char cnt3=0;
 int main(){
   int* p1 = (int*)(0x02001000);
   int* p2 = (int*)(0x02001010);
+  cnt2++;
   test(p1,p2);
 }
