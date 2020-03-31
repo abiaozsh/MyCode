18990c0838c3d8a877908e729318ea606cc0bad4
abiaozsh <abiaozsh@sina.cn>
2020-03-31 20:36:54 +0800

    1

---
+++ fpga/uartClient/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿using System.Windows.Forms;
+namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(105, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(61, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "connect";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox3.Location = new System.Drawing.Point(392, 41);
+			this.textBox3.Multiline = true;
+			this.textBox3.Name = "textBox3";
+			this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox3.Size = new System.Drawing.Size(511, 699);
+			this.textBox3.TabIndex = 5;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.textBox4.Location = new System.Drawing.Point(13, 41);
+			this.textBox4.Multiline = true;
+			this.textBox4.Name = "textBox4";
+			this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox4.Size = new System.Drawing.Size(373, 699);
+			this.textBox4.TabIndex = 8;
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(12, 13);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(76, 20);
+			this.comboBox1.TabIndex = 17;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(172, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(731, 21);
+			this.textBox1.TabIndex = 18;
+			this.textBox1.KeyDown += textBox1_KeyDown;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(915, 752);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private TextBox textBox1;
+
+	}
+}
+
---
+++ fpga/uartClient/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+using System.Globalization;
+
+namespace WindowsFormsApplication1
+{
+
+	public sealed partial class Form1 : Base
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			this.FormClosed += new FormClosedEventHandler(Form1_FormClosed);
+		}
+
+		void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			if (port != null && port.IsOpen)
+			{
+				port.Close();
+			}
+		}
+		string portName;
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			String[] list = SerialPort.GetPortNames();
+			foreach (String s in list)
+			{
+				this.comboBox1.Items.Add(s);
+			}
+		}
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			portName = (string)this.comboBox1.SelectedItem;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			connect(portName);
+		}
+
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			while (fifo.Count > 0)
+			{
+				char c = fifo.Dequeue();
+				textBox4.Text += c;
+				textBox3.Text += " " + getHex2((byte)c);
+			}
+		}
+
+		void textBox1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				port.Write(textBox1.Text);
+				textBox1.Text = "";
+			}
+		}
+
+	}
+}
